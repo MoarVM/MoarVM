f137274fbf1239867212c02c424e282701e2a214
jnthn <jnthn@jnthn.net>
2012-04-29 00:59:22 +0200

    Places to stash the various BOOT types and the core KnowHOW meta-object.

--- src/core/instance.h
+++ src/core/instance.h
@@ -1,3 +1,12 @@
+/* The various "bootstrap" types, based straight off of some core
+ * representations. They are used during the 6model bootstrap. */
+struct _MVMBootTypes {
+    MVMObject *BOOTStr;
+    MVMObject *BOOTArray;
+    MVMObject *BOOTHash;
+    MVMObject *BOOTCode;
+};
+
 /* Represents a MoarVM instance. */
 typedef struct _MVMInstance {
     /* The list of active threads. */
@@ -5,4 +14,11 @@ typedef struct _MVMInstance {
     
     /* The number of active threads. */
     MVMuint16 num_threads;
+    
+    /* Set of bootstrapping types. */
+    struct _MVMBootTypes *boot_types;
+    
+    /* The KnowHOW meta-object; all other meta-objects (which are
+     * built in user-space) are built out of this. */
+    MVMObject *KnowHOW;
 } MVMInstance;
