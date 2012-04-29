df1a21a49c67e845fe3c9160eda7c32a2c5444f7
jnthn <jnthn@jnthn.net>
2012-04-29 16:28:23 +0200

    Add MVM_gc_allocate_stable to the appropriate header file.

--- src/gc/allocation.h
+++ src/gc/allocation.h
@@ -1,4 +1,5 @@
 void * MVM_gc_allocate(MVMThreadContext *tc, size_t size);
 void * MVM_gc_allocate_zeroed(MVMThreadContext *tc, size_t size);
+MVMSTable * MVM_gc_allocate_stable(MVMThreadContext *tc, MVMREPROps *repr, MVMObject *how);
 MVMObject * MVM_gc_allocate_type_object(MVMThreadContext *tc, MVMSTable *st);
 MVMObject * MVM_gc_allocate_object(MVMThreadContext *tc, MVMSTable *st, size_t size);
