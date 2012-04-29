bdc7f53c8bc6eb2786b63ad75d6e452095ab013e
jnthn <jnthn@jnthn.net>
2012-04-29 19:35:09 +0200

    Scatter MVM_NO_RETURN on a couple of exceptiony bits.

--- src/core/exceptions.c
+++ src/core/exceptions.c
@@ -4,12 +4,14 @@
  * unrecoverable.
  * TODO: Some hook for embedders.
  */
+MVM_NO_RETURN
 void MVM_panic(const char *reason) {
     fprintf(stderr, reason);
     exit(1);
 }
 
 /* Throws an ad-hoc (untyped) exception. */
+MVM_NO_RETURN
 void MVM_exception_throw_adhoc(MVMThreadContext *tc, const char *message) {
     /* XXX Well, need to implement exceptions. So for now just panic. */
     MVM_panic(message);
--- src/core/exceptions.h
+++ src/core/exceptions.h
@@ -1,2 +1,2 @@
-void MVM_panic(const char *reason);
-void MVM_exception_throw_adhoc(MVMThreadContext *tc, const char *message);
+MVM_NO_RETURN void MVM_panic(const char *reason);
+MVM_NO_RETURN void MVM_exception_throw_adhoc(MVMThreadContext *tc, const char *message);
