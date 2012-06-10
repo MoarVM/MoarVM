23a432929036aecf4ed6f5ff6efa928ecdcccafe
diakopter <diakopter@gmail.com>
2012-06-10 15:12:26 -0700

    zero the memory, if only to make debugging easier.

--- src/strings/utf8.c
+++ src/strings/utf8.c
@@ -279,6 +279,7 @@ MVMuint8 * MVM_string_utf8_encode(MVMThreadContext *tc, MVMString *str, MVMuint6
     MVMuint8 *result = malloc(sizeof(MVMint32) * str->body.graphs);
     MVMuint8 *arr = result;
     size_t i = 0;
+    memset(result, 0, sizeof(MVMint32) * str->body.graphs);
     while (i < str->body.graphs && (arr = utf8_encode(arr, str->body.data[i++])));
     if (!arr)
         MVM_exception_throw_adhoc(tc, "Error encoding UTF-8 string");
