38962a6a26a872408a1b028da4d779c39dc2513c
diakopter <diakopter@gmail.com>
2012-06-11 15:05:07 -0700

    apr_lib

--- build/Config/APR.pm
+++ build/Config/APR.pm
@@ -20,7 +20,8 @@ sub configure {
     }
     else {
         return (%config,
-            apr_build_line => 'cd 3rdparty/apr && ./configure && make'
+            apr_build_line => 'cd 3rdparty/apr && ./configure && make',
+            apr_lib => '3rdparty/apr/.libs/libapr-1.so.0'
         );
     }
 }
