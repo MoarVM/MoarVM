af2bbe9b9b269efc9ab5d5b485f13d195476aec1
diakopter <diakopter@gmail.com>
2012-06-16 12:57:00 -0700

    file seek

--- src/io/fileops.c
+++ src/io/fileops.c
@@ -322,6 +322,18 @@ void MVM_file_spew(MVMThreadContext *tc, MVMString *output, MVMString *filename)
     /* XXX need to GC free the filehandle? */
 }
 
+/* seeks in a filehandle */
+void MVM_file_seek(MVMThreadContext *tc, MVMObject *oshandle, MVMint64 offset, MVMint64 flag) {
+    apr_status_t rv;
+    MVMOSHandle *handle;
+    
+    verify_filehandle_type(tc, oshandle, &handle, "seek in filehandle");
+    
+    if ((rv = apr_file_seek(handle->body.file_handle, (apr_seek_where_t)flag, (apr_off_t)offset)) != APR_SUCCESS) {
+        MVM_exception_throw_apr_error(tc, rv, "Failed to seek in filehandle: ");
+    }
+}
+
 /* return an OSHandle representing one of the standard streams */
 static MVMObject * MVM_file_get_stdstream(MVMThreadContext *tc, MVMObject *type_object, MVMuint8 type) {
     MVMOSHandle *result;
