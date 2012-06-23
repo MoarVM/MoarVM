6e9d50a06b9f2e0b991d3c005efb02dbca246ccf
jnthn <jnthn@jnthn.net>
2012-06-23 16:51:12 +0200

    Start to fill out the nursery worklist processing algorithm.

--- src/gc/nursery.c
+++ src/gc/nursery.c
@@ -31,6 +31,10 @@ void MVM_gc_nursery_collect(MVMThreadContext *tc) {
     
     /* Add temporary roots and process them. */
     
+    /* Add things that are roots for the first generation because
+     * they are pointed to by objects in the second generation and
+     * process them. */
+    
     /* Find roots in frames and process them. */
     
     /* Destroy the worklist. */
@@ -39,8 +43,61 @@ void MVM_gc_nursery_collect(MVMThreadContext *tc) {
 
 /* Processes the current worklist. */
 static void process_worklist(MVMThreadContext *tc, MVMGCWorklist *worklist) {
-    printf("Going to process worklist of %d items\n", worklist->items);
-    MVM_panic(15, "Out of memory; GC not yet implemented!");
+    MVMCollectable **item_ptr;
+    while (item_ptr = MVM_gc_worklist_get(tc, worklist)) {
+        /* Dereference the object we're considering. */
+        MVMCollectable *item = *item_ptr;
+
+        /* If the item is NULL, that's fine - it's just a null reference and
+         * thus we've no object to consider. */
+        if (item == NULL)
+            continue;
+
+        /* If it's in the second generation, we have nothing to do. */
+        if (item->flags & MVM_CF_SECOND_GEN)
+            continue;
+        
+        /* If we already saw the item and copied it, then it will have a
+         * forwarding address already. Just update this pointer to the
+         * new address and we're done. */
+        if (item->forwarder) {
+            *item_ptr = item->forwarder;
+            continue;
+        }
+        
+        /* If we saw it in the nursery before, then we will promote it
+         * to the second generation. */
+        if (item->flags & MVM_CF_NURSERY_SEEN) {
+            MVM_panic(15, "Promotion to second generation in NYI!");
+            continue;
+        }
+        
+        /* Otherwise, we need to do the copy. What sort of thing are we
+         * going to copy? */
+        if (!(item->flags & (MVM_CF_TYPE_OBJECT | MVM_CF_STABLE | MVM_CF_SC))) {
+            /* It's an object instance. Get the size from the STable. */
+            
+            /* Copy it to tospace and set the forwarding pointer. */
+            
+            /* Set the "seen in nursery" flag. */
+            
+            /* If needed, mark it. This will add addresses to the worklist
+             * that will need updating. Note that we are passing the address
+             * of the object *after* copying it since those are the addresses
+             * we care about updating; the old chunk of memory is now dead! */
+             
+            MVM_panic(15, "Can't copy objects in the GC yet");
+        }
+        else if (item->flags & MVM_CF_TYPE_OBJECT) {
+            MVM_panic(15, "Can't copy type objects in the GC yet");
+        }
+        else if (item->flags & MVM_CF_STABLE) {
+            MVM_panic(15, "Can't copy stables in the GC yet");
+        }
+        else if (item->flags & MVM_CF_SC) {
+            MVM_panic(15, "Can't copy serialization contexts in the GC yet");
+        }
+    }
 }
 
 /* Some objects, having been copied, need no further attention. Others
