c093e5684940ec0d55fa55029bcb30fa402bc917
jnthn <jnthn@jnthn.net>
2012-06-24 13:02:18 +0200

    Use temporary roots mechanism to make sure required things stay anchored in the various allocation routines.

--- src/gc/allocation.c
+++ src/gc/allocation.c
@@ -48,28 +48,37 @@ void * MVM_gc_allocate_zeroed(MVMThreadContext *tc, size_t size) {
 /* Allocates a new STable, based on the specified thread context, REPR
  * and meta-object. */
 MVMSTable * MVM_gc_allocate_stable(MVMThreadContext *tc, MVMREPROps *repr, MVMObject *how) {
-    MVMSTable *st    = MVM_gc_allocate_zeroed(tc, sizeof(MVMSTable));
+    MVMSTable *st;
+    MVM_gc_root_temp_push(tc, (MVMCollectable **)&how);
+    st               = MVM_gc_allocate_zeroed(tc, sizeof(MVMSTable));
     st->header.flags = MVM_CF_STABLE;
     st->header.owner = tc->thread_id;
     st->REPR         = repr;
     st->HOW          = how;
+    MVM_gc_root_temp_pop(tc);
     return st;
 }
 
 /* Allocates a new type object. */
 MVMObject * MVM_gc_allocate_type_object(MVMThreadContext *tc, MVMSTable *st) {
-    MVMObject *obj    = MVM_gc_allocate_zeroed(tc, sizeof(MVMObject));
+    MVMObject *obj;
+    MVM_gc_root_temp_push(tc, (MVMCollectable **)&st);
+    obj               = MVM_gc_allocate_zeroed(tc, sizeof(MVMObject));
     obj->header.flags = MVM_CF_TYPE_OBJECT;
     obj->header.owner = tc->thread_id;
     obj->st           = st;
+    MVM_gc_root_temp_pop(tc);
     return obj;
 }
 
 /* Allocates a new object, and points it at the specified STable. */
 MVMObject * MVM_gc_allocate_object(MVMThreadContext *tc, MVMSTable *st) {
-    MVMObject *obj    = MVM_gc_allocate_zeroed(tc, st->size);
+    MVMObject *obj;
+    MVM_gc_root_temp_push(tc, (MVMCollectable **)&st);
+    obj               = MVM_gc_allocate_zeroed(tc, st->size);
     obj->header.owner = tc->thread_id;
     obj->st           = st;
+    MVM_gc_root_temp_pop(tc);
     return obj;
 }
 
