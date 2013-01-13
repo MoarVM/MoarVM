a38756ee9b41c494ecfd8ec9c1bfe393896d9cac
Geoffrey Broadwell <gjb@sonic.net>
2013-01-13 11:08:08 -0800

    Printing cleanup in top level Configure.pl

--- Configure.pl
+++ Configure.pl
@@ -9,40 +9,48 @@ use Config::Generate;
 
 print "Welcome to MoarVM!\n\n";
 
-print "Trying to figure out how to build on your system ...";
+print dots("Trying to figure out how to build on your system ...");
 my %config = Config::BuildEnvironment::detect();
 if (!$config{'excuse'}) {
-    print "... OK\n    (OS: $config{'os'}, Compiler: $config{'cc'}, Linker: $config{'link'})\n";
+    print " OK\n    (OS: $config{'os'}, Compiler: $config{'cc'}, Linker: $config{'link'})\n";
 }
 else {
-    print "... FAILED\n";
+    print " FAILED\n";
     die "    Sorry, I'm not sure how to build on this platform:\n    $config{'excuse'}\n";
 }
 
-print "Configuring APR ...";
+print dots("Configuring APR ...");
 %config = Config::APR::configure(%config);
 
 sub check_excuse {
     if (!$config{'excuse'}) {
-        print ".................................... OK\n";
+        print " OK\n";
     }
     else {
-        print ".................................... FAILED\n";
+        print " FAILED\n";
         die "    $config{'excuse'}\n";
     }
 }
 check_excuse();
 
-print "Configuring libatomic_ops ...";
+print dots("Configuring libatomic_ops ...");
 %config = Config::LAO::configure(%config);
 check_excuse();
 
-print "Generating config.h ...";
+print dots("Generating config.h ...");
 Config::Generate::file('build/config.h.in', 'src/gen/config.h', %config);
-print "................................ OK\n";
+print " OK\n";
 
-print "Generating Makefile ...";
+print dots("Generating Makefile ...");
 Config::Generate::file('build/Makefile.in', 'Makefile', %config);
-print "................................ OK\n";
+print " OK\n";
 
 print "\nConfiguration successful. Type '$config{'make'}' to build.\n";
+
+
+sub dots {
+    my $message = shift;
+    my $length  = shift || 55;
+
+    return $message . '.' x ($length - length $message);
+}
