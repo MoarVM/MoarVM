13f42dce91cbd6f88dfab6d55a6202f860175541
Jonathan Worthington <jnthn@jnthn.net>
2013-03-04 21:39:01 +0100

    Fix APR config/build on Win32.
    Hopefully without breaking 64-bit.

--- build/Config/APR.pm
+++ build/Config/APR.pm
@@ -7,12 +7,18 @@ sub configure {
     
     if ($^O =~ /MSWin32/) {
         if ($config{'make'} eq 'nmake') {
-            # XXX TODO: Stuff for linking it in.
-            return (%config,
-                # XXX Won't fly on 32-bit...
-                apr_build_line => 'cd 3rdparty/apr && nmake -f Makefile.win ARCH="x64 Release" buildall',
-                apr_lib => '3rdparty/apr/x64/LibR/apr-1.lib'
-            );
+            if (`cl 2>&1` =~ /80x86/) {
+                return (%config,
+                    apr_build_line => 'cd 3rdparty/apr && nmake -f Makefile.win ARCH="Win32 Release" buildall',
+                    apr_lib => '3rdparty/apr/LibR/apr-1.lib'
+                );
+            }
+            else {
+                return (%config,
+                    apr_build_line => 'cd 3rdparty/apr && nmake -f Makefile.win ARCH="x64 Release" buildall',
+                    apr_lib => '3rdparty/apr/x64/LibR/apr-1.lib'
+                );
+            }
         }
         else {
             return (excuse => "Don't know how to build APR on Windows without Microsoft toolchain");
