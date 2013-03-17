cf5f38acb53792fb4ccadbbf60a5bf5b33cc8bbc
jnthn <jnthn@jnthn.net>
2013-03-17 14:12:47 +0100

    Add an MVMROOT macro.
    Usage is:
    
        MVMROOT(tc, local_variable, { ... });
    
    For the duration of the block, the specified local variable will be
    pushed as a temporary root, so even if a GC run is triggered it will
    stay where it is. Boils down to the same code you'd write by hand to
    call the push/pop function, but with less room for errors, less typing
    and some indentation to indicate the covered region.

--- src/gc/roots.h
+++ src/gc/roots.h
@@ -1,3 +1,4 @@
+/* Functions related to roots. */
 void MVM_gc_root_add_permanent(MVMThreadContext *tc, MVMCollectable **obj_ref);
 void MVM_gc_root_add_permanents_to_worklist(MVMThreadContext *tc, MVMGCWorklist *worklist);
 void MVM_gc_root_add_instance_roots_to_worklist(MVMThreadContext *tc, MVMGCWorklist *worklist);
@@ -10,3 +11,10 @@ void MVM_gc_root_gen2_agg_add(MVMThreadContext *tc, MVMCollectable *aggregate);
 void MVM_gc_root_add_gen2s_to_worklist(MVMThreadContext *tc, MVMGCWorklist *worklist);
 void MVM_gc_root_gen2_cleanup_promoted(MVMThreadContext *tc);
 void MVM_gc_root_add_frame_roots_to_worklist(MVMThreadContext *tc, MVMGCWorklist *worklist, MVMFrame *start_frame);
+
+/* Macros related to rooting objects into the temporaries list, and
+ * unrooting them afterwards. */
+#define MVMROOT(tc, obj_ref, block) \
+    MVM_gc_root_temp_push(tc, (MVMCollectable **)&(obj_ref)); \
+    block \
+    MVM_gc_root_temp_pop(tc);
