063a6541ca2e7b87ee1f3dcfda6f7c0fee1ac81e
jnthn <jnthn@jnthn.net>
2013-04-06 12:37:04 +0200

    Uncomment MOP export and native types.

--- tools/nqp-cross-compiler/nqp-src/nqp-mo.pm
+++ tools/nqp-cross-compiler/nqp-src/nqp-mo.pm
@@ -1592,14 +1592,14 @@ knowhow NQPModuleHOW {
 # Bind the HOWs into the EXPORTHOW package under the package declarator
 # names.
 my knowhow EXPORTHOW {
-#    ($?PACKAGE.WHO)<module>       := NQPModuleHOW;
-#    ($?PACKAGE.WHO)<class>        := NQPClassHOW;
-#    ($?PACKAGE.WHO)<class-attr>   := NQPAttribute;
-#    ($?PACKAGE.WHO)<grammar>      := NQPClassHOW;
-#    ($?PACKAGE.WHO)<grammar-attr> := NQPAttribute;
-#    ($?PACKAGE.WHO)<role>         := NQPParametricRoleHOW;
-#    ($?PACKAGE.WHO)<role-attr>    := NQPAttribute;
-#    ($?PACKAGE.WHO)<native>       := NQPNativeHOW;
+    ($?PACKAGE.WHO)<module>       := NQPModuleHOW;
+    ($?PACKAGE.WHO)<class>        := NQPClassHOW;
+    ($?PACKAGE.WHO)<class-attr>   := NQPAttribute;
+    ($?PACKAGE.WHO)<grammar>      := NQPClassHOW;
+    ($?PACKAGE.WHO)<grammar-attr> := NQPAttribute;
+    ($?PACKAGE.WHO)<role>         := NQPParametricRoleHOW;
+    ($?PACKAGE.WHO)<role-attr>    := NQPAttribute;
+    ($?PACKAGE.WHO)<native>       := NQPNativeHOW;
 }
 
 # vim: set ft=perl6 nomodifiable :
