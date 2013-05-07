99bda64349cebe66c0c53f75c4c95d4d180e3fdd
Geoffrey Broadwell <gjb@sonic.net>
2013-05-07 10:31:37 -0700

    Fix memory handling in coerce_sI and coerce_Is op implementations

--- src/core/interp.c
+++ src/core/interp.c
@@ -1598,10 +1598,11 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                         MVMString *s = GET_REG(cur_op, 2).s, *type = GET_REG(cur_op, 4).o;
                         MVMROOT(tc, s, {
                             MVMROOT(tc, type, {
-                                MVMuint8 *buf = MVM_string_ascii_encode(tc, s, NULL);
-                                MVMROOT(tc, buf, {
-                                    MVMObject *a = MVM_repr_alloc_init(tc, type);
+                                MVMObject *a = MVM_repr_alloc_init(tc, type);
+                                MVMROOT(tc, a, {
+                                    MVMuint8 *buf = MVM_string_ascii_encode(tc, s, NULL);
                                     MVM_bigint_from_str(a, buf);
+                                    free(buf);
                                     GET_REG(cur_op, 0).o = a;
                                 });
                             });
@@ -1611,7 +1612,9 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                     }
                     case MVM_OP_coerce_Is: {
                         MVMObject *a = GET_REG(cur_op, 2).o;
-                        GET_REG(cur_op, 0).s = MVM_bigint_to_str(tc, a);
+                        MVMROOT(tc, a, {
+                            GET_REG(cur_op, 0).s = MVM_bigint_to_str(tc, a);
+                        });
                         cur_op += 4;
                         break;
                     }
