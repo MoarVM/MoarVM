b10f29b155e32bfd0bf591674884ca6679102c0b
Tobias Leich <email@froggs.de>
2013-07-24 13:03:15 +0200

    satisfy -Wpointer-sign

--- src/strings/unicode_ops.c
+++ src/strings/unicode_ops.c
@@ -2,12 +2,12 @@
 /* Looks up a codepoint by name. Lazily constructs a hash. */
 MVMCodepoint32 MVM_unicode_lookup_by_name(MVMThreadContext *tc, MVMString *name) {
     MVMuint64 size;
-    char *cname = MVM_string_ascii_encode(tc, name, &size);
+    unsigned char *cname = MVM_string_ascii_encode(tc, name, &size);
     MVMUnicodeNameHashEntry *result;
     if (!codepoints_by_name) {
         generate_codepoints_by_name(tc);
     }
-    HASH_FIND(hash_handle, codepoints_by_name, cname, strlen(cname), result);
+    HASH_FIND(hash_handle, codepoints_by_name, cname, strlen((const char *)cname), result);
     free(cname);
     return result ? result->codepoint : -1;
 }
@@ -46,12 +46,12 @@ void generate_property_codes_by_names_aliases(MVMThreadContext *tc) {
 
 MVMint32 MVM_unicode_name_to_property_code(MVMThreadContext *tc, MVMString *name) {
     MVMuint64 size;
-    char *cname = MVM_string_ascii_encode(tc, name, &size);
+    unsigned char *cname = MVM_string_ascii_encode(tc, name, &size);
     MVMUnicodeNameHashEntry *result;
     if (!property_codes_by_names_aliases) {
         generate_property_codes_by_names_aliases(tc);
     }
-    HASH_FIND(hash_handle, property_codes_by_names_aliases, cname, strlen(cname), result);
+    HASH_FIND(hash_handle, property_codes_by_names_aliases, cname, strlen((const char *)cname), result);
     free(cname); /* not really codepoint, really just an index */
     return result ? result->codepoint : 0;
 }
@@ -98,7 +98,7 @@ static void generate_unicode_property_values_hashes(MVMThreadContext *tc) {
 
 MVMint32 MVM_unicode_name_to_property_value_code(MVMThreadContext *tc, MVMint64 property_code, MVMString *name) {
     MVMuint64 size;
-    char *cname = MVM_string_ascii_encode(tc, name, &size);
+    unsigned char *cname = MVM_string_ascii_encode(tc, name, &size);
     MVMUnicodeNameHashEntry *result;
     
     if (property_code < 0 || property_code >= MVMNUMPROPERTYCODES)
@@ -107,7 +107,7 @@ MVMint32 MVM_unicode_name_to_property_value_code(MVMThreadContext *tc, MVMint64 
     if (!unicode_property_values_hashes) {
         generate_unicode_property_values_hashes(tc);
     }
-    HASH_FIND(hash_handle, unicode_property_values_hashes[property_code], cname, strlen(cname), result);
+    HASH_FIND(hash_handle, unicode_property_values_hashes[property_code], cname, strlen((const char *)cname), result);
     free(cname); /* not really codepoint, really just an index */
     return result ? result->codepoint : 0;
 }
