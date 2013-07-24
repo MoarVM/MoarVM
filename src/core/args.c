98c7dabb42ca7b9634279201f0b65b174619bc14
Tobias Leich <email@froggs.de>
2013-07-24 15:15:19 +0200

    fixed reallocation of new_args
    This makes t/nqp/57-construction.t pass using clang.

--- src/core/args.c
+++ src/core/args.c
@@ -585,11 +585,11 @@ static void flatten_args(MVMThreadContext *tc, MVMArgProcContext *ctx) {
             
             HASH_ITER(hash_handle, body->hash_head, current, tmp) {
                 
-                if (new_arg_pos == new_args_size) {
-                    new_args = realloc(new_args, (new_args_size *= 2) * sizeof(MVMRegister));
+                if (new_arg_pos + 2 > new_args_size) {
+                    new_args = realloc(new_args, (new_args_size += 2) * sizeof(MVMRegister));
                 }
-                if (new_flag_pos == new_arg_flags_size) {
-                    new_arg_flags = realloc(new_arg_flags, (new_arg_flags_size *= 2) * sizeof(MVMCallsiteEntry));
+                if (new_flag_pos + 2 > new_arg_flags_size) {
+                    new_arg_flags = realloc(new_arg_flags, (new_arg_flags_size += 2) * sizeof(MVMCallsiteEntry));
                 }
                 
                 (new_args + new_arg_pos++)->s = (MVMString *)current->key;
