0aa87fbc27c5571731982501584415ae099fec95
Jimmy Zhuo <zhuomingliang01@qq.com>
2013-08-13 22:49:26 +0800

    starting get uv_handle_t into MVMOSHandle

--- src/6model/reprs/MVMOSHandle.h
+++ src/6model/reprs/MVMOSHandle.h
@@ -1,5 +1,10 @@
 /* Representation used by VM-level OS handles. */
 struct MVMOSHandleBody {
+    uv_handle_t *handle;
+
+    MVMuint8 tty_type; /* We need it untill libuv supports uv_file_t,
+                        * they said will support it soon. */
+
     /* see MVMOSHandleTypes */
     MVMuint8 handle_type;
     MVMuint8 encoding_type;
@@ -18,8 +23,14 @@ struct MVMOSHandle {
 };
 
 typedef enum {
-    MVM_OSHANDLE_UNINIT = 0,
-    MVM_OSHANDLE_FILE   = 1,
+    MVM_STDIN  = 0,
+    MVM_STDOUT = 1,
+    MVM_STDERR = 2
+} MVMSTDHandleType;
+
+typedef enum {
+    MVM_OSHANDLE_UNINIT = UV_UNKNOWN_HANDLE,
+    MVM_OSHANDLE_FILE   = UV_FILE,
     MVM_OSHANDLE_DIR    = 2,
     MVM_OSHANDLE_SOCKET = 3
 } MVMOSHandleTypes;
