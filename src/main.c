550002b4710266606b093dc9da6bfa2dc3e121b1
Gerhard R <gerd.r.devel@googlemail.com>
2013-08-21 20:27:19 +0200

    No longer use APR for command line arg parsing

--- src/main.c
+++ src/main.c
@@ -1,71 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <moarvm.h>
 
-int main(int argc, const char *argv[]) {
+/* flags need to be sorted alphabetically */
+
+enum {
+    NOT_A_FLAG = -2,
+    UNKNOWN_FLAG = -1,
+
+    FLAG_CRASH,
+    FLAG_DUMP,
+    FLAG_HELP,
+};
+
+static const char *const FLAGS[] = { "--crash", "--dump", "--help" };
+
+static const char USAGE[] = "\
+USAGE: moarvm [--dump] [--crash] input.moarvm [program args]\n\
+       moarvm --help\n\
+\n\
+    --help   display this message\n\
+    --dump   dump the bytecode to stdout instead of executing\n\
+    --crash  abort instead of exiting on unhandled exception";
+
+static int cmp_flag(const void *key, const void *value)
+{
+    return strcmp(key, *(char **)value);
+}
+
+static int parse_flag(const char *arg)
+{
+    const char *const *found;
+
+    if(!arg || arg[0] != '-')
+        return NOT_A_FLAG;
+
+    found = bsearch(arg, FLAGS, sizeof FLAGS / sizeof *FLAGS, sizeof *FLAGS, cmp_flag);
+    return found ? found - FLAGS : UNKNOWN_FLAG;
+}
+
+int main(int argc, char *argv[])
+{
     MVMInstance *instance;
+    const char  *input_file;
 
-    apr_status_t rv;
-    apr_pool_t *mp;
-    static const apr_getopt_option_t opt_option[] = {
-        /* values must be greater than 255 so it doesn't have a single-char form
-           Otherwise, use a character such as 'h' */
-        { "dump", 256, 0, "dump bytecode" },
-        { "help", 257, 0, "show help" },
-        { "crash", 258, 0, "abort instead of exiting on unhandled exception" },
-        { NULL, 0, 0, NULL }
-    };
-    apr_getopt_t *opt;
-    int optch;
-    const char *optarg;
     int dump = 0;
-    char *input_file;
-    int exitcode = 0;
-    const char *helptext = "\
-    MoarVM usage: moarvm [options] bytecode.moarvm [program args]           \n\
-      --help, display this message                                          \n\
-      --dump, dump the bytecode to stdout instead of executing              \n\
-      --crash, abort instead of exiting on unhandled exception  \n";
-    int processed_args = 0;
+    int argi = 1;
+    int flag;
 
-    instance = MVM_vm_create_instance();
-
-    apr_pool_create(&mp, NULL);
-    apr_getopt_init(&opt, mp, argc, argv);
-    while ((rv = apr_getopt_long(opt, opt_option, &optch, &optarg)) == APR_SUCCESS) {
-        switch (optch) {
-        case 256:
-            dump = 1;
-            break;
-        case 257:
-            printf("%s", helptext);
-            goto terminate;
-        case 258:
+    for (; (flag = parse_flag(argv[argi])) != NOT_A_FLAG; ++argi) {
+        switch (flag) {
+            case FLAG_CRASH:
             MVM_crash_on_error();
-            break;
+            continue;
+
+            case FLAG_DUMP:
+            dump = 1;
+            continue;
+
+            case FLAG_HELP:
+            puts(USAGE);
+            return EXIT_SUCCESS;
+
+            default:
+            fprintf(stderr, "ERROR: Unknown flag %s.\n\n%s\n", argv[argi], USAGE);
+            return EXIT_FAILURE;
         }
     }
-    processed_args = opt->ind;
-    if (processed_args == argc) {
-        fprintf(stderr, "Error: You must supply an input file.\n\n%s", helptext);
-        exitcode = 1;
-        goto terminate;
+
+    if (argi >= argc) {
+        fprintf(stderr, "ERROR: Missing input file.\n\n%s\n", USAGE);
+        return EXIT_FAILURE;
     }
-    input_file = (char *)opt->argv[processed_args++];
+
+    instance   = MVM_vm_create_instance();
+    input_file = argv[argi++];
 
     /* stash the rest of the raw command line args in the instance */
-    instance->num_clargs = argc - processed_args;
-    instance->raw_clargs = (char **)(opt->argv + processed_args);
+    instance->num_clargs = argc - argi;
+    instance->raw_clargs = argv + argi;
 
-    if (dump) {
-        MVM_vm_dump_file(instance, input_file);
-    }
-    else {
-        MVM_vm_run_file(instance, input_file);
-    }
+    if (dump) MVM_vm_dump_file(instance, input_file);
+    else MVM_vm_run_file(instance, input_file);
 
-  terminate:
-    apr_pool_destroy(mp);
-    MVM_vm_destroy_instance(instance);
-
-    return exitcode;
+    return EXIT_SUCCESS;
 }
