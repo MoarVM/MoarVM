e0ac69adaba96e5b9956ac2c362936593a219497
Gerhard R <gerd.r.devel@googlemail.com>
2013-08-21 12:06:43 +0200

    Do not rely on APR for byte order detection

--- src/core/bytecode.h
+++ src/core/bytecode.h
@@ -1,9 +1,2 @@
 void MVM_bytecode_unpack(MVMThreadContext *tc, MVMCompUnit *cu);
 MVMBytecodeAnnotation * MVM_bytecode_resolve_annotation(MVMThreadContext *tc, MVMStaticFrameBody *sfb, MVMuint32 offset);
-
-/* Steal endianness from APR */
-#ifdef APR_IS_BIGENDIAN
- #if APR_IS_BIGENDIAN
-  #define MVM_BIGENDIAN           1
- #endif
-#endif
