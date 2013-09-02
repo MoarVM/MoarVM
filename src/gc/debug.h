fabda86d29ac21d9e35b0b19d0ce0d9c90f7fd50
jnthn <jnthn@jnthn.net>
2013-09-02 22:55:49 +0200

    Add header with handy GC debugging macro.
    Have hacked this into code a couple of times while debugging GC issues
    and figured it's best shared and easily available.

---
+++ src/gc/debug.h
@@ -0,0 +1,8 @@
+/* Include this file after moarvm.h in a source file to get some help with GC
+ * debugging. */
+ 
+#define MVM_ASSERT_NOT_FROMSPACE(tc, c) do { \
+        if ((char *)(c) >= (char *)tc->nursery_fromspace && \
+                (char *)(c) < (char *)tc->nursery_fromspace + MVM_NURSERY_SIZE) \
+            MVM_exception_throw_adhoc(tc, "Collectable in fromspace accessed"); \
+    } while (0);
