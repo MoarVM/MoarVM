0439d5ed4922163750cc96787338cc477689739c
Gerhard R <gerd.r.devel@googlemail.com>
2013-09-13 12:07:32 +0200

    Add missing const to MVM_string_ascii_decode*()

--- src/strings/ascii.h
+++ src/strings/ascii.h
@@ -1,5 +1,5 @@
-MVMString * MVM_string_ascii_decode(MVMThreadContext *tc, MVMObject *result_type, char *ascii, size_t bytes);
-MVMString * MVM_string_ascii_decode_nt(MVMThreadContext *tc, MVMObject *result_type, char *ascii);
+MVMString * MVM_string_ascii_decode(MVMThreadContext *tc, MVMObject *result_type, const char *ascii, size_t bytes);
+MVMString * MVM_string_ascii_decode_nt(MVMThreadContext *tc, MVMObject *result_type, const char *ascii);
 MVMuint8 * MVM_string_ascii_encode_substr(MVMThreadContext *tc, MVMString *str, MVMuint64 *output_size, MVMint64 start, MVMint64 length);
 MVMuint8 * MVM_string_ascii_encode(MVMThreadContext *tc, MVMString *str, MVMuint64 *output_size);
 MVMuint8 * MVM_string_ascii_encode_any(MVMThreadContext *tc, MVMString *str);
