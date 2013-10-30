11d8ad8ca76df4c12abe8b518c59c7ea5c8f887c
Moritz Lenz <moritz@faui2k3.org>
2013-10-30 21:11:18 +0100

    [Configure] fix for non-existing --prefix
    abs_path produces the empty string in such cases, use File::Spec->rel2abs instead

--- Configure.pl
+++ Configure.pl
@@ -7,7 +7,7 @@ use warnings;
 use Config;
 use Getopt::Long;
 use Pod::Usage;
-use Cwd qw/abs_path/;
+use File::Spec;
 
 use build::setup;
 use build::auto;
@@ -67,7 +67,7 @@ $config{name}   = $NAME;
 $config{perl}   = $^X;
 $config{config} = join ' ', map { / / ? "\"$_\"" : $_ } @args;
 
-$config{prefix} = abs_path($args{prefix} // '.');
+$config{prefix} = File::Spec->rel2abs($args{prefix} // '.');
 
 # set options that take priority over all others
 my @keys = qw( cc ld make );
