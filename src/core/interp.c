091a2369ef11f6e4689be8e2c2381ed462c2adef
jnthn <jnthn@jnthn.net>
2013-11-30 23:38:10 +0100

    pow_I tried to box a bigint into a Num box.
    The op is actually meant to fall back to Num in certain cases. That
    still needs to be implemented. For now, this makes it at least not
    explode.

--- src/core/interp.c
+++ src/core/interp.c
@@ -1778,7 +1778,8 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 goto NEXT;
             }
             OP(pow_I): {
-                MVMObject *   const type = GET_REG(cur_op, 6).o;
+                /* XXX Needs to handle the Num fallback case too. */
+                MVMObject *   const type = GET_REG(cur_op, 8).o;
                 MVMObject * const result = MVM_repr_alloc_init(tc, type);
                 MVM_bigint_pow(tc, result, GET_REG(cur_op, 2).o, GET_REG(cur_op, 4).o);
                 GET_REG(cur_op, 0).o = result;
