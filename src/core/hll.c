36e958e937569c2a052ae3ed3b2cab1f98531519
Nicholas Clark <nick@ccl4.org>
2013-12-25 21:57:20 +0100

    In MVM_hll_set_config() need to root config_hash, as GC might move it.
    The calls to MVM_string_utf8_decode() allocate, which means that they can
    trigger GC.

--- src/core/hll.c
+++ src/core/hll.c
@@ -73,20 +73,23 @@ MVMObject * MVM_hll_set_config(MVMThreadContext *tc, MVMString *name, MVMObject 
         MVM_exception_throw_adhoc(tc, "set hll config needs concrete hash");
     }
 
-    check_config_key(tc, config_hash, "int_box", int_box_type, config);
-    check_config_key(tc, config_hash, "num_box", num_box_type, config);
-    check_config_key(tc, config_hash, "str_box", str_box_type, config);
-    check_config_key(tc, config_hash, "slurpy_array", slurpy_array_type, config);
-    check_config_key(tc, config_hash, "slurpy_hash", slurpy_hash_type, config);
-    check_config_key(tc, config_hash, "array_iter", array_iterator_type, config);
-    check_config_key(tc, config_hash, "hash_iter", hash_iterator_type, config);
-    check_config_key(tc, config_hash, "foreign_type_int", foreign_type_int, config);
-    check_config_key(tc, config_hash, "foreign_type_num", foreign_type_num, config);
-    check_config_key(tc, config_hash, "foreign_type_str", foreign_type_str, config);
-    check_config_key(tc, config_hash, "foreign_transform_array", foreign_transform_array, config);
-    check_config_key(tc, config_hash, "foreign_transform_hash", foreign_transform_hash, config);
-    check_config_key(tc, config_hash, "foreign_transform_code", foreign_transform_code, config);
-    check_config_key(tc, config_hash, "null_value", null_value, config);
+    /* MVM_string_utf8_decode() can potentially allocate, and hence gc. */
+    MVMROOT(tc, config_hash, {
+            check_config_key(tc, config_hash, "int_box", int_box_type, config);
+            check_config_key(tc, config_hash, "num_box", num_box_type, config);
+            check_config_key(tc, config_hash, "str_box", str_box_type, config);
+            check_config_key(tc, config_hash, "slurpy_array", slurpy_array_type, config);
+            check_config_key(tc, config_hash, "slurpy_hash", slurpy_hash_type, config);
+            check_config_key(tc, config_hash, "array_iter", array_iterator_type, config);
+            check_config_key(tc, config_hash, "hash_iter", hash_iterator_type, config);
+            check_config_key(tc, config_hash, "foreign_type_int", foreign_type_int, config);
+            check_config_key(tc, config_hash, "foreign_type_num", foreign_type_num, config);
+            check_config_key(tc, config_hash, "foreign_type_str", foreign_type_str, config);
+            check_config_key(tc, config_hash, "foreign_transform_array", foreign_transform_array, config);
+            check_config_key(tc, config_hash, "foreign_transform_hash", foreign_transform_hash, config);
+            check_config_key(tc, config_hash, "foreign_transform_code", foreign_transform_code, config);
+            check_config_key(tc, config_hash, "null_value", null_value, config);
+        });
 
     return config_hash;
 }
