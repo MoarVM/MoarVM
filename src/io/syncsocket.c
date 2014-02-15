2ed615e16a61ad7e2f4dd9f13cf05b80a635b696
jnthn <jnthn@jnthn.net>
2014-02-15 20:10:49 +0100

    Resolve hostnames in connect.

--- src/io/syncsocket.c
+++ src/io/syncsocket.c
@@ -1,5 +1,9 @@
 #include "moar.h"
 
+#if defined(_MSC_VER)
+#define snprintf _snprintf
+#endif
+
  /* Data that we keep for a socket-based handle. */
 typedef struct {
     /* Start with same fields as a sync stream, since we will re-use most
@@ -27,7 +31,29 @@ static void gc_free(MVMThreadContext *tc, MVMObject *h, void *d) {
     do_close(tc, data);
 }
 
-void on_connect(uv_connect_t* req, int status) {
+static struct sockaddr * resolve_host_name(MVMThreadContext *tc, MVMString *host, MVMint64 port) {
+    char *host_cstr = MVM_string_utf8_encode_C_string(tc, host);
+    struct sockaddr *dest = malloc(sizeof(struct sockaddr));
+    struct addrinfo *result;
+    int error;
+    char port_cstr[8];
+    snprintf(port_cstr, 8, "%d", (int)port);
+
+    error = getaddrinfo(host_cstr, port_cstr, NULL, &result);
+    free(host_cstr);
+    if (error == 0) {
+        memcpy(dest, result->ai_addr, sizeof(struct sockaddr));
+    }
+    else {
+        free(dest);
+        MVM_exception_throw_adhoc(tc, "Failed to resolve host name");
+    }
+    freeaddrinfo(result);
+
+    return dest;
+}
+
+static void on_connect(uv_connect_t* req, int status) {
     uv_unref((uv_handle_t *)req->handle);
     if (status < 0) {
         MVMThreadContext *tc = ((MVMIOSyncSocketData *)req->data)->ss.cur_tc;
@@ -38,26 +64,18 @@ void on_connect(uv_connect_t* req, int status) {
 void socket_connect(MVMThreadContext *tc, MVMOSHandle *h, MVMString *host, MVMint64 port) {
     MVMIOSyncSocketData *data = (MVMIOSyncSocketData *)h->body.data;
     if (!data->ss.handle) {
-        char         *host_cstr = MVM_string_utf8_encode_C_string(tc, host); /* XXX Host res. */
-        uv_tcp_t     *socket    = malloc(sizeof(uv_tcp_t));
-        uv_connect_t *connect   = malloc(sizeof(uv_connect_t));
-        struct sockaddr_in dest;
+        struct sockaddr *dest    = resolve_host_name(tc, host, port);
+        uv_tcp_t        *socket  = malloc(sizeof(uv_tcp_t));
+        uv_connect_t    *connect = malloc(sizeof(uv_connect_t));
         int r;
-
-        if ((r = uv_ip4_addr(host_cstr, port, &dest)) != 0) {
-            free(socket);
-            free(connect);
-            free(host_cstr);
-            MVM_exception_throw_adhoc(tc, "Failed to connect: %s", uv_strerror(r));
-        }
 
         data->ss.cur_tc = tc;
         connect->data   = data;
         if ((r = uv_tcp_init(tc->loop, socket)) < 0 ||
-                (r = uv_tcp_connect(connect, socket, (const struct sockaddr *)&dest, on_connect)) < 0) {
+                (r = uv_tcp_connect(connect, socket, dest, on_connect)) < 0) {
             free(socket);
             free(connect);
-            free(host_cstr);
+            free(dest);
             MVM_exception_throw_adhoc(tc, "Failed to connect: %s", uv_strerror(r));
         }
         uv_ref((uv_handle_t *)socket);
@@ -66,7 +84,7 @@ void socket_connect(MVMThreadContext *tc, MVMOSHandle *h, MVMString *host, MVMin
         data->ss.handle = (uv_stream_t *)socket;
 
         free(connect);
-        free(host_cstr);
+        free(dest);
     }
     else {
         MVM_exception_throw_adhoc(tc, "Socket already has a connection");
