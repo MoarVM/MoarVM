a927cb55075be03642f2c65ad14b421c79049411
Jonathan Worthington <jnthn@jnthn.net>
2014-03-11 23:00:13 +0100

    Add encode all the string UTF-16 function.

--- src/strings/utf16.c
+++ src/strings/utf16.c
@@ -116,8 +116,13 @@ MVMuint8 * MVM_string_utf16_encode_substr(MVMThreadContext *tc, MVMString *str, 
             result_pos += 2;
         }
     }
-    result_pos[0] = 0;
+    result_pos[0] = result_pos[1] = 0;
     if (output_size)
         *output_size = (char *)result_pos - (char *)result;
     return (MVMuint8 *)result;
+}
+
+/* Encodes the whole string, double-NULL terminated. */
+MVMuint8 * MVM_string_utf16_encode(MVMThreadContext *tc, MVMString *str) {
+    return MVM_string_utf16_encode_substr(tc, str, NULL, 0, NUM_GRAPHS(str));
 }
