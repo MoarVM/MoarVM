81d57d523753e9cd941fba9879653a71f0e8d401
jnthn <jnthn@jnthn.net>
2014-03-31 21:08:25 +0200

    Protect all I/O operations using handle mutex.
    This takes it at a much higher level, which should avoid issues with
    duplicate takes. Uses exception-release mechanism also, to be far more
    robust in the event of an exception.

--- src/io/io.c
+++ src/io/io.c
@@ -9,10 +9,25 @@ static MVMOSHandle * verify_is_handle(MVMThreadContext *tc, MVMObject *oshandle,
     return (MVMOSHandle *)oshandle;
 }
 
+uv_mutex_t * acquire_mutex(MVMThreadContext *tc, MVMOSHandle *handle) {
+    uv_mutex_t *mutex = handle->body.mutex;
+    uv_mutex_lock(mutex);
+    MVM_tc_set_ex_release_mutex(tc, mutex);
+    return mutex;
+}
+
+void release_mutex(MVMThreadContext *tc, uv_mutex_t *mutex) {
+    uv_mutex_unlock(mutex);
+    MVM_tc_clear_ex_release_mutex(tc);
+}
+
 void MVM_io_close(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "close");
-    if (handle->body.ops->closable)
+    if (handle->body.ops->closable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->closable->close(tc, handle);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot close this kind of handle");
 }
@@ -21,8 +36,11 @@ void MVM_io_set_encoding(MVMThreadContext *tc, MVMObject *oshandle, MVMString *e
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "set encoding");
     MVMROOT(tc, handle, {
         const MVMuint8 encoding_flag = MVM_string_find_encoding(tc, encoding_name);
-        if (handle->body.ops->encodable)
+        if (handle->body.ops->encodable) {
+            uv_mutex_t *mutex = acquire_mutex(tc, handle);
             handle->body.ops->encodable->set_encoding(tc, handle, encoding_flag);
+            release_mutex(tc, mutex);
+        }
         else
             MVM_exception_throw_adhoc(tc, "Cannot set encoding on this kind of handle");
     });
@@ -30,40 +48,58 @@ void MVM_io_set_encoding(MVMThreadContext *tc, MVMObject *oshandle, MVMString *e
 
 void MVM_io_seek(MVMThreadContext *tc, MVMObject *oshandle, MVMint64 offset, MVMint64 flag) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "seek");
-    if (handle->body.ops->seekable)
+    if (handle->body.ops->seekable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->seekable->seek(tc, handle, offset, flag);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot seek this kind of handle");
 }
 
 MVMint64 MVM_io_tell(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "tell");
-    if (handle->body.ops->seekable)
-        return handle->body.ops->seekable->tell(tc, handle);
+    if (handle->body.ops->seekable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMint64 result = handle->body.ops->seekable->tell(tc, handle);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot tell this kind of handle");
 }
 
 void MVM_io_set_separator(MVMThreadContext *tc, MVMObject *oshandle, MVMString *sep) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "set separator");
-    if (handle->body.ops->sync_readable)
+    if (handle->body.ops->sync_readable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->sync_readable->set_separator(tc, handle, sep);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot set a separator on this kind of handle");
 }
 
 MVMString * MVM_io_readline(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "readline");
-    if (handle->body.ops->sync_readable)
-        return handle->body.ops->sync_readable->read_line(tc, handle);
+    if (handle->body.ops->sync_readable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMString *result = handle->body.ops->sync_readable->read_line(tc, handle);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot read lines from this kind of handle");
 }
 
 MVMString * MVM_io_read_string(MVMThreadContext *tc, MVMObject *oshandle, MVMint64 chars) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "read string");
-    if (handle->body.ops->sync_readable)
-        return handle->body.ops->sync_readable->read_chars(tc, handle, chars);
+    if (handle->body.ops->sync_readable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMString *result = handle->body.ops->sync_readable->read_chars(tc, handle, chars);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot read characters from this kind of handle");
 }
@@ -84,8 +120,11 @@ void MVM_io_read_bytes(MVMThreadContext *tc, MVMObject *oshandle, MVMObject *res
     if (length < 1 || length > 99999999)
         MVM_exception_throw_adhoc(tc, "read from filehandle length out of range");
 
-    if (handle->body.ops->sync_readable)
+    if (handle->body.ops->sync_readable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         bytes_read = handle->body.ops->sync_readable->read_bytes(tc, handle, &buf, length);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot read characters from this kind of handle");
 
@@ -98,8 +137,12 @@ void MVM_io_read_bytes(MVMThreadContext *tc, MVMObject *oshandle, MVMObject *res
 
 MVMString * MVM_io_slurp(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "slurp");
-    if (handle->body.ops->sync_readable)
-        return handle->body.ops->sync_readable->slurp(tc, handle);
+    if (handle->body.ops->sync_readable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMString *result = handle->body.ops->sync_readable->slurp(tc, handle);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot slurp this kind of handle");
 }
@@ -108,8 +151,12 @@ MVMint64 MVM_io_write_string(MVMThreadContext *tc, MVMObject *oshandle, MVMStrin
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "write string");
     if (str == NULL)
         MVM_exception_throw_adhoc(tc, "Failed to write to filehandle: NULL string given");
-    if (handle->body.ops->sync_writable)
-        return handle->body.ops->sync_writable->write_str(tc, handle, str, addnl);
+    if (handle->body.ops->sync_writable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMint64 result = handle->body.ops->sync_writable->write_str(tc, handle, str, addnl);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot write a string to this kind of handle");
 }
@@ -130,72 +177,102 @@ void MVM_io_write_bytes(MVMThreadContext *tc, MVMObject *oshandle, MVMObject *bu
     output = ((MVMArray *)buffer)->body.slots.i8 + ((MVMArray *)buffer)->body.start;
     output_size = ((MVMArray *)buffer)->body.elems;
 
-    if (handle->body.ops->sync_writable)
+    if (handle->body.ops->sync_writable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         bytes_written = handle->body.ops->sync_writable->write_bytes(tc, handle, output, output_size);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot write bytes to this kind of handle");
 }
 
 MVMint64 MVM_io_eof(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "eof");
-    if (handle->body.ops->sync_readable)
-        return handle->body.ops->sync_readable->eof(tc, handle);
+    if (handle->body.ops->sync_readable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMint64 result = handle->body.ops->sync_readable->eof(tc, handle);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot eof this kind of handle");
 }
 
 MVMint64 MVM_io_lock(MVMThreadContext *tc, MVMObject *oshandle, MVMint64 flag) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "lock");
-    if (handle->body.ops->lockable)
-        return handle->body.ops->lockable->lock(tc, handle, flag);
+    if (handle->body.ops->lockable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMint64 result = handle->body.ops->lockable->lock(tc, handle, flag);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot lock this kind of handle");
 }
 
 void MVM_io_unlock(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "unlock");
-    if (handle->body.ops->lockable)
+    if (handle->body.ops->lockable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->lockable->unlock(tc, handle);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot unlock this kind of handle");
 }
 
 void MVM_io_flush(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "flush");
-    if (handle->body.ops->sync_writable)
+    if (handle->body.ops->sync_writable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->sync_writable->flush(tc, handle);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot flush this kind of handle");
 }
 
 void MVM_io_truncate(MVMThreadContext *tc, MVMObject *oshandle, MVMint64 offset) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "truncate");
-    if (handle->body.ops->sync_writable)
+    if (handle->body.ops->sync_writable) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->sync_writable->truncate(tc, handle, offset);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot truncate this kind of handle");
 }
 
 void MVM_io_connect(MVMThreadContext *tc, MVMObject *oshandle, MVMString *host, MVMint64 port) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "connect");
-    if (handle->body.ops->sockety)
+    if (handle->body.ops->sockety) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->sockety->connect(tc, handle, host, port);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot connect this kind of handle");
 }
 
 void MVM_io_bind(MVMThreadContext *tc, MVMObject *oshandle, MVMString *host, MVMint64 port) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "bind");
-    if (handle->body.ops->sockety)
+    if (handle->body.ops->sockety) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
         handle->body.ops->sockety->bind(tc, handle, host, port);
+        release_mutex(tc, mutex);
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot bind this kind of handle");
 }
 
 MVMObject * MVM_io_accept(MVMThreadContext *tc, MVMObject *oshandle) {
     MVMOSHandle *handle = verify_is_handle(tc, oshandle, "accept");
-    if (handle->body.ops->sockety)
-        return handle->body.ops->sockety->accept(tc, handle);
+    if (handle->body.ops->sockety) {
+        uv_mutex_t *mutex = acquire_mutex(tc, handle);
+        MVMObject *result = handle->body.ops->sockety->accept(tc, handle);
+        release_mutex(tc, mutex);
+        return result;
+    }
     else
         MVM_exception_throw_adhoc(tc, "Cannot accept this kind of handle");
 }
