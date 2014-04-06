42902dc71785d08351e379f25f6f846d356bdeff
Timo Paulssen <timonator@perpetuum-immobile.de>
2014-04-06 19:35:39 +0200

    spesh for fastcreate of MVMArray.

--- src/6model/reprs/MVMHash.c
+++ src/6model/reprs/MVMHash.c
@@ -154,6 +154,23 @@ static void deserialize_stable_size(MVMThreadContext *tc, MVMSTable *st, MVMSeri
     st->size = sizeof(MVMHash);
 }
 
+/* Bytecode specialization for this REPR. */
+static void spesh(MVMThreadContext *tc, MVMSTable *st, MVMSpeshGraph *g, MVMSpeshBB *bb, MVMSpeshIns *ins) {
+    switch (ins->info->opcode) {
+    case MVM_OP_create: {
+        MVMSpeshOperand target   = ins->operands[0];
+        MVMSpeshOperand type     = ins->operands[1];
+        ins->info                = MVM_op_get_op(MVM_OP_sp_fastcreate);
+        ins->operands            = MVM_spesh_alloc(tc, g, 3 * sizeof(MVMSpeshOperand));
+        ins->operands[0]         = target;
+        ins->operands[1].lit_i16 = sizeof(MVMHash);
+        ins->operands[2].lit_i16 = MVM_spesh_add_spesh_slot(tc, g, (MVMCollectable *)st);
+        MVM_spesh_get_facts(tc, g, type)->usages--;
+        break;
+    }
+    }
+}
+
 /* Initializes the representation. */
 const MVMREPROps * MVMHash_initialize(MVMThreadContext *tc) {
     return &this_repr;
@@ -188,7 +205,7 @@ static const MVMREPROps this_repr = {
     NULL, /* gc_mark_repr_data */
     NULL, /* gc_free_repr_data */
     compose,
-    NULL, /* spesh */
+    spesh,
     "VMHash", /* name */
     MVM_REPR_ID_MVMHash,
     0, /* refs_frames */
