3f5c316ec0497f26c45b63d6b39354410941d82a
Andrew Egeler <andrew@egeler.us>
2014-04-26 00:00:46 -0400

    Serialize CStruct repr_data

--- src/6model/reprs/CStruct.c
+++ src/6model/reprs/CStruct.c
@@ -612,13 +612,87 @@ static MVMStorageSpec get_storage_spec(MVMThreadContext *tc, MVMSTable *st) {
 /* Serializes the REPR data. */
 static void serialize_repr_data(MVMThreadContext *tc, MVMSTable *st, MVMSerializationWriter *writer) {
     MVMCStructREPRData *repr_data = (MVMCStructREPRData *)st->REPR_data;
-    /* Could do this, but can also re-compute it each time for now. */
+    MVMint32 i, num_classes, num_slots;
+
+    writer->write_int(tc, writer, repr_data->struct_size);
+    writer->write_int(tc, writer, repr_data->num_attributes);
+    writer->write_int(tc, writer, repr_data->num_child_objs);
+    for(i = 0; i < repr_data->num_attributes; i++){
+        writer->write_int(tc, writer, repr_data->attribute_locations[i]);
+        writer->write_int(tc, writer, repr_data->struct_offsets[i]);
+
+        writer->write_varint(tc, writer, repr_data->flattened_stables[i] != NULL);
+        if (repr_data->flattened_stables[i])
+            writer->write_stable_ref(tc, writer, repr_data->flattened_stables[i]);
+
+        writer->write_ref(tc, writer, repr_data->member_types[i]);
+    }
+
+    i=0;
+    while (repr_data->name_to_index_mapping[i].class_key)
+        i++;
+    num_classes = i;
+    writer->write_int(tc, writer, num_classes);
+    for(i = 0; i < num_classes; i++){
+        writer->write_ref(tc, writer, repr_data->name_to_index_mapping[i].class_key);
+        writer->write_ref(tc, writer, repr_data->name_to_index_mapping[i].name_map);
+    }
+
+    i=0;
+    while(repr_data->initialize_slots && repr_data->initialize_slots[i] != -1)
+        i++;
+    num_slots = i;
+    writer->write_int(tc, writer, num_slots);
+    for(i = 0; i < num_slots; i++){
+        writer->write_int(tc, writer, repr_data->initialize_slots[i]);
+    }
 }
 
 /* Deserializes the REPR data. */
 static void deserialize_repr_data(MVMThreadContext *tc, MVMSTable *st, MVMSerializationReader *reader) {
-    /* Just allocating it will do for now. */
-    st->REPR_data = calloc(1, sizeof(MVMCStructREPRData));
+    MVMCStructREPRData *repr_data = (MVMCStructREPRData *) malloc(sizeof(MVMCStructREPRData));
+    MVMint32 i, num_classes, num_slots;
+
+    repr_data->struct_size = reader->read_int(tc, reader);
+    repr_data->num_attributes = reader->read_int(tc, reader);
+    repr_data->num_child_objs = reader->read_int(tc, reader);
+
+    repr_data->attribute_locations = (MVMint32 *)malloc(sizeof(MVMint32) * repr_data->num_attributes);
+    repr_data->struct_offsets      = (MVMint32 *)malloc(sizeof(MVMint32) * repr_data->num_attributes);
+    repr_data->flattened_stables   = (MVMSTable **)malloc(repr_data->num_attributes * sizeof(MVMSTable *));
+    repr_data->member_types        = (MVMObject **)malloc(repr_data->num_attributes * sizeof(MVMObject *));
+
+    for(i = 0; i < repr_data->num_attributes; i++) {
+        repr_data->attribute_locations[i] = reader->read_int(tc, reader);
+        repr_data->struct_offsets[i] = reader->read_int(tc, reader);
+
+        if(reader->read_varint(tc, reader)){
+            repr_data->flattened_stables[i] = reader->read_stable_ref(tc, reader);
+        }
+        else {
+            repr_data->flattened_stables[i] = NULL;
+        }
+
+        repr_data->member_types[i] = reader->read_ref(tc, reader);
+    }
+
+    num_classes = reader->read_int(tc, reader);
+    repr_data->name_to_index_mapping = (MVMCStructNameMap *)malloc(sizeof(MVMCStructNameMap) * (1 + num_classes));
+    for(i = 0; i < num_classes; i++){
+        repr_data->name_to_index_mapping[i].class_key = reader->read_ref(tc, reader);
+        repr_data->name_to_index_mapping[i].name_map = reader->read_ref(tc, reader);
+    }
+    repr_data->name_to_index_mapping[i].class_key = NULL;
+    repr_data->name_to_index_mapping[i].name_map = NULL;
+
+    num_slots = reader->read_int(tc, reader);
+    repr_data->initialize_slots = (MVMint32 *)malloc(sizeof(MVMint32) * (1 + num_slots));
+    for(i = 0; i < num_slots; i++){
+        repr_data->initialize_slots[i] = reader->read_int(tc, reader);
+    }
+    repr_data->initialize_slots[i] = -1;
+
+    st->REPR_data = repr_data;
 }
 
 static void deserialize_stable_size(MVMThreadContext *tc, MVMSTable *st, MVMSerializationReader *reader) {
