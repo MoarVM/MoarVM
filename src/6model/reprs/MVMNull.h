2de460c2854b73a2709d785319aa47f8175c95dd
Jonathan Worthington <jnthn@jnthn.net>
2014-04-29 23:27:25 +0200

    Add static inline func for doing a VM-null check.

--- src/6model/reprs/MVMNull.h
+++ src/6model/reprs/MVMNull.h
@@ -5,3 +5,8 @@ struct MVMNull {
 
 /* Function for REPR setup. */
 const MVMREPROps * MVMNull_initialize(MVMThreadContext *tc);
+
+/* Macro for VM null checks. */
+MVM_STATIC_INLINE MVMint64 MVM_is_null(MVMThreadContext *tc, MVMObject *check) {
+    return !check || check == tc->instance->VMNull;
+}
