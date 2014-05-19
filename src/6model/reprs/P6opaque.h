aab76aa130a58b7d1c277c300b75fc24479c4faa
Nicholas Clark <nick@ccl4.org>
2014-05-19 15:43:09 +0200

    Re-order members of MVMP6opaqueREPRData to avoid alignment holes.
    Saves 4 bytes on ARM and x86, 16 bytes on x86_64.

--- src/6model/reprs/P6opaque.h
+++ src/6model/reprs/P6opaque.h
@@ -41,18 +41,11 @@ struct MVMP6opaqueREPRData {
      * slots can vary in size. */
     MVMuint16 num_attributes;
 
-    /* Maps attribute position numbers to the byte offset in the object. */
-    MVMuint16 *attribute_offsets;
+    /* Slot containing object to delegate for positional things. */
+    MVMint16 pos_del_slot;
 
-    /* If the attribute was actually flattened in to this object from another
-     * representation, this is the s-table of the type of that attribute. NULL
-     * for attributes that are just reference types. */
-    MVMSTable **flattened_stables;
-
-    /* Instantiated objects are just a blank piece of memory that needs to
-     * be set up. However, in some cases we'd like them to magically turn in
-     * to some container type. */
-    MVMObject **auto_viv_values;
+    /* Slot containing object to delegate for associative things. */
+    MVMint16 ass_del_slot;
 
     /* Flags if we are MI or not. */
     MVMuint16 mi;
@@ -66,23 +59,30 @@ struct MVMP6opaqueREPRData {
     /* Slot to delegate to when we need to unbox to a native string. */
     MVMint16 unbox_str_slot;
 
+    /* Offsets into the object that are eligible for GC marking, and how
+     * many of them we have. */
+    MVMuint16 gc_obj_mark_offsets_count;
+    MVMuint16 *gc_obj_mark_offsets;
+
+    /* Maps attribute position numbers to the byte offset in the object. */
+    MVMuint16 *attribute_offsets;
+
+    /* If the attribute was actually flattened in to this object from another
+     * representation, this is the s-table of the type of that attribute. NULL
+     * for attributes that are just reference types. */
+    MVMSTable **flattened_stables;
+
+    /* Instantiated objects are just a blank piece of memory that needs to
+     * be set up. However, in some cases we'd like them to magically turn in
+     * to some container type. */
+    MVMObject **auto_viv_values;
+
     /* If we have any other boxings, this maps repr ID to slot. */
     MVMP6opaqueBoxedTypeMap *unbox_slots;
-
-    /* Slot containing object to delegate for positional things. */
-    MVMint16 pos_del_slot;
-
-    /* Slot containing object to delegate for associative things. */
-    MVMint16 ass_del_slot;
 
     /* A table mapping attribute names to indexes (which can then be looked
      * up in the offset table). Uses a final null entry as a sentinel. */
     MVMP6opaqueNameMap *name_to_index_mapping;
-
-    /* Offsets into the object that are eligible for GC marking, and how
-     * many of them we have. */
-    MVMuint16 *gc_obj_mark_offsets;
-    MVMuint16 gc_obj_mark_offsets_count;
 
     /* Slots holding flattened objects that need another REPR to initialize
      * them; terminated with -1. */
