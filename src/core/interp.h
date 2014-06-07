dc0e7f589fde1922a687d8dcae9502e777af56f5
jnthn <jnthn@jnthn.net>
2014-06-07 12:55:05 +0200

    Correct operand_type_mask.

--- src/core/interp.h
+++ src/core/interp.h
@@ -77,8 +77,8 @@ struct MVMOpInfo {
 #define MVM_operand_type_var    (10 << 3)
 #define MVM_operand_coderef     (12 << 3)
 #define MVM_operand_callsite    (13 << 3)
-#define MVM_operand_type_mask   (15 << 3)
 #define MVM_operand_spesh_slot  (16 << 3)
+#define MVM_operand_type_mask   (31 << 3)
 
 /* Functions. */
 void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContext *, void *), void *invoke_data);
