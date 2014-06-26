42d571bd7407562e9ca935f1e939e51c06cceb3d
Jonathan Worthington <jnthn@jnthn.net>
2014-06-26 13:17:01 +0200

    Start sketching out OSR handling.

--- src/spesh/osr.c
+++ src/spesh/osr.c
@@ -1,5 +1,18 @@
 #include "moar.h"
 
 void MVM_spesh_osr(MVMThreadContext *tc) {
-    /* TODO */
+    MVMSpeshCandidate *specialized;
+
+    /* Ensure that we are in a position to specialize. */
+    if (!tc->cur_frame->caller)
+        return;
+    if (!tc->cur_frame->params.callsite->is_interned)
+        return;
+
+    /* Produce logging spesh candidate. */
+    specialized = MVM_spesh_candidate_setup(tc, tc->cur_frame->static_info,
+        tc->cur_frame->params.callsite, tc->cur_frame->params.args);
+    if (specialized) {
+        /* TODO: install. */
+    }
 }
