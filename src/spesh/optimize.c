c92d1a4cd5340d787ab5f8f42348573f99738c37
Timo Paulssen <timonator@perpetuum-immobile.de>
2014-06-28 21:58:24 +0200

    may save a tiny bit of ram on bytecodes by using const_i64_16

--- src/spesh/optimize.c
+++ src/spesh/optimize.c
@@ -90,11 +90,11 @@ static void optimize_istype(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshIns 
         MVMint32 result;
         if (!MVM_6model_try_cache_type_check(tc, obj_facts->type, type_facts->type, &result))
             return;
-        ins->info = MVM_op_get_op(MVM_OP_const_i64);
+        ins->info = MVM_op_get_op(MVM_OP_const_i64_16);
         result_facts = MVM_spesh_get_facts(tc, g, ins->operands[0]);
         result_facts->flags |= MVM_SPESH_FACT_KNOWN_VALUE;
-        ins->operands[1].lit_i64 = result;
-        result_facts->value.i64  = result;
+        ins->operands[1].lit_i16 = result;
+        result_facts->value.i16  = result;
         obj_facts->usages--;
         type_facts->usages--;
     }
@@ -136,10 +136,10 @@ static void optimize_isconcrete(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpesh
     MVMSpeshFacts *obj_facts = MVM_spesh_get_facts(tc, g, ins->operands[1]);
     if (obj_facts->flags & (MVM_SPESH_FACT_CONCRETE | MVM_SPESH_FACT_TYPEOBJ)) {
         MVMSpeshFacts *result_facts = MVM_spesh_get_facts(tc, g, ins->operands[0]);
-        ins->info                   = MVM_op_get_op(MVM_OP_const_i64);
+        ins->info                   = MVM_op_get_op(MVM_OP_const_i64_16);
         result_facts->flags        |= MVM_SPESH_FACT_KNOWN_VALUE;
-        result_facts->value.i64     = obj_facts->flags & MVM_SPESH_FACT_CONCRETE ? 1 : 0;
-        ins->operands[1].lit_i64    = result_facts->value.i64;
+        result_facts->value.i16     = obj_facts->flags & MVM_SPESH_FACT_CONCRETE ? 1 : 0;
+        ins->operands[1].lit_i16    = result_facts->value.i16;
         obj_facts->usages--;
     }
 }
@@ -298,10 +298,10 @@ static void optimize_can_op(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshBB *
             MVM_spesh_get_facts(tc, g, ins->operands[2])->usages--;
 
         result_facts                = MVM_spesh_get_facts(tc, g, ins->operands[0]);
-        ins->info                   = MVM_op_get_op(MVM_OP_const_i64);
+        ins->info                   = MVM_op_get_op(MVM_OP_const_i64_16);
         result_facts->flags        |= MVM_SPESH_FACT_KNOWN_VALUE;
-        ins->operands[1].lit_i64    = can_result;
-        result_facts->value.i64     = can_result;
+        ins->operands[1].lit_i16    = can_result;
+        result_facts->value.i16     = can_result;
         obj_facts->usages--;
     }
 }
