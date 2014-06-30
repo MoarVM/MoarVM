7a94806134a70b83c2885fd7dc451f3759fc5ea8
jnthn <jnthn@jnthn.net>
2014-06-30 23:06:46 +0200

    Mark a couple more ops noinline.
    They are the kind that the optimizer should be killing off anyway.

--- src/core/ops.c
+++ src/core/ops.c
@@ -5949,7 +5949,7 @@ static MVMOpInfo MVM_op_infos[] = {
         2,
         1,
         0,
-        0,
+        1,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_str }
     },
     {
@@ -5959,7 +5959,7 @@ static MVMOpInfo MVM_op_infos[] = {
         2,
         1,
         0,
-        0,
+        1,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_str }
     },
     {
