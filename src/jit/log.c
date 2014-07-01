2834d7c75f78dca5c836ae699e6fc6e985a28630
Bart Wiegmans <bartwiegmans@gmail.com>
2014-07-01 20:54:31 +0200

    Dump JIT code in separate files
    We use the CUID as well as the name of the static frame
    to determine the name of the file.

--- src/jit/log.c
+++ src/jit/log.c
@@ -10,12 +10,31 @@ void MVM_jit_log(MVMThreadContext *tc, const char * fmt, ...) {
     va_end(args);
 }
 
+static char * jitcode_name(MVMThreadContext *tc, MVMJitCode *code) {
+    MVMuint64 cuuid_len;
+    MVMuint64 name_len;
+    MVMuint8 *cuuid = MVM_string_ascii_encode(tc, code->sf->body.cuuid,
+                                              &cuuid_len);
+    MVMuint8 *name  = MVM_string_ascii_encode(tc, code->sf->body.name,
+                                              &name_len);
+    MVMuint64 dirname_len = strlen(tc->instance->jit_bytecode_dir);
+    // 3 chars for the separators, 4 for the postfix, 1 for the 0
+    char *filename = malloc(dirname_len + name_len + cuuid_len + 8);
+    memcpy(filename, tc->instance->jit_bytecode_dir, dirname_len);
+    filename[dirname_len] = '/';
+    memcpy(filename + dirname_len + 1, cuuid, cuuid_len);
+    filename[dirname_len + cuuid_len + 1] = '.';
+    memcpy(filename + dirname_len + cuuid_len + 2, name, name_len);
+    memcpy(filename + dirname_len + cuuid_len + name_len + 2, ".bin", 5);
+    free(name);
+    free(cuuid);
+    return filename;
+}
+
 void MVM_jit_log_bytecode(MVMThreadContext *tc, MVMJitCode *code) {
-    size_t dirname_length = strlen(tc->instance->jit_bytecode_dir);
-    char * filename = malloc(dirname_length + strlen("/jit-code.bin") + 1);
-    strcpy(filename, tc->instance->jit_bytecode_dir);
-    strcpy(filename + dirname_length, "/jit-code.bin");
+    char * filename = jitcode_name(tc, code);
     FILE * f = fopen(filename, "w");
     fwrite(code->func_ptr, sizeof(char), code->size, f);
     fclose(f);
+    free(filename);
 }
