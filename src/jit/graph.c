34d35ec596aa7bd30132d7abf980333d2146d503
jnthn <jnthn@jnthn.net>
2014-07-29 00:53:27 +0200

    Correct an arg count.

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -634,7 +634,7 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
                                  { MVM_JIT_REG_VAL, invocant },
                                  { MVM_JIT_REG_VAL, key_pos },
                                  { MVM_JIT_REG_VAL, value } };
-        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_VOID, -1);
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 4, args, MVM_JIT_RV_VOID, -1);
         break;
     }
     case MVM_OP_getattr_s: {
