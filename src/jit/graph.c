7483c74f0a6129ea1b91d2fe87da7276167cf4cd
Timo Paulssen <timonator@perpetuum-immobile.de>
2014-07-30 02:17:44 +0200

    implement iscclass

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -169,6 +169,7 @@ static void * op_to_func(MVMThreadContext *tc, MVMint16 opcode) {
     case MVM_OP_eqat_s: return &MVM_string_equal_at;
     case MVM_OP_chars: case MVM_OP_graphs_s: return &MVM_string_graphs;
     case MVM_OP_codes_s: return &MVM_string_codes;
+    case MVM_OP_iscclass: return &MVM_string_is_cclass;
     default:
         MVM_exception_throw_adhoc(tc, "No function for op %d", opcode);
     }
@@ -893,6 +894,18 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
                                  { MVM_JIT_REG_VAL, src } };
         MVMJitRVMode rv_mode = (op == MVM_OP_flip ? MVM_JIT_RV_PTR : MVM_JIT_RV_INT);
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 2, args, rv_mode, dst);
+        break;
+    }
+    case MVM_OP_iscclass: {
+        MVMint16 dst    = ins->operands[0].reg.orig;
+        MVMint16 cclass = ins->operands[1].reg.orig;
+        MVMint16 str    = ins->operands[2].reg.orig;
+        MVMint16 offset = ins->operands[3].reg.orig;
+        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
+                                 { MVM_JIT_REG_VAL, cclass },
+                                 { MVM_JIT_REG_VAL, str },
+                                 { MVM_JIT_REG_VAL, offset } };
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 4, args, MVM_JIT_RV_INT, dst);
         break;
     }
         /* returning */
