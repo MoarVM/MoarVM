2be113164bc158e04bbba89b0ee747767f81a312
jnthn <jnthn@jnthn.net>
2014-08-04 23:33:11 +0200

    Change ext op API to make JITting them easier.
    Now we pass in the memory associated with the current bytecode, so the
    JIT will be able to do the same trick.

--- src/core/interp.c
+++ src/core/interp.c
@@ -4689,7 +4689,7 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 /* Bounds checking? Never heard of that. */
                 MVMFrame *frame_before = tc->cur_frame;
                 MVMExtOpRecord *record = &cu->body.extops[op - MVM_OP_EXT_BASE];
-                record->func(tc);
+                record->func(tc, cur_op);
                 if (tc->cur_frame == frame_before)
                     cur_op += record->operand_bytes;
                 goto NEXT;
@@ -4701,7 +4701,7 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                     MVMFrame *frame_before = tc->cur_frame;
                     MVMExtOpRecord *record =
                             &cu->body.extops[op - MVM_OP_EXT_BASE];
-                    record->func(tc);
+                    record->func(tc, cur_op);
                     if (tc->cur_frame == frame_before)
                         cur_op += record->operand_bytes;
                     goto NEXT;
