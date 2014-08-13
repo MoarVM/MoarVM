22c22e246a6580999a38c70e93c0c8ececede305
jnthn <jnthn@jnthn.net>
2014-08-14 01:16:26 +0200

    JIT coerce_Is and base_I.

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -264,6 +264,7 @@ static void * op_to_func(MVMThreadContext *tc, MVMint16 opcode) {
     case MVM_OP_sub_I: return &MVM_bigint_sub;
     case MVM_OP_mul_I: return &MVM_bigint_mul;
     case MVM_OP_lcm_I: return &MVM_bigint_lcm;
+    case MVM_OP_coerce_Is: case MVM_OP_base_I: return &MVM_bigint_to_str;
     default:
         MVM_exception_throw_adhoc(tc, "No function for op %d", opcode);
     }
@@ -1356,6 +1357,27 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
                                  { MVM_JIT_REG_VAL, src_a },
                                  { MVM_JIT_REG_VAL, src_b } };
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 4, args,
+                          MVM_JIT_RV_PTR, dst);
+        break;
+    }
+    case MVM_OP_coerce_Is: {
+        MVMint16 src = ins->operands[1].reg.orig;
+        MVMint16 dst = ins->operands[0].reg.orig;
+        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
+                                 { MVM_JIT_REG_VAL, src },
+                                 { MVM_JIT_LITERAL, 10 } };
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args,
+                          MVM_JIT_RV_PTR, dst);
+        break;
+    }
+    case MVM_OP_base_I: {
+        MVMint16 src  = ins->operands[1].reg.orig;
+        MVMint16 base = ins->operands[2].reg.orig;
+        MVMint16 dst  = ins->operands[0].reg.orig;
+        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
+                                 { MVM_JIT_REG_VAL, src },
+                                 { MVM_JIT_REG_VAL, base } };
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args,
                           MVM_JIT_RV_PTR, dst);
         break;
     }
