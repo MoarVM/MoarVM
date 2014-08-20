a265d01ae04e75ef1d80dcff7cf180d6db41a16d
Carlin <cb@viennan.net>
2014-08-21 04:45:53 +1200

    Add more signal handlers

--- src/io/signals.c
+++ src/io/signals.c
@@ -68,12 +68,107 @@ MVMObject * MVM_io_signal_handle(MVMThreadContext *tc, MVMObject *queue,
 
     /* Transform the signal number. */
     switch (signal) {
+    case MVM_SIG_HUP:       signum = SIGHUP;    break;
     case MVM_SIG_INT:       signum = SIGINT;    break;
+#ifdef SIGQUIT
+    case MVM_SIG_QUIT:      signum = SIGQUIT;   break;
+#endif
+#ifdef SIGILL
+    case MVM_SIG_ILL:       signum = SIGILL;    break;
+#endif
+#ifdef SIGTRAP
+    case MVM_SIG_TRAP:      signum = SIGTRAP;   break;
+#endif
+#ifdef SIGABRT
+    case MVM_SIG_ABRT:      signum = SIGABRT;   break;
+#endif
+#ifdef SIGEMT
+    case MVM_SIG_EMT:       signum = SIGEMT;    break;
+#endif
+#ifdef SIGFPE
+    case MVM_SIG_FPE:       signum = SIGFPE;    break;
+#endif
+#ifdef SIGKILL
+    case MVM_SIG_KILL:      signum = SIGKILL;   break;
+#endif
+#ifdef SIGBUS
+    case MVM_SIG_BUS:       signum = SIGBUS;    break;
+#endif
+#ifdef SIGSEGV
+    case MVM_SIG_SEGV:      signum = SIGSEGV;   break;
+#endif
+#ifdef SIGSYS
+    case MVM_SIG_SYS:       signum = SIGSYS;    break;
+#endif
+#ifdef SIGPIPE
+    case MVM_SIG_PIPE:      signum = SIGPIPE;   break;
+#endif
+#ifdef SIGALRM
+    case MVM_SIG_ALRM:      signum = SIGALRM;   break;
+#endif
+#ifdef SIGTERM
+    case MVM_SIG_TERM:      signum = SIGTERM;   break;
+#endif
+#ifdef SIGURG
+    case MVM_SIG_URG:       signum = SIGURG;    break;
+#endif
+#ifdef SIGSTOP
+    case MVM_SIG_STOP:      signum = SIGSTOP;   break; /* hammer time */
+#endif
+#ifdef SIGTSTP
+    case MVM_SIG_TSTP:      signum = SIGTSTP;   break;
+#endif
+#ifdef SIGCONT
+    case MVM_SIG_CONT:      signum = SIGCONT;   break;
+#endif
+#ifdef SIGCHLD
+    case MVM_SIG_CHLD:      signum = SIGCHLD;   break;
+#endif
+#ifdef SIGTTIN
+    case MVM_SIG_TTIN:      signum = SIGTTIN;   break;
+#endif
+#ifdef SIGTTOU
+    case MVM_SIG_TTOU:      signum = SIGTTOU;   break;
+#endif
+#ifdef SIGIO
+    case MVM_SIG_IO:        signum = SIGIO;     break;
+#endif
+#ifdef SIGXCPU
+    case MVM_SIG_XCPU:      signum = SIGXCPU;   break;
+#endif
+#ifdef SIGXFSZ
+    case MVM_SIG_XFSZ:      signum = SIGXFSZ;   break;
+#endif
+#ifdef SIGVTALRM
+    case MVM_SIG_VTALRM:    signum = SIGVTALRM; break;
+#endif
+#ifdef SIGPROF
+    case MVM_SIG_PROF:      signum = SIGPROF;   break;
+#endif
+#ifdef SIGWINCH
+    case MVM_SIG_WINCH:     signum = SIGWINCH;  break;
+#endif
+#ifdef SIGINFO
+    case MVM_SIG_INFO:      signum = SIGINFO;   break;
+#endif
+#ifdef SIGUSR1
+    case MVM_SIG_USR1:      signum = SIGUSR1;   break;
+#endif
+#ifdef SIGUSR2
+    case MVM_SIG_USR2:      signum = SIGUSR2;   break;
+#endif
+#ifdef SIGTHR
+    case MVM_SIG_THR:       signum = SIGTHR;    break;
+#endif
+#ifdef SIGSTKFLT
+    case MVM_SIG_STKFLT:    signum = SIGSTKFLT; break;
+#endif
+#ifdef SIGPWR
+    case MVM_SIG_PWR:       signum = SIGPWR;    break;
+#endif
 #ifdef SIGBREAK
     case MVM_SIG_BREAK:     signum = SIGBREAK;  break;
 #endif
-    case MVM_SIG_HUP:       signum = SIGHUP;    break;
-    case MVM_SIG_WINCH:     signum = SIGWINCH;  break;
     default:
         MVM_exception_throw_adhoc(tc, "Unsupported signal handler %d",
             (int)signal);
