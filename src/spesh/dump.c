2966c01cc12d76901bb928e3389547fa23097a1e
Bart Wiegmans <bartwiegmans@gmail.com>
2014-08-25 22:53:39 +0200

    Don't leak memory when dumping the filename info

--- src/spesh/dump.c
+++ src/spesh/dump.c
@@ -203,15 +203,15 @@ static void dump_fileinfo(MVMThreadContext *tc, DumpStr *ds, MVMSpeshGraph *g) {
     MVMint32           str_idx = ann ? ann->filename_string_heap_index : 0;
     MVMint32           line_nr = ann ? ann->line_number : 1;
     MVMString        *filename = cu->body.filename;
-    MVMuint8    *filename_utf8;
+    MVMuint8    *filename_utf8 = "<unknown>";
     if (ann && str_idx < cu->body.num_strings) {
         filename = cu->body.strings[str_idx];
     }
     if (filename)
         filename_utf8 = MVM_string_utf8_encode(tc, filename, NULL);
-    else
-        filename_utf8 = strdup("<unknown>");
     appendf(ds, "%s:%d", filename_utf8, line_nr);
+    if (filename)
+        free(filename_utf8);
 }
 
 /* Dump a spesh graph into string form, for debugging purposes. */
