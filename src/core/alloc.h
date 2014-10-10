a1ab118178d7e34c4bde9dad0edd199bb40890b4
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-10-10 15:27:12 +0800

    Add src/core/alloc.h to Makefile and fix build

--- src/core/alloc.h
+++ src/core/alloc.h
@@ -1,26 +1,26 @@
-MVM_STATIC_INLINE void * MVM_malloc(size_t len) {
-    void *ptr = malloc(len);
+MVM_STATIC_INLINE void * MVM_malloc(size_t size) {
+    void *ptr = malloc(size);
 
     if (!ptr)
-        MVM_panic_allocation_failed(len);
+        MVM_panic_allocation_failed(size);
 
     return ptr;
 }
 
-MVM_STATIC_INLINE void * MVM_calloc(size_t len) {
-    void *ptr = calloc(len);
+MVM_STATIC_INLINE void * MVM_calloc(size_t num, size_t size) {
+    void *ptr = calloc(num, size);
 
     if (!ptr)
-        MVM_panic_allocation_failed(len);
+        MVM_panic_allocation_failed(size);
 
     return ptr;
 }
 
-MVM_STATIC_INLINE void * MVM_realloc(void *p, size_t len) {
-    void *ptr = realloc(p, len);
+MVM_STATIC_INLINE void * MVM_realloc(void *p, size_t size) {
+    void *ptr = realloc(p, size);
 
-    if (!ptr && len > 0)
-        MVM_panic_allocation_failed(len);
+    if (!ptr && size > 0)
+        MVM_panic_allocation_failed(size);
 
     return ptr;
 }
