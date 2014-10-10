4a91df93e9a40c669c10fc0a251b0c090d441677
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-10-10 15:17:55 +0800

    Added MVM_calloc function

--- src/core/alloc.h
+++ src/core/alloc.h
@@ -1,20 +1,28 @@
 MVM_STATIC_INLINE void * MVM_malloc(size_t len) {
-    void *p;
-    p = malloc(len);
-    if (!p)
+    void *ptr = malloc(len);
+
+    if (!ptr)
         MVM_panic_allocation_failed(len);
 
-    return p;
+    return ptr;
+}
+
+MVM_STATIC_INLINE void * MVM_calloc(size_t len) {
+    void *ptr = calloc(len);
+
+    if (!ptr)
+        MVM_panic_allocation_failed(len);
+
+    return ptr;
 }
 
 MVM_STATIC_INLINE void * MVM_realloc(void *p, size_t len) {
-    void *n;
-    n = realloc(p, len);
+    void *ptr = realloc(p, len);
 
-    if (!n && len > 0)
+    if (!ptr && len > 0)
         MVM_panic_allocation_failed(len);
 
-    return n;
+    return ptr;
 }
 
 MVM_STATIC_INLINE void MVM_free(void *p) {
