7fd90e75696de09340f77bacfab49a171c1fffeb
Timo Paulssen <timonator@perpetuum-immobile.de>
2014-10-18 15:25:11 +0200

    multicachefind for the jit
    needs invokewithcapture in order to actually work.

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -187,6 +187,7 @@ static void * op_to_func(MVMThreadContext *tc, MVMint16 opcode) {
     case MVM_OP_newlexotic: return &MVM_exception_newlexotic_from_jit;
     case MVM_OP_usecapture: return &MVM_args_use_capture;
     case MVM_OP_savecapture: return &MVM_args_save_capture;
+    case MVM_OP_multicachefind: return &MVM_multi_cache_find;
     case MVM_OP_return: return &MVM_args_assert_void_return_ok;
     case MVM_OP_return_i: return &MVM_args_set_result_int;
     case MVM_OP_return_s: return &MVM_args_set_result_str;
@@ -942,6 +943,16 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
         MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
                                  { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_FRAME }};
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 2, args, MVM_JIT_RV_PTR, dst);
+        break;
+    }
+    case MVM_OP_multicachefind: {
+        MVMint16 dst     = ins->operands[0].reg.orig;
+        MVMint16 cache   = ins->operands[1].reg.orig;
+        MVMint16 capture = ins->operands[2].reg.orig;
+        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
+                                 { MVM_JIT_REG_VAL, cache },
+                                 { MVM_JIT_REG_VAL, capture } };
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_PTR, dst);
         break;
     }
     case MVM_OP_flattenropes: {
