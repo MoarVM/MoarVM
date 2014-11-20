c8b0b979fab9d99447467ced83dcc37ef19cdecd
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-11-20 23:46:15 +0800

    remove an unused semicolon

--- src/gc/debug.h
+++ src/gc/debug.h
@@ -2,7 +2,7 @@
  * debugging. */
 
 #define MVM_ASSERT_NOT_FROMSPACE(tc, c) do { \
-        if ((char *)(c) >= (char *)tc->nursery_fromspace && \
-                (char *)(c) < (char *)tc->nursery_fromspace + MVM_NURSERY_SIZE) \
-            MVM_exception_throw_adhoc(tc, "Collectable in fromspace accessed"); \
-    } while (0);
+    if ((char *)(c) >= (char *)tc->nursery_fromspace && \
+            (char *)(c) < (char *)tc->nursery_fromspace + MVM_NURSERY_SIZE) \
+        MVM_exception_throw_adhoc(tc, "Collectable in fromspace accessed"); \
+} while (0)
