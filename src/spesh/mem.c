9bd5a7e56bbd4830449b561fff6c2e8ed08e3893
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-12-23 15:33:19 +0800

    Start Alias analysis for array and hash access

--- src/spesh/mem.c
+++ src/spesh/mem.c
@@ -1,5 +1,9 @@
 #include "moar.h"
 
+/** NOTE:
+  * these property are from luajit(Including the API)  see: http://wiki.luajit.org/Optimizations
+  * */
+
 /* alias case . */
 typedef enum {
   ALIAS_NO,	
@@ -9,7 +13,7 @@ typedef enum {
 
 static AliasResult aa_escape(MVMSpeshGraph *g, MVMSpeshIns *ins, MVMSpeshIns *stop) {
     MVMint64 operand = ins->operands[0].lit_i64; /* The operand that might be stored. */
-    while(ins = ins->next) {
+    while(ins = ins->next && ins != stop) {
         if(ins->info->esc) {
             MVMuint8 num_operands = ins->info->num_operands;
             MVMSpeshOperand *operands = ins->operands;
@@ -25,22 +29,35 @@ static AliasResult aa_escape(MVMSpeshGraph *g, MVMSpeshIns *ins, MVMSpeshIns *st
 
 /* Alias analysis for two different object operand. */
 static AliasResult aa_object(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshOperand oa, MVMSpeshOperand ob) {
-    MVMSpeshIns *ins_a = MVM_spesh_get_facts(tc, g, oa)->writer;
-    MVMSpeshIns *ins_b = MVM_spesh_get_facts(tc, g, ob)->writer;
+    MVMSpeshIns *insa = MVM_spesh_get_facts(tc, g, oa)->writer;
+    MVMSpeshIns *insb = MVM_spesh_get_facts(tc, g, ob)->writer;
 
-    int allocates_a = ins_a->info->allocates;
-    int allocates_b = ins_b->info->allocates;
+    int allocates_a = insa->info->allocates;
+    int allocates_b = insb->info->allocates;
     if(allocates_a && allocates_b) {
-            return ALIAS_NO;  /* Two different allocations never alias. */
+        return ALIAS_NO;  /* Two different allocations never alias. */
     }
 
     if(allocates_b) {              /* At least one allocation? */
-        MVMSpeshIns *tmp = ins_a;
-        ins_a = ins_b;
-        ins_b = tmp;
+        MVMSpeshIns *tmp = insa;
+        insa = insb;
+        insb = tmp;
     } else if(!allocates_a) {
         return ALIAS_MAY;  /* Anything else, we just don't know. */
     }
 
-    return aa_escape(g, ins_a, ins_b);
+    return aa_escape(g, insa, insb);
+}
+
+/* Alias analysis for array and hash access using key-based disambiguation. */
+static AliasResult aa_ahref(MVMSpeshGraph *g, MVMSpeshIns *insa, MVMSpeshIns *insb) {
+    MVMSpeshOperand oa = insa->operands[2];
+    MVMSpeshOperand ob = insb->operands[2];
+    MVMSpeshIns *keya, *keyb;
+    
+    if (insa == insb)
+        return ALIAS_MUST;  /* Shortcut for same refs. */
+
+    keya = MVM_spesh_get_facts(tc, g, oa)->writer;
+    keyb = MVM_spesh_get_facts(tc, g, ob)->writer;
 }
