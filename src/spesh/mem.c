ee8ad966060d594f3729e98a6873cdc02d557cbc
Jimmy Zhuo <zhuomingliang01@qq.com>
2014-12-23 16:00:16 +0800

    small fixes to aa_escape

--- src/spesh/mem.c
+++ src/spesh/mem.c
@@ -14,12 +14,13 @@ typedef enum {
 static AliasResult aa_escape(MVMSpeshGraph *g, MVMSpeshIns *ins, MVMSpeshIns *stop) {
     MVMint64 operand = ins->operands[0].lit_i64; /* The operand that might be stored. */
     while(ins = ins->next && ins != stop) {
-        if(ins->info->esc) {
+        MVMuint8 esc = ins->info->esc;
+        if(esc) {
             MVMuint8 num_operands = ins->info->num_operands;
             MVMSpeshOperand *operands = ins->operands;
             MVMuint8 i;
             for(i =0; i < num_operands; i++) {
-                if(operands[i].lit_i64 == operand)
+                if((esc[i] == MVM_ESCAPE_YES || (esc[i] & MVM_ESCAPE_KIND) == MVM_ESCAPE_INTO) && operands[i].lit_i64 == operand)
                     return ALIAS_MAY; /* operand was stored and might alias. */
             }
         }
