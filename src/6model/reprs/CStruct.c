604b2be67057a755cd5831f2122f1c46ae865209
sergot <filip@sergot.pl>
2015-02-25 09:55:34 +0100

    fix allocating an empty (no members) CStruct bug
    it segfaults when we try to allocate memory for an empty CStruct, this fix prevents from allocating memory of size -1

--- src/6model/reprs/CStruct.c
+++ src/6model/reprs/CStruct.c
@@ -314,8 +314,7 @@ static void initialize(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, voi
 
     /* Allocate object body. */
     MVMCStructBody *body = (MVMCStructBody *)data;
-    body->cstruct = MVM_malloc(repr_data->struct_size > 0 ? repr_data->struct_size : 1);
-    memset(body->cstruct, 0, repr_data->struct_size);
+    body->cstruct = MVM_calloc(1, repr_data->struct_size > 0 ? repr_data->struct_size : 1);
 
     /* Allocate child obj array. */
     if (repr_data->num_child_objs > 0)
