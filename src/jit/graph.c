8aa8924ad64bb8c4a723ac273102ff7bc5d84e74
jnthn <jnthn@jnthn.net>
2015-03-01 12:15:02 +0100

    JIT decont_[ins].

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -312,6 +312,9 @@ static void * op_to_func(MVMThreadContext *tc, MVMint16 opcode) {
     case MVM_OP_assign_i: return &MVM_6model_container_assign_i;
     case MVM_OP_assign_n: return &MVM_6model_container_assign_n;
     case MVM_OP_assign_s: return &MVM_6model_container_assign_s;
+    case MVM_OP_decont_i: return &MVM_6model_container_decont_i;
+    case MVM_OP_decont_n: return &MVM_6model_container_decont_n;
+    case MVM_OP_decont_s: return &MVM_6model_container_decont_s;
     case MVM_OP_getlexref_i: return &MVM_nativeref_lex_i;
     case MVM_OP_getlexref_n: return &MVM_nativeref_lex_n;
     case MVM_OP_getlexref_s: return &MVM_nativeref_lex_s;
@@ -1700,6 +1703,17 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
         MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
                                  { MVM_JIT_REG_VAL, target },
                                  { MVM_JIT_REG_VAL, value } };
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_VOID, -1);
+        break;
+    }
+    case MVM_OP_decont_i:
+    case MVM_OP_decont_n:
+    case MVM_OP_decont_s: {
+        MVMint16 dst = ins->operands[0].reg.orig;
+        MVMint16 obj = ins->operands[1].reg.orig;
+        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, MVM_JIT_INTERP_TC },
+                                 { MVM_JIT_REG_VAL, obj },
+                                 { MVM_JIT_REG_ADDR, dst } };
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_VOID, -1);
         break;
     }
