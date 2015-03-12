c6e8df8103ba58f26df79c0b0fff6d937008d54f
jnthn <jnthn@jnthn.net>
2015-03-12 22:32:13 +0100

    Further fixes to repossession.
    Now we update the STable, we must take care to repossess all STables
    *before* any of the objects, otherwise we end up with a huge mess of
    re-deserialized STables. Additionally, we update the SC of things we
    repossess at deserialization time.

--- src/6model/serialization.c
+++ src/6model/serialization.c
@@ -2269,25 +2269,27 @@ void MVM_serialization_finish_deserialize_method_cache(MVMThreadContext *tc, MVM
     }
 }
 
-/* Repossess an object or STable. */
+/* Repossess an object or STable. Ignores those not matching the specified
+ * type (where 0 = object, 1 = STable). */
 static void repossess(MVMThreadContext *tc, MVMSerializationReader *reader, MVMint64 i,
-                      MVMObject *repo_conflicts) {
+                      MVMObject *repo_conflicts, MVMint32 type) {
+    MVMuint32 slot;
+
     /* Calculate location of table row. */
     char *table_row = reader->root.repos_table + i * REPOS_TABLE_ENTRY_SIZE;
 
-    /* Do appropriate type of repossession. */
+    /* Do appropriate type of repossession, provided it matches the type of
+     * thing we're current repossessing. */
     MVMint32 repo_type = read_int32(table_row, 0);
+    if (repo_type != type)
+        return;
     if (repo_type == 0) {
-        char *obj_table_row;
+        char      *obj_table_row;
         MVMSTable *updated_st;
 
         /* Get object to repossess. */
         MVMSerializationContext *orig_sc = locate_sc(tc, reader, read_int32(table_row, 8));
         MVMObject *orig_obj = MVM_sc_get_object(tc, orig_sc, read_int32(table_row, 12));
-
-        /* Put it into objects root set at the apporpriate slot. */
-        MVMuint32 slot = read_int32(table_row, 4);
-        MVM_sc_set_object(tc, reader->root.sc, slot, orig_obj);
 
         /* If we have a reposession conflict, make a copy of the original object
          * and reference it from the conflicts list. Push the original (about to
@@ -2309,6 +2311,11 @@ static void repossess(MVMThreadContext *tc, MVMSerializationReader *reader, MVMi
                 MVM_repr_push_o(tc, repo_conflicts, orig_obj);
             });
         }
+
+        /* Put it into objects root set at the apporpriate slot. */
+        slot = read_int32(table_row, 4);
+        MVM_sc_set_object(tc, reader->root.sc, slot, orig_obj);
+        MVM_sc_set_obj_sc(tc, orig_obj, reader->root.sc);
 
         /* Clear it up, since we'll re-allocate all the bits inside
          * it on deserialization. */
@@ -2332,15 +2339,16 @@ static void repossess(MVMThreadContext *tc, MVMSerializationReader *reader, MVMi
         MVMSerializationContext *orig_sc = locate_sc(tc, reader, read_int32(table_row, 8));
         MVMSTable *orig_st = MVM_sc_get_stable(tc, orig_sc, read_int32(table_row, 12));
 
-        /* Put it into STables root set at the apporpriate slot. */
-        MVMuint32 slot = read_int32(table_row, 4);
-        MVM_sc_set_stable(tc, reader->root.sc, slot, orig_st);
-
         /* Make sure we don't have a reposession conflict. */
         if (MVM_sc_get_stable_sc(tc, orig_st) != orig_sc)
             fail_deserialize(tc, reader,
                 "STable conflict detected during deserialization.\n"
                 "(Probable attempt to load two modules that cannot be loaded together).");
+
+        /* Put it into STables root set at the apporpriate slot. */
+        slot = read_int32(table_row, 4);
+        MVM_sc_set_stable(tc, reader->root.sc, slot, orig_st);
+        MVM_sc_set_stable_sc(tc, orig_st, reader->root.sc);
 
         /* XXX TODO: consider clearing up STable, however we must do it out of
          * this repossess routine, since we may depend on original data to do
@@ -2419,10 +2427,14 @@ void MVM_serialization_deserialize(MVMThreadContext *tc, MVMSerializationContext
         codes_static, OBJECT_BODY(codes_static),
         scodes + reader->root.num_closures);
 
-    /* If we're repossessing objects and STables from other SCs, then first
-      * get those raw objects into our root set. */
+    /* If we're repossessing STables and objects from other SCs, then first
+      * get those raw objects into our root set. Note we do all the STables,
+      * then all the objects, since the objects may, post-repossession, refer
+      * to a repossessed STable. */
      for (i = 0; i < reader->root.num_repos; i++)
-        repossess(tc, reader, i, repo_conflicts);
+        repossess(tc, reader, i, repo_conflicts, 1);
+     for (i = 0; i < reader->root.num_repos; i++)
+        repossess(tc, reader, i, repo_conflicts, 0);
 
     /* Enter the work loop to deal with the things we immediately need to
      * handle in order to complete repossession object deserialization. */
