72d227135002b51d9d619e4fccdf841fc6730982
Francois Perrad <francois.perrad@gadz.org>
2015-03-27 21:11:53 +0100

    remove duplicated prototype
    the prototype MVMint32 MVM_jit_support() is already in moar.h

--- src/jit/emit.h
+++ src/jit/emit.h
@@ -1,5 +1,4 @@
 /* Declarations for architecture-specific codegen stuff */
-MVM_PUBLIC const MVMint32 MVM_jit_support(void);
 const unsigned char * MVM_jit_actions(void);
 const unsigned int MVM_jit_num_globals(void);
 void MVM_jit_emit_prologue(MVMThreadContext *tc, MVMJitGraph *jg,
