060346c28d64f57bcf09e2a31f4f008ccc1080fd
skids <bri@abrij.org>
2015-04-23 15:00:06 -0400

    Add missing C prototype.

--- src/strings/windows1252.h
+++ src/strings/windows1252.h
@@ -1,2 +1,3 @@
 MVMString * MVM_string_windows1252_decode(MVMThreadContext *tc, MVMObject *result_type, char *windows1252, size_t bytes);
+MVM_PUBLIC void MVM_string_windows1252_decodestream(MVMThreadContext *tc, MVMDecodeStream *ds, MVMint32 *stopper_chars, MVMint32 *stopper_sep);
 char * MVM_string_windows1252_encode_substr(MVMThreadContext *tc, MVMString *str, MVMuint64 *output_size, MVMint64 start, MVMint64 length);
