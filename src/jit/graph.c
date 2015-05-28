de4592775c472c9442ebcb5089710fb8a15a7c80
Bart Wiegmans <b.wiegmans@peperzaken.nl>
2015-05-28 11:20:14 +0200

    Support NFG synthetics in ordat/ordfirst for JIT
    ordat and ordfirst want to return the base of an NFG synthetic,
    so *if* the character is a synthetic, it should be converted. This
    patch does just that, at the cost of converting the c-call to
    a custom piece of amd64 asm. This will be fixed soon, I hope.

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -292,8 +292,6 @@ static void * op_to_func(MVMThreadContext *tc, MVMint16 opcode) {
     case MVM_OP_lc: return &MVM_string_lc;
     case MVM_OP_eq_s: return &MVM_string_equal;
     case MVM_OP_eqat_s: return &MVM_string_equal_at;
-    case MVM_OP_ordat: return &MVM_string_get_grapheme_at;
-    case MVM_OP_ordfirst: return &MVM_string_get_grapheme_at;
     case MVM_OP_chars: case MVM_OP_graphs_s: return &MVM_string_graphs;
     case MVM_OP_codes_s: return &MVM_string_codes;
     case MVM_OP_index_s: return &MVM_string_index;
@@ -1447,10 +1445,13 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
     case MVM_OP_paramnamesused:
     case MVM_OP_assertparamcheck:
     case MVM_OP_getobjsc:
+    case MVM_OP_ordat:
+    case MVM_OP_ordfirst:
         /* Profiling */
     case MVM_OP_prof_enterspesh:
     case MVM_OP_prof_enterinline:
     case MVM_OP_invokewithcapture:
+
         jgb_append_primitive(tc, jgb, ins);
         break;
         /* branches */
@@ -1931,18 +1932,6 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
                                  { MVM_JIT_REG_VAL, { offset } } };
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 4, args,
                           MVM_JIT_RV_INT, dst);
-        break;
-    }
-    case MVM_OP_ordat:
-    case MVM_OP_ordfirst: {
-        // XXX these two operators have to check for negative numbers and use MVM_nfg_get_synthetic_info(tc, g)->base.
-        MVMint16 dst    = ins->operands[0].reg.orig;
-        MVMint16 src    = ins->operands[1].reg.orig;
-        MVMint16 offset = op == MVM_OP_ordat ? ins->operands[2].reg.orig : 0;
-        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, { MVM_JIT_INTERP_TC } },
-                                 { MVM_JIT_REG_VAL, { src } },
-                                 { op == MVM_OP_ordat ? MVM_JIT_REG_VAL : MVM_JIT_LITERAL, { offset } } };
-        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_INT, dst);
         break;
     }
     case MVM_OP_chars:
