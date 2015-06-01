9da66bcc470a6798fc31ebeda8cb77d10a3fb6e0
Timo Paulssen <timonator@perpetuum-immobile.de>
2015-06-01 21:14:13 +0200

    sacrifice 15 for -1 in the int cache. -1 is very popular!

--- src/core/intcache.c
+++ src/core/intcache.c
@@ -16,12 +16,12 @@ void MVM_intcache_for(MVMThreadContext *tc, MVMObject *type) {
     }
     if (right_slot != -1) {
         int val;
-        for (val = 0; val < 16; val++) {
+        for (val = -1; val < 15; val++) {
             MVMObject *obj;
             obj = MVM_repr_alloc_init(tc, type);
             MVM_repr_set_int(tc, obj, val);
-            tc->instance->int_const_cache->cache[type_index][val] = obj;
-            MVM_gc_root_add_permanent(tc, (MVMCollectable **)&tc->instance->int_const_cache->cache[type_index][val]);
+            tc->instance->int_const_cache->cache[type_index][val + 1] = obj;
+            MVM_gc_root_add_permanent(tc, (MVMCollectable **)&tc->instance->int_const_cache->cache[type_index][val + 1]);
         }
         tc->instance->int_const_cache->types[type_index] = type;
         MVM_gc_root_add_permanent(tc, (MVMCollectable **)&tc->instance->int_const_cache->types[type_index]);
@@ -33,7 +33,7 @@ MVMObject *MVM_intcache_get(MVMThreadContext *tc, MVMObject *type, MVMint64 valu
     int type_index;
     int right_slot = -1;
 
-    if (value < 0 || value >= 16)
+    if (value < -1 || value >= 15)
         return NULL;
 
     for (type_index = 0; type_index < 4; type_index++) {
@@ -43,7 +43,7 @@ MVMObject *MVM_intcache_get(MVMThreadContext *tc, MVMObject *type, MVMint64 valu
         }
     }
     if (right_slot != -1) {
-        return tc->instance->int_const_cache->cache[right_slot][value];
+        return tc->instance->int_const_cache->cache[right_slot][value + 1];
     }
     return NULL;
 }
