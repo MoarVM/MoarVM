8a6a6c75111412b841906f4c47c41ca77f57094d
Bart Wiegmans <bartwiegmans@gmail.com>
2015-07-03 21:58:25 +0200

    Add roots and end-of-block stores
    Roots are the terminals of the tree. The code generator should
    evaluate each root in order. At the end of a block, values are
    spilled to memory (that is, until we implement register
    allocation). These spills also become roots. This should
    ensure that the whole basic block is eventually compiled.

--- src/jit/expr.c
+++ src/jit/expr.c
@@ -4,48 +4,65 @@
 
 typedef struct {
     MVMJitGraph    *graph;
-    MVMJitExprNode *buffer;
+    MVMJitExprNode *nodes;
     MVMint32       *computed;
     MVMint32       *roots;
-    MVMint32        top;
+    MVMint32        num;
     MVMint32        alloc;
-    MVMint32        roots_top;
+    MVMint32        roots_num;
     MVMint32        roots_alloc;
 }  MVMJitTreeBuilder;
 
 static inline void builder_make_space(MVMJitTreeBuilder *builder, MVMint32 space) {
-    if (builder->top + space >= builder->alloc) {
+    if (builder->num + space >= builder->alloc) {
         builder->alloc *= 2;
-        builder->buffer = MVM_realloc(builder, builder->alloc*sizeof(MVMJitExprNode));
+        builder->nodes = MVM_realloc(builder->nodes, builder->alloc*sizeof(MVMJitExprNode));
     }
 }
 
 static inline void builder_append_direct(MVMJitTreeBuilder *builder, MVMJitExprNode *template,
                                          MVMint32 len) {
     builder_make_space(builder, len);
-    memcpy(&builder->buffer[builder->top], template, len*sizeof(MVMJitExprNode));
-    builder->top += len;
+    memcpy(&builder->nodes[builder->num], template, len*sizeof(MVMJitExprNode));
+    builder->num += len;
 }
 
-static inline void builder_add_root(MVMJitTreeBuilder *builder, MVMint32 root) {
-    /* NYI - this should ensure that the roots of our tree are kept in order */
+static inline void builder_add_root(MVMJitTreeBuilder *builder, MVMint32 node) {
+    if (builder->roots_num + 1 >= builder->roots_alloc) {
+        builder->roots_alloc *= 2;
+        builder->roots = MVM_realloc(builder->roots, builder->roots_alloc*sizeof(MVMint32));
+    }
+    builder->roots[builder->roots_num++] = node;
 }
 
-static MVMint32 MVM_jit_expr_add_loadreg(MVMThreadContext *tc, MVMJitTreeBuilder *builder, MVMuint16 reg) {
-    MVMJitExprNode template[] = { MVM_JIT_ADDR, MVM_JIT_LOCAL, reg, MVM_JIT_LOAD, builder->top, sizeof(MVMJitExprNode) };
-    MVMint32 top        = builder->top;
+static MVMint32 MVM_jit_expr_add_loadreg(MVMThreadContext *tc, MVMJitTreeBuilder *builder,
+                                         MVMuint16 reg) {
+    MVMJitExprNode template[] = { MVM_JIT_ADDR, MVM_JIT_LOCAL, reg,
+                                  MVM_JIT_LOAD, builder->num, sizeof(MVMJitExprNode) };
+    MVMint32 num        = builder->num;
     builder_append_direct(builder, template, sizeof(template)/sizeof(MVMJitExprNode));
-    return top + 3;
+    return num + 3;
 }
 
-static MVMint32 MVM_jit_expr_add_const(MVMThreadContext *tc, MVMJitTreeBuilder *builder, MVMSpeshOperand opr, MVMuint8 info) {
+
+static MVMint32 MVM_jit_expr_add_storereg(MVMThreadContext *tc, MVMJitTreeBuilder *builder,
+                                          MVMint32 node, MVMuint16 reg) {
+    MVMJitExprNode template[] = { MVM_JIT_ADDR, MVM_JIT_LOCAL, reg,
+                                  MVM_JIT_STORE, node, builder->num, sizeof(MVMRegister) };
+    MVMint32 num = builder->num;
+    builder_append_direct(builder, template, sizeof(template)/sizeof(MVMJitExprNode));
+    return num + 3;
+}
+
+
+static MVMint32 MVM_jit_expr_add_const(MVMThreadContext *tc, MVMJitTreeBuilder *builder,
+                                       MVMSpeshOperand opr, MVMuint8 info) {
     /* TODO implement this properly; this only works correctly for 64 bit values */
     MVMJitExprNode template[]  = { MVM_JIT_CONST, opr.lit_i64, sizeof(MVMint64) };
-    MVMint32 top               = builder->top;
+    MVMint32 num               = builder->num;
     builder_append_direct(builder, template, sizeof(template)/sizeof(MVMJitExprNode));
-    return top;
+    return num;
 }
-
 
 void MVM_jit_expr_load_operands(MVMThreadContext *tc, MVMJitTreeBuilder *builder,
                                 MVMSpeshIns *ins, MVMint32 *operands) {
@@ -67,49 +84,55 @@ void MVM_jit_expr_load_operands(MVMThreadContext *tc, MVMJitTreeBuilder *builder
     }
 }
 
-/* Add template to buffer, filling in operands and linking tree nodes. Return template root */
+/* Add template to nodes, filling in operands and linking tree nodes. Return template root */
 MVMint32 MVM_jit_expr_apply_template(MVMThreadContext *tc, MVMJitTreeBuilder *builder,
                                      const MVMJitExprTemplate *template, MVMint32 *operands) {
-    int i, top;
-    top = builder->top;
+    int i, num;
+    num = builder->num;
     builder_make_space(builder, template->len);
     /* Loop over string until the end */
     for (i = 0; template->info[i]; i++) {
         switch (template->info[i]) {
         case 'l':
-            /* link template-relative to buffer-relative */
-            builder->buffer[top+i] = template->code[i] + top;
+            /* link template-relative to nodes-relative */
+            builder->nodes[num+i] = template->code[i] + num;
             break;
         case 'f':
-            /* add operand node into the buffer */
-            builder->buffer[top+i] = operands[template->code[i]];
+            /* add operand node into the nodes */
+            builder->nodes[num+i] = operands[template->code[i]];
             break;
         default:
-            /* copy from template to buffer */
-            builder->buffer[top+i] = template->code[i];
+            /* copy from template to nodes */
+            builder->nodes[num+i] = template->code[i];
             break;
         }
     }
-    builder->top = top + template->len;
-    return top + template->root; /* root relative to buffer */
+    builder->num = num + template->len;
+    return num + template->root; /* root relative to nodes */
 }
 
 /* TODO add labels to the expression tree */
 MVMJitExprTree * MVM_jit_build_expression_tree(MVMThreadContext *tc, MVMJitGraph *jg,
                                                MVMSpeshBB *bb) {
     MVMint32 operands[MVM_MAX_OPERANDS];
-    MVMint32 last_root;
+    MVMint32 root;
     MVMJitTreeBuilder builder;
     MVMJitExprTree *tree = NULL;
     MVMSpeshGraph *sg = jg->sg;
     MVMSpeshIns *ins = bb->first_ins;
+    MVMuint16 i;
+    /* Hold the tree */
+    builder.num      = 0;
+    builder.alloc    = 64;
+    builder.nodes   = MVM_malloc(sizeof(MVMJitExprNode)*builder.alloc);
+    /* Hold it's roots (terminal statements) */
+    builder.roots_alloc = 16;
+    builder.roots_num   = 0;
+    builder.roots       = MVM_malloc(sizeof(MVMint32)*builder.roots_alloc);
+    /* Hold indices to the node that last computed a value belonging
+     * to a register. Initialized as -1 to indicate that these
+     * values are empty. */
     builder.computed = MVM_malloc(sizeof(MVMint32)*sg->num_locals);
-    builder.top      = 0;
-    builder.alloc    = 32;
-    builder.buffer   = MVM_malloc(sizeof(MVMJitExprNode)*builder.alloc);
-    /* Initialize computed as -1, indicating they haven't been loaded yet.
-     * Note that once we do implement register allocation, we will want to
-     * initialize this with values for allocated registers. */
     memset(builder.computed, -1, sizeof(MVMint32)*sg->num_locals);
     /* This is very similar to a code generation algorithm for a RISC
        machine with unlimited registers. (Or at least more registers
@@ -130,21 +153,34 @@ MVMJitExprTree * MVM_jit_build_expression_tree(MVMThreadContext *tc, MVMJitGraph
             break;
         }
         MVM_jit_expr_load_operands(tc, &builder, ins, operands);
-        last_root = MVM_jit_expr_apply_template(tc, &builder, templ, operands);
+        root = MVM_jit_expr_apply_template(tc, &builder, templ, operands);
         /* assign computed value to computed nodes, at least if we've
            written something */
         if ((ins->info->operands[0] & MVM_operand_rw_mask) == MVM_operand_write_reg) {
-            builder.computed[ins->operands[0].reg.orig] = last_root;
+            builder.computed[ins->operands[0].reg.orig] = root;
         } else {
-            /* still need to add it to roots */
-            builder_add_root(&builder, last_root);
+            /* Terminal, add it to roots */
+            builder_add_root(&builder, root);
         }
     }
+    /* Add stores for final values */
+    for (i = 0; i < sg->num_locals; i++) {
+        if (builder.computed[i] >= 0) {
+            MVMint32 root = MVM_jit_expr_add_storereg(tc, &builder, builder.computed[i], i);
+            builder_add_root(&builder, root);
+        }
+    }
+
     /* Reached the end correctly? Build a tree */
     if (ins == NULL) {
-        /* Hmm */
+        tree = MVM_spesh_alloc(tc, sg, sizeof(MVMJitExprTree));
+        tree->nodes = MVM_spesh_alloc(tc, sg, builder.num*sizeof(MVMJitExprNode));
+        tree->num_nodes = builder.num;
+        memcpy(tree->nodes, builder.nodes, builder.num*sizeof(MVMJitExprNode));
+        tree->roots = MVM_spesh_alloc(tc, sg, builder.roots_num*sizeof(MVMint32));
+        tree->num_roots = builder.roots_num;
     }
-    MVM_free(builder.buffer);
+    MVM_free(builder.nodes);
     MVM_free(builder.computed);
     return tree;
 }
