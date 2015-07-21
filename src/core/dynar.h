f6028a65f9b59ed6e50d4949321a5bd7f959bc18
Bart Wiegmans <bartwiegmans@gmail.com>
2015-07-21 13:22:25 +0200

    Ensure dynamic array block is zeroed
    Being zeored at initialisation is very useful, especially for
    pointer arrays.

--- src/core/dynar.h
+++ src/core/dynar.h
@@ -4,31 +4,35 @@
     size_t x ## _alloc;
 
 #define MVM_DYNAR_INIT(x, size) do { \
-        x = MVM_malloc((size)*sizeof(*x));    \
+        x = MVM_calloc((size), sizeof(*x));      \
         x ## _num = 0; \
         x ## _alloc = (size); \
     } while (0)
 
 #define MVM_DYNAR_GROW(x, size) do {\
         x = MVM_realloc(x, (size)*sizeof(*x));   \
+        memset(x + (x ## _alloc), 0, ((size) - (x ## _alloc)) * sizeof(*x)); \
         x ## _alloc = (size); \
     } while (0)
 
-#define MVM_DYNAR_ENSURE(x, space) \
-    if (((x ## _num) + (space)) >= (x ## _alloc)) {     \
-        size_t required = ((x ## _num) + (space)); \
-        size_t newsize = (x ## _alloc) * 2; \
-        while (newsize < required) newsize *= 2; \
+
+#define MVM_DYNAR_ENSURE_SIZE(x, size) \
+    if ((size) >= (x ## _alloc)) {     \
+        size_t newsize = (x ## _alloc) * 2 + 1; \
+        while ((size) >= newsize) newsize *= 2;  \
         MVM_DYNAR_GROW(x, newsize); \
     }
 
+#define MVM_DYNAR_ENSURE_SPACE(x, space) \
+    MVM_DYNAR_ENSURE_SIZE(x, (x ## _num) + (space))
+
 #define MVM_DYNAR_PUSH(x, value) do { \
-        MVM_DYNAR_ENSURE(x, 1); \
+        MVM_DYNAR_ENSURE_SPACE(x, 1); \
         x[x ## _num++] = (value); \
     } while(0)
 
 #define MVM_DYNAR_APPEND(x, ar, len) do { \
-        MVM_DYNAR_ENSURE(x, len); \
+        MVM_DYNAR_ENSURE_SPACE(x, len); \
         memcpy(x + x ## _num, ar, (len) * sizeof(x[0]));        \
         x ## _num += len; \
     } while(0)
