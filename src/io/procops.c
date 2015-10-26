3ab4710e5603b0de9c2fb57e7853a5f89850b654
jnthn <jnthn@jnthn.net>
2015-10-26 18:55:03 +0100

    Use /*...*/ style comments.
    As in rest of the codebase. Also move it in with another comment and
    provide a little more explanation.

--- src/io/procops.c
+++ src/io/procops.c
@@ -943,7 +943,8 @@ static void spawn_cancel(MVMThreadContext *tc, uv_loop_t *loop, MVMObject *async
     MVMIOAsyncProcessData *apd     = (MVMIOAsyncProcessData *)handle->body.data;
     uv_process_t          *phandle = apd->handle;
 
-    /* If it didn't already end, try to kill it. */
+    /* If it didn't already end, try to kill it. exit_cb will clean up phandle
+     * should the signal lead to process exit. */
     if (phandle) {
 #ifdef _WIN32
         /* On Windows, make sure we use a signal that will actually work. */
@@ -951,7 +952,6 @@ static void spawn_cancel(MVMThreadContext *tc, uv_loop_t *loop, MVMObject *async
             apd->signal = SIGKILL;
 #endif
         uv_process_kill(phandle, (int)apd->signal);
-        // let exit_cb clean up phandle
     }
 }
 
