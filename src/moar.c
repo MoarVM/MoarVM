2004013eb092b8e1575cf579e7133d4fec1a67c0
Rob Hoelz <rob@hoelz.ro>
2015-11-23 19:51:21 -0600

    Clean up interned callsites on interpreter shutdown

--- src/moar.c
+++ src/moar.c
@@ -284,6 +284,32 @@ void MVM_vm_exit(MVMInstance *instance) {
     exit(0);
 }
 
+static void cleanup_callsite_interns(MVMInstance *instance) {
+    int i;
+
+    for (i = 0; i < MVM_INTERN_ARITY_LIMIT; i++) {
+        int callsite_count = instance->callsite_interns->num_by_arity[i];
+        int j;
+
+        if (callsite_count) {
+            MVMCallsite **callsites = instance->callsite_interns->by_arity[i];
+
+            for (j = 0; j < callsite_count; j++) {
+                MVMCallsite *callsite = callsites[j];
+
+                if (MVM_callsite_is_common(callsite)) {
+                    continue;
+                }
+
+                MVM_callsite_destroy(callsite);
+            }
+
+            MVM_free(callsites);
+        }
+    }
+    MVM_free(instance->callsite_interns);
+}
+
 /* Destroys a VM instance. This must be called only from the main thread. It
  * should clear up all resources and free all memory; in practice, it falls
  * short of this goal at the moment. */
@@ -341,6 +367,10 @@ void MVM_vm_destroy_instance(MVMInstance *instance) {
 
     /* Clean up multi cache addition mutex. */
     uv_mutex_destroy(&instance->mutex_multi_cache_add);
+
+    /* Clean up interned callsites */
+    uv_mutex_destroy(&instance->mutex_callsite_interns);
+    cleanup_callsite_interns(instance);
 
     /* Clean up spesh install mutex and close any log. */
     uv_mutex_destroy(&instance->mutex_spesh_install);
