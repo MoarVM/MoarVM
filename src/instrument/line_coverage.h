a4dab451b2c9c40ee0ca6f4f025f3738716be2f1
Timo Paulssen <timonator@perpetuum-immobile.de>
2015-12-06 19:56:20 +0100

    use a blob of memory instead of BOOTIntArray
    this ought to make things a bit less expensive.

--- src/instrument/line_coverage.h
+++ src/instrument/line_coverage.h
@@ -1,2 +1,2 @@
 void MVM_line_coverage_instrument(MVMThreadContext *tc, MVMStaticFrame *static_frame);
-void MVM_line_coverage_report(MVMThreadContext *tc, MVMString *filename, MVMuint32 line_number, MVMuint16 cache_slot, MVMObject *cache);
+void MVM_line_coverage_report(MVMThreadContext *tc, MVMString *filename, MVMuint32 line_number, MVMuint16 cache_slot, char *cache);
