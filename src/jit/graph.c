6b2e4e07e31cc5187cc68bd86d298bae5412dbed
Timo Paulssen <timonator@perpetuum-immobile.de>
2016-03-06 17:31:14 +0100

    jit the atposref_* ops

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -383,6 +383,9 @@ static void * op_to_func(MVMThreadContext *tc, MVMint16 opcode) {
     case MVM_OP_getattrsref_i: return MVM_nativeref_attr_i;
     case MVM_OP_getattrsref_n: return MVM_nativeref_attr_n;
     case MVM_OP_getattrsref_s: return MVM_nativeref_attr_s;
+    case MVM_OP_atposref_i: return MVM_nativeref_pos_i;
+    case MVM_OP_atposref_n: return MVM_nativeref_pos_n;
+    case MVM_OP_atposref_s: return MVM_nativeref_pos_s;
     case MVM_OP_sp_boolify_iter: return MVM_iter_istrue;
     case MVM_OP_prof_allocated: return MVM_profile_log_allocated;
     case MVM_OP_prof_exit: return MVM_profile_log_exit;
@@ -2631,6 +2634,18 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
                                  { MVM_JIT_REG_VAL, { class } },
                                  { MVM_JIT_REG_VAL, { name } } };
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 4, args, MVM_JIT_RV_PTR, dst);
+        break;
+    }
+    case MVM_OP_atposref_i:
+    case MVM_OP_atposref_n:
+    case MVM_OP_atposref_s: {
+        MVMint16 dst     = ins->operands[0].reg.orig;
+        MVMint16 obj     = ins->operands[1].reg.orig;
+        MVMint16 index   = ins->operands[2].reg.orig;
+        MVMJitCallArg args[] = { { MVM_JIT_INTERP_VAR, { MVM_JIT_INTERP_TC } },
+                                 { MVM_JIT_REG_VAL, { obj } },
+                                 { MVM_JIT_REG_VAL, { index } } };
+        jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_PTR, dst);
         break;
     }
         /* profiling */
