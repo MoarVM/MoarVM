82535c30450743b54deb30c702486ee979a2d8a9
Bart Wiegmans <bartwiegmans@gmail.com>
2016-03-23 15:32:56 +0100

    Typo fix!

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -2091,8 +2091,7 @@ static MVMint32 jgb_consume_ins(MVMThreadContext *tc, JitGraphBuilder *jgb,
         MVMint16 typ = ins->operands[2].reg.orig;
         MVMJitCallArg args[] = {{ MVM_JIT_INTERP_VAR, { MVM_JIT_INTERP_TC } },
                                 { MVM_JIT_REG_VAL,   { typ } },
-                                { MVM_JIT_REG_VAL_F, { src } };
-
+                                { MVM_JIT_REG_VAL_F, { src } }};
 
         jgb_append_call_c(tc, jgb, op_to_func(tc, op), 3, args, MVM_JIT_RV_PTR, dst);
         break;
