d78a5e34046fc8c8b69dfaac86b06d1169ef87ed
Timo Paulssen <timonator@perpetuum-immobile.de>
2016-03-24 09:07:25 +0100

    Describing SCRef's refs.

--- src/6model/reprs/SCRef.c
+++ src/6model/reprs/SCRef.c
@@ -133,21 +133,67 @@ static void compose(MVMThreadContext *tc, MVMSTable *st, MVMObject *info) {
     /* Nothing to do for this REPR. */
 }
 
-/* Calculates the non-GC-managed memory we hold on to. */
-/* FIXME this is currently b0rked, see MoarVM Issue #348. */
-/* static MVMuint64 unmanaged_size(MVMThreadContext *tc, MVMSTable *st, void *data) {
-    MVMSerializationContextBody *body = (MVMSerializationContextBody *)data;
+static MVMuint64 unmanaged_size(MVMThreadContext *tc, MVMSTable *st, void *data) {
+    MVMSerializationContextBody     *body      = ((MVMSerializationContextBody **)data)[0];
     MVMuint64 size = 0;
 
     size += sizeof(MVMObject *) * body->num_objects;
     size += sizeof(MVMSTable *) * body->num_stables;
 
-    fprintf(stderr, "SCRef size: %"PRIu64" objects, %"PRIu64" stables, size is %"PRIu64"\n", body->num_objects, body->num_stables, size); */
-
     /* XXX probably have to measure the MVMSerializationReader, too */
 
-/*    return size;
-}*/
+    return size;
+}
+
+static void describe_refs(MVMThreadContext *tc, MVMHeapSnapshotState *ss, MVMSTable *st, void *data) {
+    MVMSerializationContextBody *body = ((MVMSerializationContextBody **)data)[0];
+    MVMuint64 index;
+
+    if (body->sr)
+        return;
+
+    for (index = 0; index < body->num_objects; index++)
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->root_objects[index], "Object root set");
+    for (index = 0; index < body->num_stables; index++)
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->root_stables[index], "STable root set");
+
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->root_codes, "Root code refs");
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->rep_indexes, "Repossession indices");
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->rep_scs, "Repossession SCs");
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->owned_objects, "Owned Objects");
+
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->handle, "Handle");
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->description, "Description");
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->sc, "SC");
+    MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+        (MVMCollectable *)body->mutex, "Mutex");
+
+    /* Mark serialization reader, if we have one. */
+    if (body->sr) {
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->sr->root.sc, "Reader Root SC");
+        for (index = 0; index < body->sr->root.num_dependencies; index++)
+            MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+                (MVMCollectable *)body->sr->root.dependent_scs[index], "SC Dependency (Reader)");
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->sr->root.string_heap, "String heap (Reader)");
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->sr->root.string_comp_unit, "String compilation unit (Reader)");
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->sr->codes_list, "Code objects list (Reader)");
+        MVM_profile_heap_add_collectable_rel_const_cstr(tc, ss,
+            (MVMCollectable *)body->sr->current_object, "Current object (Reader)");
+    }
+}
 
 /* Initializes the representation. */
 const MVMREPROps * MVMSCRef_initialize(MVMThreadContext *tc) {
@@ -181,6 +227,6 @@ static const MVMREPROps this_repr = {
     "SCRef", /* name */
     MVM_REPR_ID_SCRef,
     0, /* refs_frames */
-    NULL, /* unmanaged_size */
-    NULL, /* describe_refs */
+    unmanaged_size,
+    describe_refs,
 };
