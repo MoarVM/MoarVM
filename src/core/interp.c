4115a67c8c1a6825ec612d811550a366a67539b7
Stefan Seifert <nine@detonation.org>
2016-05-04 18:32:52 +0200

    Implement loadbytecodebuffer OP
    Usefull for loading mbc from an in-memory buffer.

--- src/core/interp.c
+++ src/core/interp.c
@@ -3072,6 +3072,19 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 MVM_load_bytecode(tc, filename);
                 goto NEXT;
             }
+            OP(loadbytecodebuffer): {
+                /* This op will end up returning into the runloop to run
+                 * deserialization and load code, so make sure we're done
+                 * processing this op really. */
+                MVMObject *buffer = GET_REG(cur_op, 0).o;
+                cur_op += 2;
+
+                /* Set up return (really continuation after load) address
+                 * and enter bytecode loading process. */
+                tc->cur_frame->return_address = cur_op;
+                MVM_load_bytecode_buffer(tc, buffer);
+                goto NEXT;
+            }
             OP(masttofile):
                 MVM_mast_to_file(tc, GET_REG(cur_op, 0).o,
                     GET_REG(cur_op, 2).o, GET_REG(cur_op, 4).s);
--- src/core/loadbytecode.h
+++ src/core/loadbytecode.h
@@ -1,1 +1,2 @@
+void MVM_load_bytecode_buffer(MVMThreadContext *tc, MVMObject *buf);
 void MVM_load_bytecode(MVMThreadContext *tc, MVMString *filename);
