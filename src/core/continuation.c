da178fb2cef35d5cfc9a9a40db686481c4aaec63
jnthn <jnthn@jnthn.net>
2016-05-11 14:53:06 +0200

    Optimize away a loop we no longer need.
    Was needed when we had to fiddle with refcounts, which we no longer
    do.

--- src/core/continuation.c
+++ src/core/continuation.c
@@ -106,8 +106,7 @@ void MVM_continuation_control(MVMThreadContext *tc, MVMint64 protect,
     }
 
     /* Move back to the frame with the reset in it. */
-    while (tc->cur_frame != jump_frame)
-        tc->cur_frame = tc->cur_frame->caller;
+    tc->cur_frame = jump_frame;
     *(tc->interp_cur_op) = tc->cur_frame->return_address;
     *(tc->interp_bytecode_start) = tc->cur_frame->effective_bytecode;
     *(tc->interp_reg_base) = tc->cur_frame->work;
