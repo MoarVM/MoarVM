dc0b36cad3bbd3eacd356f6051921661b44d5143
Jonathan Worthington <jnthn@jnthn.net>
2016-06-01 12:36:31 +0200

    Mark getlexcaller as :noinline.

--- src/core/ops.c
+++ src/core/ops.c
@@ -5779,7 +5779,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         2,
         1,
         0,
-        0,
+        1,
         0,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_str }
     },
