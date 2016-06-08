085c69861c2752760c909844a3f7a851cd2926cc
Jonathan Worthington <jnthn@jnthn.net>
2016-06-08 12:25:26 +0200

    Make throwpayloadlex as :noinline.
    Since it needs its lexical chain to be in shape, like various other
    things that do lexical lookup. (We'll be able to be smarter in the
    future.)

--- src/core/ops.c
+++ src/core/ops.c
@@ -8151,7 +8151,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         3,
         0,
         0,
-        0,
+        1,
         0,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_int64, MVM_operand_read_reg | MVM_operand_obj }
     },
