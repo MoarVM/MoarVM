c797bd744edb3190431d56f1f757087e42eb53ed
Timo Paulssen <timonator@perpetuum-immobile.de>
2016-06-30 17:53:07 +0200

    chase changes in staticframe struct

--- src/core/interp.c
+++ src/core/interp.c
@@ -5592,10 +5592,10 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 goto NEXT;
             }
             OP(coverage_log): {
-                MVMString *filename = cu->body.strings[GET_UI32(cur_op, 0)];
+                MVMString *filename = MVM_cu_string(tc, cu, GET_UI32(cur_op, 0));
                 MVMuint32 lineno    = GET_UI32(cur_op, 4);
                 MVMuint32 cacheidx  = GET_UI32(cur_op, 8);
-                MVMObject *cache    = (char *)MVM_BC_get_I64(cur_op, 12);
+                char      *cache    = (char *)MVM_BC_get_I64(cur_op, 12);
                 MVM_line_coverage_report(tc, filename, lineno, cacheidx, cache);
                 cur_op += 20;
                 goto NEXT;
