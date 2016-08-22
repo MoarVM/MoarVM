bca85b242eb4f54941cb0659562c783d3d460580
Timo Paulssen <timonator@perpetuum-immobile.de>
2016-08-22 23:41:12 +0200

    validate indices of param_ ops
    we can already identify b0rked frames in the bytecode
    validation phase in this aspect. afl-fuzzy was able to
    create a crapton of crashing cases based on the access
    to param buffers not being bounds-checked at all. Now
    instead of paying a little bit for a bounds check on
    every single param access, we make validation a bit
    smarter up-front.

--- src/core/validation.c
+++ src/core/validation.c
@@ -46,6 +46,7 @@ typedef struct {
     MVMuint32         cur_instr;
     MVMCallsite      *cur_call;
     MVMuint16         cur_arg;
+    MVMint32          acceptable_max_arity;
     MVMCallsiteEntry  expected_named_arg;
     MVMuint16         remaining_args;
     MVMuint16         remaining_positionals;
@@ -392,12 +393,39 @@ static void validate_operands(Validator *val) {
 
             break;
         }
+        case MVM_OP_checkarity: {
+            MVMint64 count;
+
+            validate_literal_operand(val, operands[0]);
+
+            validate_literal_operand(val, operands[1]);
+            val->acceptable_max_arity = GET_UI16(val->cur_op, -2);
+
+            break;
+        }
 
         default: {
             int i;
 
-            for (i = 0; i < val->cur_info->num_operands; i++)
-                validate_operand(val, val->cur_info->operands[i]);
+            if (val->cur_mark[1] == 'p') {
+                /* For the p-marked ops, which is a subset of param_* ops,
+                 * we check the second argument against the value checkarity
+                 * checked against. */
+                for (i = 0; i < val->cur_info->num_operands; i++) {
+                    validate_operand(val, val->cur_info->operands[i]);
+
+                    /* This is the argument we want to check */
+                    if (i == 1) {
+                        MVMint16 value = GET_UI16(val->cur_op, -2);
+                        if (value > val->acceptable_max_arity) {
+                            fail(val, MSG(val, "tried to take arg number %d after checkarity with %d"), value, val->acceptable_max_arity);
+                        }
+                    }
+                }
+            } else {
+                for (i = 0; i < val->cur_info->num_operands; i++)
+                    validate_operand(val, val->cur_info->operands[i]);
+            }
         }
     }
 }
