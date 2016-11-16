22bbe0282f1058538a17f3cfde21322087d85f1a
Tobias Leich <email@froggs.de>
2016-11-16 20:54:43 +0100

    Basic support of TILE-Gx and TILEPro CPUs
    This patch is an updated version of the CentOS 6 patch that we have
    been carrying as part of our own CentOS-like distribution since 2012.
    
    * src/Makefile.am (nobase_private_HEADERS): Add tile.h.
    * src/atomic_ops.h [__tile__]: Include tile.h file.
    * src/atomic_ops/sysdeps/gcc/tile.h: New file.
    https://github.com/ivmai/libatomic_ops/commit/04a2864
    Author: Chris Metcalf <cmetcalf@mellanox.com>
    Date:   Wed May 11 22:19:57 2016 +0300

--- 3rdparty/libatomic_ops/src/atomic_ops.h
+++ 3rdparty/libatomic_ops/src/atomic_ops.h
@@ -294,6 +294,9 @@
 # if defined(__hexagon__)
 #   include "atomic_ops/sysdeps/gcc/hexagon.h"
 # endif
+# if defined(__tile__)
+#   include "atomic_ops/sysdeps/gcc/tile.h"
+# endif
 #endif /* __GNUC__ && !AO_USE_PTHREAD_DEFS */
 
 #if (defined(__IBMC__) || defined(__IBMCPP__)) && !defined(__GNUC__) \
