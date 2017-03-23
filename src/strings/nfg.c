MoarVM/MoarVM

commit f273133979912bed1bf1590299f0975994e32ade

Special case "\r\n" in MVM_nfg_is_concat_stable
"\r\n" can never combine with anything else, so we can special-case it
in the check whether the relevant graphemes are synthetic.

src/strings/nfg.c
@@ -354,6 +354,7 @@ static MVMint32 passes_quickcheck_and_zero_ccc(MVMThreadContext *tc, MVMCodepoin
 MVMint32 MVM_nfg_is_concat_stable(MVMThreadContext *tc, MVMString *a, MVMString *b) {
     MVMGrapheme32 last_a;
     MVMGrapheme32 first_b;
+    MVMGrapheme32 crlf;
 
     /* If either string is empty, we're good. */
     if (a->body.num_graphs == 0 || b->body.num_graphs == 0)
@@ -363,10 +364,12 @@ MVMint32 MVM_nfg_is_concat_stable(MVMThreadContext *tc, MVMString *a, MVMString 
     last_a = MVM_string_get_grapheme_at_nocheck(tc, a, a->body.num_graphs - 1);
     first_b = MVM_string_get_grapheme_at_nocheck(tc, b, 0);
 
-    /* If either is synthetic, assume we'll have to re-normalize (this is an
-     * over-estimate, most likely). Note if you optimize this that it serves
-     * as a guard for what follows. */
-    if (last_a < 0 || first_b < 0)
+    crlf = MVM_nfg_crlf_grapheme(tc);
+
+    /* If either is synthetic other than "\r\n", assume we'll have to re-normalize
+     * (this is an over-estimate, most likely). Note if you optimize this that it
+     * serves as a guard for what follows. */
+    if ((last_a != crlf && last_a < 0) || (first_b != crlf && first_b < 0))
         return 0;
 
     /* If both less than the first significant char for NFC, and the first is

commit a00278e611a65292bad2da9f21b3d0b535d4d76d

Fix exception concat’ing some strings with \r\n line endings
The bug was introduced in f2731339, and is resolved by this commit.
Bug would cause “should eventually be unreachable” message upon MVM
throwing an adhoc exception from inside unicode_db.c

This was caused by checking property values for codepoints under 0
(which are synthetics). The \r\n special case allowed this synthetic
to fall through to the end of MVM_nfg_is_concat_stable.

This bug was seen concat’ing Korean text (Hangul script) that contained
\r\n line endings. Tests will be added to roast to cover this soon.

src/strings/nfg.c
@@ -386,8 +386,8 @@ MVMint32 MVM_nfg_is_concat_stable(MVMThreadContext *tc, MVMString *a, MVMString 
 
     /* If either fail quickcheck or have ccc > 0, and it does not have
      * Grapheme_Cluster_Break=Control we have to re-normalize */
-    return (codepoint_GCB_Control(tc, last_a) || passes_quickcheck_and_zero_ccc(tc, last_a))
-    && (codepoint_GCB_Control(tc, first_b) || passes_quickcheck_and_zero_ccc(tc, first_b));
+    return (last_a == crlf || codepoint_GCB_Control(tc, last_a) || passes_quickcheck_and_zero_ccc(tc, last_a))
+        && (first_b == crlf || codepoint_GCB_Control(tc, first_b) || passes_quickcheck_and_zero_ccc(tc, first_b));
 }
 
 /* Free all memory allocated to hold synthetic graphemes. These are global