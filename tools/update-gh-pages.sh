b049e7114f5da1eddd10c1e84db01357a2ddd66d
Samantha McVey <samantham@posteo.net>
2017-04-14 15:21:55 -0700

    fix

--- tools/update-gh-pages.sh
+++ tools/update-gh-pages.sh
@@ -19,7 +19,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$TARGET_BRANCH"  
   # -----------------------------
   #go to home and setup git
   cd -- "$HOME" || echo "Couldn't cd into $HOME";
-  git config user.name "Travis CI"
+  git config --global user.name "Travis CI"
   #git config user.email "$COMMIT_AUTHOR_EMAIL"
   git config --global user.email "travis@travis-ci.org"
   #git config --global user.name "Travis"
