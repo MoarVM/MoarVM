362277b79fef8bfa45ad44a4062a05785187ddfe
Samantha McVey <samantham@posteo.net>
2017-05-10 13:39:32 -0700

    Better version of patch to fix right shifting negative smallints
    The previous patch did not check what sign was there and didn't fully
    handle big/smallint containing P6int's. This one adds a new
    BIGINT_IS_NEGATIVE() function to check if it is negative.
    
    This function may be very useful other places we need to check if a
    p6int is negative.

--- src/math/bigintops.c
+++ src/math/bigintops.c
@@ -719,7 +719,17 @@ MVMObject *MVM_bigint_shl(MVMThreadContext *tc, MVMObject *result_type, MVMObjec
 
     return result;
 }
-
+/* Checks if a MVMP6bigintBody is negative. Handles cases where it is stored as
+ * a small int as well as cases when it is stored as a bigint */
+int BIGINT_IS_NEGATIVE (MVMP6bigintBody *ba) {
+    mp_int *mp_a = ba->u.bigint;
+    if MVM_BIGINT_IS_BIG(ba) {
+        return SIGN(mp_a) == MP_NEG;
+    }
+    else {
+        return ba->u.smallint.value < 0;
+    }
+}
 MVMObject *MVM_bigint_shr(MVMThreadContext *tc, MVMObject *result_type, MVMObject *a, MVMint64 n) {
     MVMP6bigintBody *ba = get_bigint_body(tc, a);
     MVMP6bigintBody *bb;
@@ -741,7 +751,7 @@ MVMObject *MVM_bigint_shr(MVMThreadContext *tc, MVMObject *result_type, MVMObjec
         clear_temp_bigints(tmp, 1);
         adjust_nursery(tc, bb);
     } else if (n >= 32) {
-        store_int64_result(bb, -1);
+        store_int64_result(bb, BIGINT_IS_NEGATIVE(ba) ? -1 : 0);
     } else {
         MVMint32 value = ba->u.smallint.value;
         value = value >> n;
