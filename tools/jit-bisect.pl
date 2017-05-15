9bc0ed2b9c5120a253810152292c4bc4133521e9
Bart Wiegmans <bartwiegmans@gmail.com>
2017-05-15 14:39:45 +0200

    Split jit-dump from jit-bisect
    The jit-bisect debug tool was somewhat unreliable and frankly did
    too many things at once. I've split the code to get a specific JIT
    dump into its own script and refactored the bisecting code. Stuff
    is generally more reliable now.

--- tools/jit-bisect.pl
+++ tools/jit-bisect.pl
@@ -1,178 +1,174 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use FindBin;
 use Getopt::Long;
-use File::Temp 'tempdir';
 use File::Spec;
-use File::Copy;
+use FindBin;
 
 
-my $interpreter = 'perl6';
-my $test;
-my $command;
-my $verbose;
-my $objdump;
-my @libs;
-
-
-GetOptions(
-    'test=s' => \$test,
-    'interpreter=s' => \$interpreter,
-    'lib=s' => \@libs,
-    'command=s' => \$command,
-    'verbose' => \$verbose,
-    'objdump=s' => \$objdump,
-);
-
-my @command;
-if (defined $command) {
-    # TODO: this wants some better evaluation strategy rather than
-    # removing " blindly
-    @command = map s/"//rg, split / /, $command;
-} else {
-    unless (defined($test)) {
-        $test = pop @ARGV;
-    }
-
-    unless (defined $command || defined($test) && -f $test) {
-        print STDERR "Pass a test file argument";
-        exit 1;
-    }
-
-    # add lib for standard module layout
-    unless (@libs) {
-        my ($v, $d, $n) = File::Spec->splitpath($test);
-        my @dirs = File::Spec->splitdir($d);
-        if ($dirs[-2] eq 't') {
-            $dirs[-2] = 'lib';
-            my $lib = File::Spec->catpath($v, File::Spec->catdir(@dirs));
-            push @libs, $lib if -d $lib;
+sub run_with {
+    my ($command, $env) = @_;
+    my $status;
+    {
+        # simulate 'local' env vars, which doesn't really work with
+        # child processes
+        my %copy;
+        while (my ($k,$v) = each %$env) {
+            $copy{$k} = $ENV{$v};
+            $ENV{$k} = $v;
+        }
+        $status = system @$command;
+        while (my ($k,$v) = each %copy) {
+            if (defined $v) {
+                $ENV{$k} = $v;
+            } else {
+                delete $ENV{$k};
+            }
         }
     }
 
-    @command = ('prove', '-e', $interpreter, (@libs ? '-I' : ''), @libs, $test);
+    if ($status == -1) {
+        local $" = ' ';
+        die "Failed to start: `@$command`: $!";
+    }
+
+    return $status;
 }
 
+sub quietly(&) {
+    my ($code) = @_;
+    my ($error, @result);
+    my ($dupout, $duperr);
 
-$objdump = ($^O =~ m/darwin/ ? 'gobjdump' : 'objdump') unless defined $objdump;
 
-my $bytecode_dir = tempdir();
-$ENV{'MVM_JIT_BYTECODE_DIR'} = $bytecode_dir;
-
-# clone stdout, stderr
-open my $stdout, '>&', STDOUT;
-open my $stderr, '>&', STDERR;
-
-# silence prove
-unless ($verbose) {
+    open $dupout, '>&', \*STDOUT;
+    open $duperr, '>&', \*STDERR;
     close STDOUT;
     close STDERR;
-    open STDOUT, '>', '/dev/null';
-    open STDERR, '>', '/dev/null';
+    open STDOUT, '>', File::Spec->devnull;
+    open STDERR, '>', File::Spec->devnull;
+
+    eval {
+        if (!defined wantarray) {
+            $code->();
+        } elsif (wantarray) {
+            @result = $code->();
+        } else {
+            $result[0] = scalar $code->();
+        }
+        1;
+    } or do {
+        $error = $@ || $!;
+    };
+
+    close STDOUT;
+    close STDERR;
+    open STDOUT, '>&', $dupout;
+    open STDERR, '>&', $duperr;
+    close $dupout;
+    close $duperr;
+
+    die $error if $error;
+
+    return wantarray ? @result : $result[0];
 }
-# do initial run
-my $code = system @command;
-if ($code == 0) {
-    print $stdout "Test is OK\n";
-    exit;
+
+sub noisily(&) {
+    my ($code) = @_;
+    $code->();
 }
 
-open my $frame_map_fh, '<', "$bytecode_dir/jit-map.txt";
-my %frame_map = map { my @a = split /\s/; $a[0] => "$a[1] $a[2]" } <$frame_map_fh>;
-close $frame_map_fh;
+sub bisect {
+    my ($varname, $program, $env) = @_;
 
-my $num_frames = scalar keys %frame_map;
-print $stdout "$num_frames compiled in test\n";
-
-# don't write more bytecode files
-$ENV{'MVM_JIT_BYTECODE_DIR'} = '';
-
-my $min = 1;
-my $max = $num_frames;
-my $mid;
-while (($max - $min) > 1) {
-    $mid = int(($max + $min) / 2);
-    $ENV{'MVM_JIT_EXPR_LAST_FRAME'} = $mid;
-    print $stdout "Testing up to $mid frames....";
-    $code = system @command;
-    if ($code == 0) {
-        # ok
-        print $stdout "OK\n";
-        $min = $mid;
-    } else {
-        print $stdout "Broken\n";
-        $max = $mid;
+    $env ||= {};
+    printf STDERR ("Bisecting %s\n", $varname);
+    if (%$env) {
+        printf STDERR "Given:\n";
+        printf STDERR "  %s=%s\n", $_, $env->{$_} for keys %$env;
     }
-}
-my $frame_nr = $max;
-print $stdout "Broken at frame $frame_nr\n";
-my $frame_file = sprintf("%s/moar-jit-%04d.bin", $bytecode_dir, $frame_nr);
 
-print $stdout $frame_file, $frame_map{$frame_file}, "\n";
+    my ($low, $high, $mid) = (0,1,0);
+    my $status;
 
+    do {
+        printf STDERR "%s=%d", $varname, $high;
+        $status = quietly {
+            run_with($program, { %$env, $varname => $high });
+        };
+        if ($status == 0) {
+            print STDERR "\tOK\n";
+            ($low, $high) = ($high, $high * 2);
+        } else {
+            print STDERR "\tNOT OK\n";
+        }
+    } while ($status == 0);
 
-# exponential probing of breaking basic block
-$ENV{'MVM_JIT_EXPR_LAST_FRAME'} = $max;
-$code = 0;
-$min = 1;
-do {
-    $ENV{'MVM_JIT_EXPR_LAST_BB'} = $min;
-    print $stdout "Probing $min basic blocks\n";
-    $code = system @command;
-    $min *= 2 if $code == 0;
-} while ($code == 0);
-
-$max = $min;
-$min = $min / 2;
-while (($max - $min) > 1) {
-    $mid = int(($max + $min) / 2);
-    $ENV{'MVM_JIT_EXPR_LAST_BB'} = $mid;
-    print $stdout "Testing $mid basic blocks...";
-    $code = system @command;
-    if ($code == 0) {
-        print $stdout "OK\n";
-        $min = $mid;
-    } else {
-        print $stdout "Broken\n";
-        $max = $mid;
+     while (($high - $low) > 1) {
+        $mid = int(($high + $low) / 2);
+        printf STDERR "%s=%d", $varname, $mid;
+        $status = quietly {
+            run_with($program, { %$env, $varname => $mid });
+        };
+        if ($status == 0) {
+            $low = $mid;
+            print STDERR "\tOK\n";
+        } else {
+            $high = $mid;
+            print STDERR "\tNOT OK\n";
+        }
     }
+    return $status ? $low : $mid;
 }
-my $basic_block_nr = $max;
-print $stdout "Broken basic block nr $basic_block_nr of frame $frame_nr\n";
 
-# now, for a final trick
-print $stdout "Acquiring code diff...\n";
-$ENV{'MVM_JIT_BYTECODE_DIR'} = $bytecode_dir;
-$ENV{'MVM_JIT_EXPR_LAST_BB'} = $basic_block_nr;
-$ENV{'MVM_JIT_LOG'}          = 'broken-log.txt';
-# get the broken file
-system @command;
-copy($frame_file, 'broken-frame.bin');
-$ENV{'MVM_JIT_EXPR_LAST_BB'} = $basic_block_nr - 1;
-$ENV{'MVM_JIT_LOG'}          = 'working-log.txt';
-# and the working file
-system @command;
-copy($frame_file, 'working-frame.bin');
 
-# disassemble and make comparable
-{
-    no warnings 'qw';
+my %OPTS = (
+    verbose => 0,
+    dump => 1
+);
+GetOptions(\%OPTS, qw(verbose dump!)) or die "Could not get options";
 
-    close STDOUT;
-    open STDOUT, '>', "broken-frame.asm";
-    system($objdump, qw(-b binary -D -m i386 -M x86-64,intel broken-frame.bin)) == 0
-        or die "Error running $objdump";
-    close STDOUT;
+my @command = @ARGV;
+die 'Command is required' unless @command;
 
-    open STDOUT, '>', "working-frame.asm";
-    system($objdump qw(-b binary -D -m i386 -M x86-64,intel working-frame.bin)) == 0
-        or die "Error running $objdump";
-    close STDOUT;
-
-    my $script = "$FindBin::Bin/jit-comparify-asm.pl";
-    open STDOUT, '>', '/dev/null';
-
-    system $^X, '-i', $script, "broken-frame.asm";
-    system $^X, '-i', $script, "working-frame.asm";
+if ($OPTS{verbose}) {
+    no warnings 'redefine';
+    *quietly = \&noisily;
 }
+
+# start with a clean slate
+delete @ENV{qw(
+    MVM_JIT_EXPR_DISABLE
+    MVM_JIT_EXPR_LAST_FRAME
+    MVM_JTI_EXPR_LAST_BB
+    MVM_JIT_DISABLE
+    MVM_SPESH_DISABLE
+)};
+
+
+quietly { run_with(\@command, {}) } or do {
+    die "This program is quite alright";
+};
+quietly { run_with(\@command, { MVM_JIT_EXPR_DISABLE => 1 }) } and do {
+    die "This program cannot be bisected: $?";
+};
+printf STDERR "Checks OK, this program can be bisected\n";
+
+
+
+my $last_good_frame = bisect('MVM_JIT_EXPR_LAST_FRAME', \@command);
+my $last_good_block = bisect('MVM_JIT_EXPR_LAST_BB', \@command, {
+    MVM_JIT_EXPR_LAST_FRAME => $last_good_frame + 1
+});
+printf STDERR ('Broken Frame/BB: %d / %d'."\n", $last_good_frame + 1, $last_good_block + 1);
+
+my $dump_script = File::Spec->catfile($FindBin::Bin, 'jit-dump.pl');
+my @dump_command = (
+    $^X, $dump_script,
+    '--frame' => $last_good_frame + 1, '--block' => $last_good_block + 1,
+    '--', @command
+);
+run_with(\@dump_command, {}) if $OPTS{dump};
+
+__END__
+
