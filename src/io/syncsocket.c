9c3bdfe6f105da7d1d05cf3d51130fe280179cf1
Jonathan Worthington <jnthn@jnthn.net>
2017-05-19 15:41:52 +0200

    Duplicate syncstream functions into syncsocket.
    This is the first step towards pulling the two apart, in preparation
    for syncsocket ceasing to use libuv.

--- src/io/syncsocket.c
+++ src/io/syncsocket.c
@@ -8,6 +8,10 @@
 #define snprintf _snprintf
 #endif
 
+/* Number of bytes we read at a time to the buffer (equal to largest TCP
+ * packet size). */
+#define CHUNK_SIZE 65536
+
  /* Data that we keep for a socket-based handle. */
 typedef struct {
     /* Start with same fields as a sync stream, since we will re-use most
@@ -21,6 +25,128 @@ typedef struct {
     uv_stream_t *accept_server;
     int          accept_status;
 } MVMIOSyncSocketData;
+
+/* Read a bunch of bytes into the current decode stream. Returns true if we
+ * read some data, and false if we hit EOF. */
+static void on_alloc(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+    MVMIOSyncStreamData *data = (MVMIOSyncStreamData *)handle->data;
+    size_t size = suggested_size > 0 ? suggested_size : 4;
+    buf->base   = MVM_malloc(size);
+    MVM_telemetry_interval_annotate(size, data->interval_id, "alloced this much space");
+    buf->len    = size;
+}
+static void on_read(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf) {
+    MVMIOSyncStreamData *data = (MVMIOSyncStreamData *)handle->data;
+    if (nread > 0) {
+        MVM_string_decodestream_add_bytes(data->cur_tc, data->ds, buf->base, nread);
+        MVM_telemetry_interval_annotate(nread, data->interval_id, "read this many bytes");
+    }
+    else if (nread == UV_EOF) {
+        data->eof = 1;
+        if (buf->base)
+            MVM_free(buf->base);
+    }
+    uv_read_stop(handle);
+    uv_unref((uv_handle_t*)handle);
+}
+static MVMint32 read_to_buffer(MVMThreadContext *tc, MVMIOSyncStreamData *data, MVMint32 bytes) {
+    /* Don't try and read again if we already saw EOF. */
+    if (!data->eof) {
+        int r;
+        unsigned int interval_id;
+
+        interval_id = MVM_telemetry_interval_start(tc, "syncstream.read_to_buffer");
+        data->handle->data = data;
+        data->cur_tc = tc;
+        if ((r = uv_read_start(data->handle, on_alloc, on_read)) < 0)
+            MVM_exception_throw_adhoc(tc, "Reading from stream failed: %s",
+                uv_strerror(r));
+        uv_ref((uv_handle_t *)data->handle);
+        if (tc->loop != data->handle->loop) {
+            MVM_exception_throw_adhoc(tc, "Tried to read() from an IO handle outside its originating thread");
+        }
+        MVM_gc_mark_thread_blocked(tc);
+        uv_run(tc->loop, UV_RUN_DEFAULT);
+        MVM_gc_mark_thread_unblocked(tc);
+        MVM_telemetry_interval_stop(tc, interval_id, "syncstream.read_to_buffer");
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+/* Ensures we have a decode stream, creating it if we're missing one. */
+static void ensure_decode_stream(MVMThreadContext *tc, MVMIOSyncStreamData *data) {
+    if (!data->ds)
+        data->ds = MVM_string_decodestream_create(tc, data->encoding, 0,
+            data->translate_newlines);
+}
+
+MVMint64 socket_read_bytes(MVMThreadContext *tc, MVMOSHandle *h, char **buf, MVMint64 bytes) {
+    MVMIOSyncStreamData *data = (MVMIOSyncStreamData *)h->body.data;
+    ensure_decode_stream(tc, data);
+
+    /* See if we've already enough; if not, try and grab more. */
+    if (!MVM_string_decodestream_have_bytes(tc, data->ds, bytes))
+        read_to_buffer(tc, data, bytes > CHUNK_SIZE ? bytes : CHUNK_SIZE);
+
+    /* Read as many as we can, up to the limit. */
+    return MVM_string_decodestream_bytes_to_buf(tc, data->ds, buf, bytes);
+}
+
+/* Checks if the end of stream has been reached. */
+MVMint64 socket_eof(MVMThreadContext *tc, MVMOSHandle *h) {
+    MVMIOSyncStreamData *data = (MVMIOSyncStreamData *)h->body.data;
+
+    /* If we still have stuff in the buffer, certainly not the end (even if
+     * data->eof is set; that just means we read all we can from libuv, not
+     * that we processed it all). */
+    if (data->ds && !MVM_string_decodestream_is_empty(tc, data->ds))
+        return 0;
+
+    /* Otherwise, go on the EOF flag from the underlying stream. */
+    return data->eof;
+}
+
+void socket_flush(MVMThreadContext *tc, MVMOSHandle *h){
+    /* A no-op for sockets; we don't buffer. */
+}
+
+void socket_truncate(MVMThreadContext *tc, MVMOSHandle *h, MVMint64 bytes) {
+    MVM_exception_throw_adhoc(tc, "Cannot truncate a socket");
+}
+
+/* Writes the specified bytes to the stream. */
+static void write_cb(uv_write_t* req, int status) {
+    uv_unref((uv_handle_t *)req->handle);
+    MVM_free(req);
+}
+MVMint64 socket_write_bytes(MVMThreadContext *tc, MVMOSHandle *h, char *buf, MVMint64 bytes) {
+    MVMIOSyncStreamData *data = (MVMIOSyncStreamData *)h->body.data;
+    uv_write_t *req = MVM_malloc(sizeof(uv_write_t));
+    uv_buf_t write_buf = uv_buf_init(buf, bytes);
+    int r;
+    unsigned int interval_id;
+
+    interval_id = MVM_telemetry_interval_start(tc, "syncstream.write_bytes");
+    uv_ref((uv_handle_t *)data->handle);
+    if ((r = uv_write(req, data->handle, &write_buf, 1, write_cb)) < 0) {
+        uv_unref((uv_handle_t *)data->handle);
+        MVM_free(req);
+        MVM_telemetry_interval_stop(tc, interval_id, "syncstream.write_bytes failed");
+        MVM_exception_throw_adhoc(tc, "Failed to write bytes to stream: %s", uv_strerror(r));
+    }
+    else {
+        MVM_gc_mark_thread_blocked(tc);
+        uv_run(tc->loop, UV_RUN_DEFAULT);
+        MVM_gc_mark_thread_unblocked(tc);
+    }
+    MVM_telemetry_interval_annotate(bytes, interval_id, "written this many bytes");
+    MVM_telemetry_interval_stop(tc, interval_id, "syncstream.write_bytes");
+    data->total_bytes_written += bytes;
+    return bytes;
+}
 
 static void free_on_close_cb(uv_handle_t *handle) {
     MVM_free(handle);
@@ -186,12 +312,12 @@ static const MVMIOSyncReadable sync_readable = { socket_set_separator,
                                                  socket_read_line,
                                                  socket_slurp,
                                                  socket_read_chars,
-                                                 MVM_io_syncstream_read_bytes,
-                                                 MVM_io_syncstream_eof };
+                                                 socket_read_bytes,
+                                                 socket_eof };
 static const MVMIOSyncWritable sync_writable = { socket_write_str,
-                                                 MVM_io_syncstream_write_bytes,
-                                                 MVM_io_syncstream_flush,
-                                                 MVM_io_syncstream_truncate };
+                                                 socket_write_bytes,
+                                                 socket_flush,
+                                                 socket_truncate };
 static const MVMIOSockety            sockety = { socket_connect,
                                                  socket_bind,
                                                  socket_accept };
