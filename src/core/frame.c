5bbe95a747dc247edbf9439ccebac238defb0a38
Jonathan Worthington <jnthn@jnthn.net>
2017-07-03 14:12:17 +0200

    Remove "warm enough to log" concept.
    This would mean we never collected OSR points in the main body of a
    program, which would be a serious missed opportunity.

--- src/core/frame.c
+++ src/core/frame.c
@@ -559,7 +559,7 @@ void MVM_frame_invoke(MVMThreadContext *tc, MVMStaticFrame *static_frame,
         frame->spesh_correlation_id = 0;
         if (static_frame->body.spesh_correlation_id < MVM_SPESH_LOG_LOGGED_ENOUGH) {
             MVMint32 id = (MVMint32)MVM_incr(&(static_frame->body.spesh_correlation_id));
-            if (id >= MVM_SPESH_LOG_WARM_ENOUGH && id < MVM_SPESH_LOG_LOGGED_ENOUGH) {
+            if (id < MVM_SPESH_LOG_LOGGED_ENOUGH) { /* Re-check in case of race. */
                 frame->spesh_correlation_id = id;
                 MVMROOT(tc, static_frame, {
                     MVM_spesh_log_entry(tc, id, static_frame, callsite);
