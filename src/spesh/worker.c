a88767d915ce6c76e6bd7b0105c96bb21f97fb55
Jonathan Worthington <jnthn@jnthn.net>
2017-07-03 17:11:01 +0200

    Record total hit counts frames; dump them.

--- src/spesh/worker.c
+++ src/spesh/worker.c
@@ -6,26 +6,50 @@
 
 /* Enters the work loop. */
 static void worker(MVMThreadContext *tc, MVMCallsite *callsite, MVMRegister *args) {
-    while (1) {
-        MVMObject *log_obj = MVM_repr_shift_o(tc, tc->instance->spesh_queue);
-        if (log_obj->st->REPR->ID == MVM_REPR_ID_MVMSpeshLog) {
-            MVMSpeshLog *sl = (MVMSpeshLog *)log_obj;
-            MVMROOT(tc, sl, {
-                /* TODO process the log */
+    MVMObject *updated_static_frames = MVM_repr_alloc_init(tc,
+        tc->instance->boot_types.BOOTArray);
+    MVMROOT(tc, updated_static_frames, {
+        while (1) {
+            MVMObject *log_obj = MVM_repr_shift_o(tc, tc->instance->spesh_queue);
+            if (log_obj->st->REPR->ID == MVM_REPR_ID_MVMSpeshLog) {
+                MVMSpeshLog *sl = (MVMSpeshLog *)log_obj;
+                MVMROOT(tc, sl, {
+                    /* Update stats, and if we're logging dump each of them. */
+                    tc->instance->spesh_stats_version++;
+                    MVM_spesh_stats_update(tc, sl, updated_static_frames);
+                    if (tc->instance->spesh_log_fh) {
+                        MVMint64 i;
+                        MVMint64 n = MVM_repr_elems(tc, updated_static_frames);
+                        fprintf(tc->instance->spesh_log_fh,
+                            "Statistics Updated\n"
+                            "==================\n"
+                            "%d frames had their statistics updated.\n\n",
+                            (int)n);
+                        for (i = 0; i < n; i++) {
+                            char *dump = MVM_spesh_dump_stats(tc, (MVMStaticFrame* )
+                                MVM_repr_at_pos_o(tc, updated_static_frames, i));
+                            fprintf(tc->instance->spesh_log_fh, "%s==========\n\n", dump);
+                            MVM_free(dump);
+                        }
+                    }
 
-                /* If needed, signal sending thread that it can continue. */
-                if (sl->body.block_mutex) {
-                    uv_mutex_lock(sl->body.block_mutex);
-                    MVM_store(&(sl->body.completed), 1);
-                    uv_cond_signal(sl->body.block_condvar);
-                    uv_mutex_unlock(sl->body.block_mutex);
-                }
-            });
+                    /* Clear updated static frames array. */
+                    MVM_repr_pos_set_elems(tc, updated_static_frames, 0);
+
+                    /* If needed, signal sending thread that it can continue. */
+                    if (sl->body.block_mutex) {
+                        uv_mutex_lock(sl->body.block_mutex);
+                        MVM_store(&(sl->body.completed), 1);
+                        uv_cond_signal(sl->body.block_condvar);
+                        uv_mutex_unlock(sl->body.block_mutex);
+                    }
+                });
+            }
+            else {
+                MVM_panic(1, "Unexpected object sent to specialization worker");
+            }
         }
-        else {
-            MVM_panic(1, "Unexpected object sent to specialization worker");
-        }
-    }
+    });
 }
 
 void MVM_spesh_worker_setup(MVMThreadContext *tc) {
