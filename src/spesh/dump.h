134e3f0484bf737c22ede1bd6f0e42d086ae7735
Jonathan Worthington <jnthn@jnthn.net>
2017-07-05 16:45:38 +0200

    Add provisional spesh arg guard dumper.

--- src/spesh/dump.h
+++ src/spesh/dump.h
@@ -1,2 +1,3 @@
 MVM_PUBLIC char * MVM_spesh_dump(MVMThreadContext *tc, MVMSpeshGraph *g);
 MVM_PUBLIC char * MVM_spesh_dump_stats(MVMThreadContext *tc, MVMStaticFrame *sf);
+MVM_PUBLIC char * MVM_spesh_dump_arg_guard(MVMThreadContext *tc, MVMStaticFrame *sf);
