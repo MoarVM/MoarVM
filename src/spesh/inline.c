e26684982ce8421be93d469f8f9088d7c1bf1248
Jonathan Worthington <jnthn@jnthn.net>
2017-07-21 12:12:24 +0200

    Fix double-decrement of unused args in inlining.
    We decremented usages, then deleted the instruction, but deleting the
    instruction also decrements the usage count. This led to instructions
    being wrongly eliminated. This was certainly a bug before the changes
    involving moving spesh to a thread and so forth; it's not clear how it
    managed to hide.

--- src/spesh/inline.c
+++ src/spesh/inline.c
@@ -808,8 +808,6 @@ static void rewrite_args(MVMThreadContext *tc, MVMSpeshGraph *inliner,
             case MVM_OP_arg_n:
             case MVM_OP_arg_s:
             case MVM_OP_arg_o:
-                MVM_spesh_get_facts(tc, inliner, arg_ins->operands[1])->usages--;
-                /* fallthrough */
             case MVM_OP_argconst_i:
             case MVM_OP_argconst_n:
             case MVM_OP_argconst_s:
