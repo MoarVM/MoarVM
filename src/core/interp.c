9b9bd1bf45a9f6c53baaa224acb3b41bcc4b418d
MasterDuke17 <ddgreen@gmail.com>
2017-10-26 23:08:04 -0400

    Create and JIT coerce_II op
    Implement it with the new MVM_bigint_from_bigint function. This will be
    used in Rakudo's Int.new to create one Int from another, but without any
    mix-ins the source might have.

--- src/core/interp.c
+++ src/core/interp.c
@@ -3525,6 +3525,12 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 cur_op += 4;
                 goto NEXT;
             }
+            OP(coerce_II): {
+                MVMObject *   const type = GET_REG(cur_op, 4).o;
+                GET_REG(cur_op, 0).o = MVM_bigint_from_bigint(tc, type, GET_REG(cur_op, 2).o);
+                cur_op += 6;
+                goto NEXT;
+            }
             OP(coerce_nI): {
                 MVMObject *   const type = GET_REG(cur_op, 4).o;
                 GET_REG(cur_op, 0).o = MVM_bigint_from_num(tc, type, GET_REG(cur_op, 2).n64);
