7f6975a795df15e35e0e2d4900d72d8a9be29062
Nick Logan <nlogan@gmail.com>
2017-10-28 12:51:03 -0400

    Add another missing MVMROOT

--- src/core/bytecode.c
+++ src/core/bytecode.c
@@ -582,8 +582,10 @@ void MVM_bytecode_finish_frame(MVMThreadContext *tc, MVMCompUnit *cu,
         return;
 
     /* Acquire the update mutex on the CompUnit. */
+    MVMROOT(tc, cu, {
     MVMROOT(tc, sf, {
         MVM_reentrantmutex_lock(tc, (MVMReentrantMutex *)cu->body.deserialize_frame_mutex);
+    });
     });
 
     /* Ensure no other thread has done this for us in the mean time. */
