04e291170ec65777780e1342dfba6b69f9c2af03
Jonathan Worthington <jnthn@jnthn.net>
2017-11-30 18:33:08 +0100

    Allow throwpayloadlex to be inlined
    The limitations that prevented this before are now gone.

--- src/core/ops.c
+++ src/core/ops.c
@@ -8893,7 +8893,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         0,
         0,
         0,
-        1,
+        0,
         2,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_int64, MVM_operand_read_reg | MVM_operand_obj }
     },
