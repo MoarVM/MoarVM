4a0a912ef14c13c28e0acdc9be79a5fc1023e6e1
Stefan Seifert <nine@detonation.org>
2018-01-15 19:11:03 +0100

    New spesh-bisect.pl tool
    Finds the first MVM_SPESH_LIMIT value where test program breaks.
    Usage: tools/spesh-bisect.pl perl6 t/foo/bar.t
    Uses the process' return code for deciding whether the run was successful
    or not (like git bisect run)

---
+++ tools/spesh-bisect.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use 5.10.0;
+
+chomp $prog;
+my $min = 1;
+my $max = 100000;
+my $l = 50000;
+while ($min < $l and $l < $max) {
+    $ENV{MVM_SPESH_LIMIT} = $l;
+    say "Trying $l";
+    if (system(@ARGV) != 0) {
+        $max = $l - 1;
+    }
+    else {
+        $min = $l + 1
+    };
+    $l = $min + int(($max - $min) / 2);
+};
+say "MVM_SPESH_LIMIT=$l";
