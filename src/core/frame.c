fd7eefae06a2e36f7c1eb52c0f603188c7214282
Jeremy Studer <studerj1.mail@gmail.com>
2018-02-17 14:34:43 -0500

    Add flag indicating HLL init status on CodeRef
    Add a value that indicates whether the statevar in the coderef has been
    assigned a value by the HLL (flag set via extop on HLL side).
    
    This change is being made to coincide with a Rakudo development
    regarding statevar initialization.
    
    See [RT#102994](https://rt.perl.org/Public/Bug/Display.html?id=102994)

--- src/core/frame.c
+++ src/core/frame.c
@@ -594,8 +594,11 @@ void MVM_frame_invoke(MVMThreadContext *tc, MVMStaticFrame *static_frame,
                         }
                         else {
                             /* Allocate storage for state vars. */
+                            MVMCode *cref = (MVMCode *)frame->code_ref;
+
                             state = (MVMRegister *)MVM_calloc(1, frame->static_info->body.env_size);
-                            ((MVMCode *)frame->code_ref)->body.state_vars = state;
+                            cref->body.state_vars = state;
+                            cref->body.state_vars_is_hll_init = (MVMuint8 *)MVM_calloc(1, numlex);
                             state_act = 1;
 
                             /* Note that this frame should run state init code. */
