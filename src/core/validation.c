d44fa8941e62a075076c66e1e85cff8ed6a21b51
gerd <gp@zimt.uni-siegen.de>
2018-04-06 16:17:35 +0200

    Fix loading bytecode on big endian systems
    We used to check the value before it got endian-switched
    by validate_operand. Move the check after the switch and
    fix the offset to match.

--- src/core/validation.c
+++ src/core/validation.c
@@ -406,9 +406,9 @@ static void validate_operands(Validator *val) {
         case MVM_OP_wval:
         case MVM_OP_wval_wide: {
             validate_reg_operand(val, operands[0]);
-            if (GET_UI16(val->cur_op, 0) >= val->cu->body.num_scs)
-                fail(val, MSG(val, "out of range SC index %u"), GET_UI16(val->cur_op, 0));
             validate_literal_operand(val, operands[1]);
+            if (GET_UI16(val->cur_op, -2) >= val->cu->body.num_scs)
+                fail(val, MSG(val, "out of range SC index %u"), GET_UI16(val->cur_op, -2));
             validate_literal_operand(val, operands[2]);
             break;
         }
