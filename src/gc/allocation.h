876aa90ee7dab173771d70683d21c525cf986465
Stefan Seifert <nine@detonation.org>
2018-04-21 19:49:54 +0200

    Ensure memory blocks in the nursery are aligned where necessary
    Some architectures are really picky about memory alignment (e.g. armhf for 64
    bit floats). We already ensure alignment of fields in structs but if we
    first allocate a memory block with uneven size, the next memory block
    can end up unaligned and thus all field alignments are off. So round up
    the block size. Thanks to robertle++ who debugged this and wrote the
    initial version of the patch.

--- src/gc/allocation.h
+++ src/gc/allocation.h
@@ -1,3 +1,8 @@
+#if !defined(MVM_CAN_UNALIGNED_INT64) || !defined(MVM_CAN_UNALIGNED_NUM64)
+#define MVM_ALIGN_SIZE(size) MVM_ALIGN_SECTION(size)
+#else
+#define MVM_ALIGN_SIZE(size) (size)
+#endif
 void * MVM_gc_allocate_nursery(MVMThreadContext *tc, size_t size);
 void * MVM_gc_allocate_zeroed(MVMThreadContext *tc, size_t size);
 MVMSTable * MVM_gc_allocate_stable(MVMThreadContext *tc, const MVMREPROps *repr, MVMObject *how);
@@ -10,5 +15,5 @@ void MVM_gc_allocate_gen2_default_clear(MVMThreadContext *tc);
 MVM_STATIC_INLINE void * MVM_gc_allocate(MVMThreadContext *tc, size_t size) {
     return tc->allocate_in_gen2
         ? MVM_gc_gen2_allocate_zeroed(tc->gen2, size)
-        : MVM_gc_allocate_nursery(tc, size);
+        : MVM_gc_allocate_nursery(tc, MVM_ALIGN_SIZE(size));
 }
