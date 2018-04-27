f9dfa35ee193f6b49911fb465e070c44e4fc9136
Bart Wiegmans <bartwiegmans@gmail.com>
2018-04-27 11:04:52 +0200

    [JIT] Activate stack walker for current frame
    Look up the current return address based on the stack layout.
    Only works with -fno-omit-frame-pointer.

--- src/jit/interface.c
+++ src/jit/interface.c
@@ -2,26 +2,53 @@
 #include "internal.h"
 
 /* Walk stack to find the current return address in a code frame */
-void * stack_find_return_address_posix(void *base, size_t size, void *top);
-void * stack_find_return_address_win64(void *base, size_t size, void *top);
+void * stack_find_return_address_in_frame_posix(void *base, size_t size, void *top);
+void * stack_find_return_address_in_frame_win64(void *base, size_t size, void *top);
+
+
+MVM_STATIC_INLINE MVMint32 address_within_region(MVMThreadContext *tc, MVMJitCode *code, void *address) {
+    MVMint32 ofs = (char*)address - (char*)code->func_ptr;
+    return (0 <= ofs) && (ofs < code->size);
+}
+
 
 /* Enter the JIT code segment. The label is a continuation point where control
  * is resumed after the frame is properly setup. */
-void MVM_jit_code_enter(MVMThreadContext *tc, MVMCompUnit *cu,
-                        MVMJitCode *code) {
+void MVM_jit_code_enter(MVMThreadContext *tc, MVMJitCode *code, MVMCompUnit *cu) {
     void *label = tc->cur_frame->jit_entry_label;
-    MVMint32 ofs = (char*)label - (char*)code->func_ptr;
-    if (ofs < 0 || ofs >= code->size)
-        MVM_oops(tc, "JIT entry label out of range for code!\n"
-                 "(label %p, func_ptr %p, code size %lui, offset %li, frame_nr %i, seq nr %i)",
-                 label, code->func_ptr, code->size, ((char*)label) - ((char*)code->func_ptr),
-                 tc->cur_frame->sequence_nr, code->seq_nr);
+    if (!address_within_region(tc, code, label)) {
+        MVM_panic(1, "JIT entry label out of range for code!\n"
+                  "(label %p, func_ptr %p, code size %lui, offset %li, frame_nr %i, seq nr %i)",
+                  label, code->func_ptr, code->size,
+                  (char*) label - (char *) code->func_ptr
+                  ,
+                  tc->cur_frame->sequence_nr, code->seq_nr);
+    }
     code->func_ptr(tc, cu, label);
+}
+
+static void * get_current_position(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame) {
+    if (tc->cur_frame == frame) {
+        /* currently on C stack */
+        void *return_address = stack_find_return_address_in_frame_posix(code->func_ptr, code->size, tc->interp_cur_op);
+        if (address_within_region(tc, code, return_address)) {
+            return return_address;
+        } else {
+            /* this can only happen if we need to look up the current position
+             * (e.g. for throwing an exception) while changing the current
+             * frame, e.g. during special unwinding, in which case the
+             * jit_entry_label must be correct */
+            return frame->jit_entry_label;
+        }
+    } else {
+        /* trampolined-out of this frame, so jit_entry_label is correct */
+        return frame->jit_entry_label;
+    }
 }
 
 MVMint32 MVM_jit_code_get_active_deopt_idx(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame) {
     MVMint32 i;
-    void *current_position = frame->jit_entry_label;
+    void *current_position = get_current_position(tc, code, frame);
     for (i = 0; i < code->num_deopts; i++) {
         if (code->labels[code->deopts[i].label] == current_position) {
             break;
@@ -33,7 +60,7 @@ MVMint32 MVM_jit_code_get_active_deopt_idx(MVMThreadContext *tc, MVMJitCode *cod
 MVMint32 MVM_jit_code_get_active_handlers(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame, MVMint32 *handlers_out) {
     MVMint32 i;
     MVMint32 j = 0;
-    void *current_position = frame->jit_entry_label;
+    void *current_position = get_current_position(tc, code, frame);
     for (i = 0; i < code->num_handlers; i++) {
         void *start_label = code->labels[code->handlers[i].start_label];
         void *end_label   = code->labels[code->handlers[i].end_label];
@@ -47,7 +74,7 @@ MVMint32 MVM_jit_code_get_active_handlers(MVMThreadContext *tc, MVMJitCode *code
 MVMint32 MVM_jit_code_get_active_inlines(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame, MVMint32 *inlines_out) {
     MVMint32 i;
     MVMint32 j = 0;
-    void *current_position = frame->jit_entry_label;
+    void *current_position = get_current_position(tc, code, frame);
     for (i = 0; i < code->num_inlines; i++) {
         void *inline_start = code->labels[code->inlines[i].start_label];
         void *inline_end   = code->labels[code->inlines[i].end_label];
