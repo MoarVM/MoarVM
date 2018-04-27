5ee39d65fdb70661ed47bad55b55312a50c5907e
Bart Wiegmans <bartwiegmans@gmail.com>
2018-04-27 11:04:52 +0200

    [JIT] Activate stack walker for current frame
    Look up the current return address based on the stack layout.
    Only works with -fno-omit-frame-pointer.

--- src/jit/interface.h
+++ src/jit/interface.h
@@ -6,7 +6,7 @@ MVM_STATIC_INLINE MVMuint8 * MVM_frame_effective_bytecode(MVMFrame *f) {
     return f->static_info->body.bytecode;
 }
 
-void MVM_jit_code_enter(MVMThreadContext *tc, MVMCompUnit *cu, MVMJitCode * code);
+void MVM_jit_code_enter(MVMThreadContext *tc, MVMJitCode *code, MVMCompUnit *cu);
 MVMint32 MVM_jit_code_get_active_handlers(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame, MVMint32 *handlers);
 MVMint32 MVM_jit_code_get_active_deopt_idx(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame);
 MVMint32 MVM_jit_code_get_active_inlines(MVMThreadContext *tc, MVMJitCode *code, MVMFrame *frame,  MVMint32 *inlines);
