a84b607bd079896d7a0c9118d260d4c0b025f38d
Samantha McVey <samantham@posteo.net>
2018-05-25 18:19:32 -0400

    Add branch predictor hints macros
    MVM_LIKELY and MVM_UNLIKELY are meant to be used in if conditionals
    MVM_EXPECT is can be used on things like switches that don't just expect
    true or false type values.

--- build/setup.pm
+++ build/setup.pm
@@ -287,6 +287,9 @@ our %COMPILERS = (
         noreturnspecifier => '',
         noreturnattribute => '__attribute__((noreturn))',
         formatattribute   => '__attribute__((format(X, Y, Z)))',
+        expect_likely => '__builtin_expect((condition), 1)',
+        expect_unlikely => '__builtin_expect((condition), 0)',
+        expect_condition => '__builtin_expect((condition), (expection))'
     },
 
     clang => {
@@ -311,7 +314,10 @@ our %COMPILERS = (
         noreturnspecifier => '',
         noreturnattribute => '__attribute__((noreturn))',
         formatattribute   => '__attribute__((format(X, Y, Z)))',
-        vectorizerspecifier => '_Pragma ("clang loop vectorize(enable)")'
+        vectorizerspecifier => '_Pragma ("clang loop vectorize(enable)")',
+        expect_likely => '__builtin_expect((condition), 1)',
+        expect_unlikely => '__builtin_expect((condition), 0)',
+        expect_condition => '__builtin_expect((condition), (expection))'
     },
 
     cl => {
