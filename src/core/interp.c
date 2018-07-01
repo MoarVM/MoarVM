9490e5ca668830e10f06ad55e0c7d5b794828107
Jeremy Studer <studerj1.mail@gmail.com>
2018-07-01 00:14:00 -0400

    Add asynclisten exprjit template

--- src/core/interp.c
+++ src/core/interp.c
@@ -4160,7 +4160,7 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
             OP(asynclisten):
                 GET_REG(cur_op, 0).o = MVM_io_socket_listen_async(tc,
                     GET_REG(cur_op, 2).o, GET_REG(cur_op, 4).o, GET_REG(cur_op, 6).s,
-                    GET_REG(cur_op, 8).i64, (int)GET_REG(cur_op, 10).i64, GET_REG(cur_op, 12).o);
+                    GET_REG(cur_op, 8).i64, (MVMint32)GET_REG(cur_op, 10).i64, GET_REG(cur_op, 12).o);
                 cur_op += 14;
                 goto NEXT;
             OP(asyncwritebytes):
