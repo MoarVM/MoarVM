e305da3203c9e1f00e991bfaa72d239620fc00e8
Bart Wiegmans <bartwiegmans@gmail.com>
2018-07-12 13:46:48 +0200

    [JIT] Remove cast information from expr ops table
    We only need this in one place, so might as well define it there.

--- src/jit/expr_ops.h
+++ src/jit/expr_ops.h
@@ -14,66 +14,66 @@
 */
 #define MVM_JIT_EXPR_OPS(_) \
     /* invalid operator */ \
-    _(NOOP, 0, 0, NO_CAST), \
+    _(NOOP, 0, 0), \
     /* wrap value of other operator */ \
-    _(COPY, 1, 0, NO_CAST),   \
+    _(COPY, 1, 0),   \
     /* memory access */ \
-    _(LOAD, 1, 1, NO_CAST),   \
-    _(STORE, 2, 1, NO_CAST), \
-    _(ADDR, 1, 1, UNSIGNED),  \
-    _(IDX, 2, 1, UNSIGNED),   \
+    _(LOAD, 1, 1),   \
+    _(STORE, 2, 1), \
+    _(ADDR, 1, 1),  \
+    _(IDX, 2, 1),   \
     /* constant up to 4 bytes */ \
-    _(CONST, 0, 2, NO_CAST),  \
+    _(CONST, 0, 2),  \
     /* constant pointer (architecture-dependent size) */ \
-    _(CONST_PTR, 0, 1, NO_CAST), \
+    _(CONST_PTR, 0, 1), \
     /* large constant (8 bytes) */ \
-    _(CONST_LARGE, 0, 2, NO_CAST), \
+    _(CONST_LARGE, 0, 2), \
     /* integer comparison */ \
-    _(LT, 2, 0, SIGNED),     \
-    _(LE, 2, 0, SIGNED),     \
-    _(EQ, 2, 0, SIGNED),     \
-    _(NE, 2, 0, SIGNED),     \
-    _(GE, 2, 0, SIGNED),     \
-    _(GT, 2, 0, SIGNED),     \
-    _(NZ, 1, 0, UNSIGNED),   \
-    _(ZR, 1, 0, UNSIGNED),   \
+    _(LT, 2, 0),     \
+    _(LE, 2, 0),     \
+    _(EQ, 2, 0),     \
+    _(NE, 2, 0),     \
+    _(GE, 2, 0),     \
+    _(GT, 2, 0),     \
+    _(NZ, 1, 0),   \
+    _(ZR, 1, 0),   \
     /* flag value */ \
-    _(FLAGVAL, 1, 0, NO_CAST), \
+    _(FLAGVAL, 1, 0), \
     /* force compilation but discard result */ \
-    _(DISCARD, 1, 0, NO_CAST),       \
+    _(DISCARD, 1, 0),       \
     /* type conversion */ \
-    _(CAST, 1, 3, NO_CAST),   \
+    _(CAST, 1, 3),   \
     /* integer arithmetic */ \
-    _(ADD, 2, 0, SIGNED), \
-    _(SUB, 2, 0, SIGNED), \
+    _(ADD, 2, 0), \
+    _(SUB, 2, 0), \
     /* binary operations */ \
-    _(AND, 2, 0, UNSIGNED), \
-    _(OR, 2, 0, UNSIGNED),  \
-    _(XOR, 2, 0, UNSIGNED), \
-    _(NOT, 1, 0, UNSIGNED), \
+    _(AND, 2, 0), \
+    _(OR, 2, 0),  \
+    _(XOR, 2, 0), \
+    _(NOT, 1, 0), \
     /* boolean logic */ \
-    _(ALL, -1, 0, NO_CAST), \
-    _(ANY, -1, 0, NO_CAST), \
+    _(ALL, -1, 0), \
+    _(ANY, -1, 0), \
     /* control operators */ \
-    _(DO, -1, 0, NO_CAST),   \
-    _(DOV, -1, 0, NO_CAST),   \
-    _(WHEN, 2, 0, NO_CAST), \
-    _(IF, 3, 0, NO_CAST),    \
-    _(IFV, 3, 0, NO_CAST), \
-    _(BRANCH, 1, 0, NO_CAST), \
-    _(LABEL, 0, 1, NO_CAST),  \
-    _(MARK, 0, 1, NO_CAST), \
+    _(DO, -1, 0),   \
+    _(DOV, -1, 0),   \
+    _(WHEN, 2, 0), \
+    _(IF, 3, 0),    \
+    _(IFV, 3, 0), \
+    _(BRANCH, 1, 0), \
+    _(LABEL, 0, 1),  \
+    _(MARK, 0, 1), \
     /* call c functions */ \
-    _(CALL, 2, 1, NO_CAST),      \
-    _(CALLV, 2, 0, NO_CAST), \
-    _(ARGLIST, -1, 0, NO_CAST), \
-    _(CARG, 1, 1, NO_CAST),     \
+    _(CALL, 2, 1),      \
+    _(CALLV, 2, 0), \
+    _(ARGLIST, -1, 0), \
+    _(CARG, 1, 1),     \
     /* special constrol structures */ \
-    _(GUARD, 1, 2, NO_CAST),  \
+    _(GUARD, 1, 2),  \
     /* interpreter special variables */ \
-    _(TC, 0, 0, NO_CAST), \
-    _(CU, 0, 0, NO_CAST), \
-    _(LOCAL, 0, 0, NO_CAST), \
-    _(STACK, 0, 0, NO_CAST), \
+    _(TC, 0, 0), \
+    _(CU, 0, 0), \
+    _(LOCAL, 0, 0), \
+    _(STACK, 0, 0), \
     /* End of list marker */ \
-    _(MAX_NODES, 0, 0, NO_CAST)
+    _(MAX_NODES, 0, 0)
