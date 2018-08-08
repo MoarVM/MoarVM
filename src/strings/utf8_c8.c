91d2878f175c415aa4a286a897896e8daf205cef
Zoffix Znet <zoffixznet@users.noreply.github.com>
2018-08-08 13:11:14 -0400

    Document why we're bumping buffer by 1

--- src/strings/utf8_c8.c
+++ src/strings/utf8_c8.c
@@ -468,6 +468,7 @@ MVMuint32 MVM_string_utf8_c8_decodestream(MVMThreadContext *tc, MVMDecodeStream 
     while (cur_bytes && !reached_stopper) {
         /* Set up decode state for this buffer. */
         MVMint32 bytes = cur_bytes->length;
+        /* Space for graphemes we have + 1 grapheme we receive from last buffer */
         state.result = MVM_malloc((bytes + 1) * sizeof(MVMGrapheme32));
         state.orig_codes = MVM_realloc(state.orig_codes,
             sizeof(MVMCodepoint) * (state.orig_codes_pos + bytes));
