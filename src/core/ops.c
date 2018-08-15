981a1805681efd3c2b519fdf748b345ab8f11553
Jonathan Worthington <jnthn@jnthn.net>
2018-08-15 15:25:28 +0200

    Mark [sp_]speshresolve as :invokish

--- src/core/ops.c
+++ src/core/ops.c
@@ -11942,7 +11942,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         0,
         1,
         1,
-        0,
+        1,
         0,
         1,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_str }
@@ -12467,7 +12467,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         1,
         0,
         0,
-        0,
+        1,
         1,
         0,
         { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_str, MVM_operand_uint32, MVM_operand_spesh_slot }
