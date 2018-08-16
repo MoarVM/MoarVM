7277670061a1ee973f7bb72a618ae9b61bdfa6b2
Samantha McVey <samantham@posteo.net>
2018-08-16 23:25:10 +0100

    Make release.sh more clear as to which we want written to MANIFEST
    Also restricts the scope of what is output to MANIFEST to a specific
    section of code and not the entire script.
    
    Indent the for/do loop as well.

--- tools/release.sh
+++ tools/release.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 VERSION=$1
-exec >MANIFEST
-echo MANIFEST
-git ls-files | perl -ne "print unless /^3rdparty\/\w+$/"
-for submod in 3rdparty/libatomicops/ 3rdparty/dyncall/ 3rdparty/libuv/ 3rdparty/dynasm/ 3rdparty/libtommath/ 3rdparty/cmp/; do
-cd $submod
-git ls-files | perl -pe "s{^}{$submod}"
-cd ../..;
-done
+{
+    echo MANIFEST
+    git ls-files | perl -ne "print unless /^3rdparty\/\w+$/"
+    for submod in 3rdparty/libatomicops/ 3rdparty/dyncall/ 3rdparty/libuv/ 3rdparty/dynasm/ 3rdparty/libtommath/ 3rdparty/cmp/; do
+        cd $submod
+        git ls-files | perl -pe "s{^}{$submod}"
+        cd ../..;
+    done
+} > MANIFEST
 [ -d MoarVM-$VERSION ] || ln -s . MoarVM-$VERSION
 perl -pe "s{^}{MoarVM-$VERSION/}" MANIFEST | tar zc -T - -f MoarVM-$VERSION.tar.gz
 rm MoarVM-$VERSION
-
