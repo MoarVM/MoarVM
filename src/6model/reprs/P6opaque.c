183aff234b3a902f3bf334f75af6668fe0e0c99e
Timo Paulssen <timonator@perpetuum-immobile.de>
2018-08-16 07:32:44 +0200

    add type/concrete to box_* spesh, too
    also, no need to get_and_use_facts when we're
    changing the operation that writes that register.

--- src/6model/reprs/P6opaque.c
+++ src/6model/reprs/P6opaque.c
@@ -1413,7 +1413,7 @@ static void spesh(MVMThreadContext *tc, MVMSTable *st, MVMSpeshGraph *g, MVMSpes
         if (repr_data->initialize_slots[0] < 0 && !(st->mode_flags & MVM_FINALIZE_TYPE)) {
             MVMSpeshOperand target   = ins->operands[0];
             MVMSpeshOperand type     = ins->operands[1];
-            MVMSpeshFacts *tgt_facts = MVM_spesh_get_and_use_facts(tc, g, target);
+            MVMSpeshFacts *tgt_facts = MVM_spesh_get_facts(tc, g, target);
 
             ins->info                = MVM_op_get_op(MVM_OP_sp_fastcreate);
             ins->operands            = MVM_spesh_alloc(tc, g, 3 * sizeof(MVMSpeshOperand));
@@ -1622,13 +1622,17 @@ static void spesh(MVMThreadContext *tc, MVMSTable *st, MVMSpeshGraph *g, MVMSpes
             if (embedded_st->REPR->ID == MVM_REPR_ID_P6num) {
                 /* Prepend a fastcreate instruction. */
                 MVMSpeshIns *fastcreate = MVM_spesh_alloc(tc, g, sizeof(MVMSpeshIns));
+                MVMSpeshFacts *tgt_facts = MVM_spesh_get_facts(tc, g, ins->operands[0]);
                 fastcreate->info = MVM_op_get_op(MVM_OP_sp_fastcreate);
                 fastcreate->operands = MVM_spesh_alloc(tc, g, 3 * sizeof(MVMSpeshOperand));
                 fastcreate->operands[0] = ins->operands[0];
-                MVM_spesh_get_facts(tc, g, fastcreate->operands[0])->writer = fastcreate;
+                tgt_facts->writer = fastcreate;
                 fastcreate->operands[1].lit_i16 = st->size;
                 fastcreate->operands[2].lit_i16 = MVM_spesh_add_spesh_slot(tc, g, (MVMCollectable *)st);
                 MVM_spesh_manipulate_insert_ins(tc, bb, ins->prev, fastcreate);
+
+                tgt_facts->flags |= MVM_SPESH_FACT_KNOWN_TYPE | MVM_SPESH_FACT_CONCRETE;
+                tgt_facts->type = st->WHAT;
 
                 /* Change instruction to a bind. */
                 MVM_spesh_usages_delete_by_reg(tc, g, ins->operands[2], ins);
@@ -1646,13 +1650,17 @@ static void spesh(MVMThreadContext *tc, MVMSTable *st, MVMSpeshGraph *g, MVMSpes
             if (embedded_st->REPR->ID == MVM_REPR_ID_P6str) {
                 /* Prepend a fastcreate instruction. */
                 MVMSpeshIns *fastcreate = MVM_spesh_alloc(tc, g, sizeof(MVMSpeshIns));
+                MVMSpeshFacts *tgt_facts = MVM_spesh_get_facts(tc, g, ins->operands[0]);
                 fastcreate->info = MVM_op_get_op(MVM_OP_sp_fastcreate);
                 fastcreate->operands = MVM_spesh_alloc(tc, g, 3 * sizeof(MVMSpeshOperand));
                 fastcreate->operands[0] = ins->operands[0];
-                MVM_spesh_get_facts(tc, g, fastcreate->operands[0])->writer = fastcreate;
+                tgt_facts->writer = fastcreate;
                 fastcreate->operands[1].lit_i16 = st->size;
                 fastcreate->operands[2].lit_i16 = MVM_spesh_add_spesh_slot(tc, g, (MVMCollectable *)st);
                 MVM_spesh_manipulate_insert_ins(tc, bb, ins->prev, fastcreate);
+
+                tgt_facts->flags |= MVM_SPESH_FACT_KNOWN_TYPE | MVM_SPESH_FACT_CONCRETE;
+                tgt_facts->type = st->WHAT;
 
                 /* Change instruction to a bind. */
                 MVM_spesh_usages_delete_by_reg(tc, g, ins->operands[2], ins);
