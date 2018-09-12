36188cea4c53ba0b733ff0954b7eb74b6e8cfd21
Paweł Murias <pawelmurias@gmail.com>
2018-09-12 11:50:31 +0200

    Fix localtime_s use

--- src/platform/win32/time.c
+++ src/platform/win32/time.c
@@ -45,7 +45,8 @@ void MVM_platform_nanosleep(MVMuint64 nanos)
 
 void MVM_platform_decodelocaltime(MVMint64 time, MVMint64 decoded[]) {
     const time_t t = (time_t)time;
-    struct tm *tm = localtime_s(&t, &tm);
+    struct tm tm;
+    localtime_s(&tm, &t);
 
     decoded[0] = tm->tm_sec;
     decoded[1] = tm->tm_min;
