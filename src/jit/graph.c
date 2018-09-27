02815677f8270f5e33dac7196215e631aa5c9864
Bart Wiegmans <bartwiegmans@gmail.com>
2018-09-27 11:40:00 +0200

    [JIT] Log expr tree before tile log
    This is easier for the developer because tile log and expression tree
    are close together.

--- src/jit/graph.c
+++ src/jit/graph.c
@@ -3536,8 +3536,6 @@ static MVMint32 consume_bb(MVMThreadContext *tc, MVMJitGraph *jg,
                 node->u.tree     = tree;
                 tree->seq_nr     = jg->expr_seq_nr++;
                 jg_append_node(jg, node);
-                if (MVM_jit_debug_enabled(tc))
-                    MVM_jit_dump_expr_tree(tc, tree);
             }
             if (iter->ins) {
                 /* something we can't compile yet, or simply an empty tree */
