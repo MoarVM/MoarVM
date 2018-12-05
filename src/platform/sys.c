6455d1f6ac49682c49376296720146713c201cd6
Ben Davies <kaiepi@outlook.com>
2018-12-05 14:26:30 -0400

    Fix double free in platform/sys.c

--- src/platform/sys.c
+++ src/platform/sys.c
@@ -4,9 +4,10 @@
 MVMuint32 MVM_platform_cpu_count(void) {
     int count;
     uv_cpu_info_t *info;
+    int e;
 
-    uv_cpu_info(&info, &count);
-    uv_free_cpu_info(info, count);
+    e = uv_cpu_info(&info, &count);
+    if (e == 0) uv_free_cpu_info(info, count);
 
     return count;
 }
