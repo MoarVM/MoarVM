MoarVM/MoarVM

commit 25b486dee8a0f66a944d4a25279de8e7c315807f

Avoid preserving ->caller unless we really need it
We need it for backtraces and context introspection. Otherwise, it can
go. Preserving it can cause leaks involving taken closures: the taking
of the closure has an ->outer frame that in turn points to a ->caller
and keeps it alive. This was involved in the leak that was reported in
https://github.com/rakudo/rakudo/issues/2803.

src/core/exceptions.c
@@ -360,6 +360,7 @@ static void run_handler(MVMThreadContext *tc, LocatedHandler lh, MVMObject *ex_o
         /* Create active handler record. */
         MVMActiveHandler *ah = MVM_malloc(sizeof(MVMActiveHandler));
         MVMFrame *cur_frame = tc->cur_frame;
+        MVMFrame *pres_frame;
         MVMObject *handler_code;
 
         /* Ensure we have an exception object. */
@@ -369,6 +370,16 @@ static void run_handler(MVMThreadContext *tc, LocatedHandler lh, MVMObject *ex_o
             });
             ((MVMException *)ex_obj)->body.category = category;
             MVM_ASSIGN_REF(tc, &(ex_obj->header), ((MVMException *)ex_obj)->body.payload, payload);
+        }
+
+        /* Preserve the frame caller chain, so we can do backtraces. */
+        pres_frame = ((MVMException *)ex_obj)->body.origin;
+        while (pres_frame) {
+            MVMFrameExtra *extra = MVM_frame_extra(tc, pres_frame);
+            if (extra->caller_info_needed)
+                break;
+            extra->caller_info_needed = 1;
+            pres_frame = pres_frame->caller;
         }
 
         /* Find frame to invoke. */

src/6model/reprs/MVMContext.c
@@ -272,8 +272,9 @@ static const MVMREPROps MVMContext_this_repr = {
 static void snapshot_frame_callees(MVMThreadContext *tc, MVMFrame *f) {
     while (f && f->caller) {
         MVMSpeshCandidate *cand = f->caller->spesh_cand;
+        MVMFrameExtra *extra = MVM_frame_extra(tc, f);
+        extra->caller_info_needed = 1;
         if (cand && cand->num_inlines) {
-            MVMFrameExtra *extra = MVM_frame_extra(tc, f);
             if (cand->jitcode) {
                 if (extra->caller_jit_position)
                     return;

src/core/frame.c
@@ -849,6 +849,7 @@ MVMFrame * MVM_frame_create_for_deopt(MVMThreadContext *tc, MVMStaticFrame *stat
 static MVMuint64 remove_one_frame(MVMThreadContext *tc, MVMuint8 unwind) {
     MVMFrame *returner = tc->cur_frame;
     MVMFrame *caller   = returner->caller;
+    MVMuint32 need_caller;
     MVM_ASSERT_NOT_FROMSPACE(tc, caller);
 
     /* Clear up any extra frame data. */
@@ -856,12 +857,16 @@ static MVMuint64 remove_one_frame(MVMThreadContext *tc, MVMuint8 unwind) {
         MVMFrameExtra *e = returner->extra;
         if (e->continuation_tags)
             MVM_continuation_free_tags(tc, returner);
+        need_caller = e->caller_info_needed;
         /* Preserve the extras if the frame has been used in a ctx operation
          * and marked with caller info. */
         if (!(e->caller_deopt_idx || e->caller_jit_position)) {
             MVM_fixed_size_free(tc, tc->instance->fsa, sizeof(MVMFrameExtra), e);
             returner->extra = NULL;
         }
+    }
+    else {
+        need_caller = 0;
     }
 
     /* Clean up frame working space. */
@@ -888,6 +893,8 @@ static MVMuint64 remove_one_frame(MVMThreadContext *tc, MVMuint8 unwind) {
      * in dynamic scope. */
     else {
         returner->work = NULL;
+        if (!need_caller)
+            returner->caller = NULL;
     }
 
     /* Switch back to the caller frame if there is one. */

src/core/frame.h
@@ -148,6 +148,11 @@ struct MVMFrameExtra {
     MVMRegister *dynlex_cache_reg;
     MVMuint16    dynlex_cache_type;
 
+    /* If we use the ctx op, then we need to preserve the caller chain for
+     * walking. We don't want to do that in the general case, since it can
+     * cause memory leaks in certain patterns of closure use. */
+    MVMuint16 caller_info_needed;
+
     /* If we use the ctx op and we have inlining, we no longer have an
      * immutable chain of callers to walk. When our caller had inlines, we
      * record here in the callee the deopt index or JIT position. Then, when

commit 0c014bfb982ecbec9bdef4e9428c6d6ecb30cb97

Remove premature opt to unbreak backtraces
It seems that this doesn't work out too well in combination with
continuations; will need to look into why, but in the meantime this
fixes the regression in Rakudo `make test` and preserves the leak fix
introduced in 25b486dee8.

src/core/exceptions.c
@@ -376,8 +376,6 @@ static void run_handler(MVMThreadContext *tc, LocatedHandler lh, MVMObject *ex_o
         pres_frame = ((MVMException *)ex_obj)->body.origin;
         while (pres_frame) {
             MVMFrameExtra *extra = MVM_frame_extra(tc, pres_frame);
-            if (extra->caller_info_needed)
-                break;
             extra->caller_info_needed = 1;
             pres_frame = pres_frame->caller;
         }