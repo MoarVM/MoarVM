2911ec7e405f91aa3da658541b3286834fea2b8a
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-07-25 17:10:15 +0200

    Fix GC rooting in MVM_platform_uname
    dogbert17++ for finding the crashes,
    ugexe++ for pointing out the reason behind
    splitting decode and bind_pos calls

--- src/platform/sys.c
+++ src/platform/sys.c
@@ -23,21 +23,25 @@ MVMint64 MVM_platform_total_memory(void) {
 MVMObject * MVM_platform_uname(MVMThreadContext *tc) {
     int           error;
     uv_utsname_t  uname;
-    MVMObject    *result;
+    MVMObject    *result = NULL;
 
     if ((error = uv_os_uname(&uname)) != 0)
         MVM_exception_throw_adhoc(tc, "Unable to uname: %s", uv_strerror(error));
 
     MVMROOT(tc, result, {
-        MVMString *sysname = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.sysname, strlen((char *)uname.sysname));
-        MVMString *release = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.release, strlen((char *)uname.release));
-        MVMString *version = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.version, strlen((char *)uname.version));
-        MVMString *machine = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.machine, strlen((char *)uname.machine));
+        MVMString *sysname;
+        MVMString *release;
+        MVMString *version;
+        MVMString *machine;
+        sysname = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.sysname, strlen((char *)uname.sysname));
+        release = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.release, strlen((char *)uname.release));
+        version = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.version, strlen((char *)uname.version));
+        machine = MVM_string_utf8_decode(tc, tc->instance->VMString, uname.machine, strlen((char *)uname.machine));
         result = MVM_repr_alloc_init(tc, tc->instance->boot_types.BOOTStrArray);
-        MVM_repr_bind_pos_s(tc, result, 0, sysname);
-        MVM_repr_bind_pos_s(tc, result, 1, release);
-        MVM_repr_bind_pos_s(tc, result, 2, version);
-        MVM_repr_bind_pos_s(tc, result, 3, machine);
+        MVM_repr_push_s(tc, result, sysname);
+        MVM_repr_push_s(tc, result, release);
+        MVM_repr_push_s(tc, result, version);
+        MVM_repr_push_s(tc, result, machine);
     });
 
     return result;
