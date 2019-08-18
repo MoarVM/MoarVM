24e2986a1d5f4321fce1fb52c2729be73616fc0f
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-08-18 18:46:48 +0200

    Do heapsnapshot/instrumented data gathering earlier in GC
    The other place it was in allowed other threads to continue
    running while the main thread was doing the data gathering
    for instrumented profiles or the heapsnapshot profiler, which
    caused quite some confusion and some crashes.

--- src/gc/orchestrate.c
+++ src/gc/orchestrate.c
@@ -183,6 +183,11 @@ static void finish_gc(MVMThreadContext *tc, MVMuint8 gen, MVMuint8 is_coordinato
         }
 
         GCDEBUG_LOG(tc, MVM_GC_DEBUG_ORCHESTRATE,
+            "Thread %d run %d : Co-ordinator heapsnapshot or instrumented profiler data output\n");
+        MVM_profile_dump_instrumented_data(tc);
+        MVM_profile_heap_take_snapshot(tc);
+
+        GCDEBUG_LOG(tc, MVM_GC_DEBUG_ORCHESTRATE,
             "Thread %d run %d : Co-ordinator handling fixed-size allocator safepoint frees\n");
         MVM_fixed_size_safepoint(tc, tc->instance->fsa);
         MVM_alloc_safepoint(tc);
@@ -257,11 +262,6 @@ static void finish_gc(MVMThreadContext *tc, MVMuint8 gen, MVMuint8 is_coordinato
             MVM_cas(&other->gc_status, MVMGCStatus_STOLEN, MVMGCStatus_UNABLE);
             MVM_cas(&other->gc_status, MVMGCStatus_INTERRUPT, MVMGCStatus_NONE);
         }
-    }
-
-    if (is_coordinator) {
-        MVM_profile_dump_instrumented_data(tc);
-        MVM_profile_heap_take_snapshot(tc);
     }
 
     /* Signal acknowledgement of completing the cleanup,
