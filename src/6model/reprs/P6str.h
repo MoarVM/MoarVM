a1a21562ff2830d79b9f5785f42ce4ea022059cb
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-10-16 22:57:19 +0200

    introduce MVMP6str_get_str and MVMP6str_set_str

--- src/6model/reprs/P6str.h
+++ src/6model/reprs/P6str.h
@@ -9,3 +9,6 @@ struct MVMP6str {
 
 /* Function for REPR setup. */
 const MVMREPROps * MVMP6str_initialize(MVMThreadContext *tc);
+
+void MVMP6str_set_str(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMString *value);
+MVMString * MVMP6str_get_str(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data);
