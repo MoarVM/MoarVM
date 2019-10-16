f01930b55d4aab0ddab99283cea6980b2adafb4c
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-10-16 23:24:55 +0200

    introduce MVM_VMArray_push

--- src/6model/reprs/VMArray.h
+++ src/6model/reprs/VMArray.h
@@ -78,3 +78,5 @@ struct MVMArrayREPRData {
 void MVM_VMArray_at_pos(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMint64 index, MVMRegister *value, MVMuint16 kind);
 void *MVM_VMArray_find_fast_impl_for_jit(MVMThreadContext *tc, MVMSTable *st, MVMint16 op, MVMuint16 kind);
 void MVM_VMArray_bind_pos(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMint64 index, MVMRegister value, MVMuint16 kind);
+
+void MVM_VMArray_push(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMRegister value, MVMuint16 kind);
