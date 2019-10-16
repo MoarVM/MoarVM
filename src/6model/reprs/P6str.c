83f1ce6d819fd425f6767a830d2299c65e61a63d
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-10-16 22:57:44 +0200

    deserialization: rely on P6str REPR being known
    use MVM_gc_allocate_object instead of MVM_repr_alloc_init,
    and use MVMP6str_set_str instead of REPR_set_str.

--- src/6model/reprs/P6str.c
+++ src/6model/reprs/P6str.c
@@ -125,7 +125,7 @@ const MVMREPROps * MVMP6str_initialize(MVMThreadContext *tc) {
 
 static const MVMREPROps P6str_this_repr = {
     type_object_for,
-    MVM_gc_allocate_object,
+    MVM_gc_allocate_object, /* serialization.c relies on this and the next line */
     NULL, /* initialize */
     copy_to,
     MVM_REPR_DEFAULT_ATTR_FUNCS,
