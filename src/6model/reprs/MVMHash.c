f0f54b6f4ace8341fb4e44f1e2595a24f4c2f543
Timo Paulssen <timonator@perpetuum-immobile.de>
2019-10-16 22:46:31 +0200

    deserialization: rely on MVMHash being the known repr
    by using gc_allocate_object directly rather than going
    through repr_alloc_init (MVMHash has no init reprop) and
    by using MVMHash_bind_key directly instead of going
    via repr_bind_key.

--- src/6model/reprs/MVMHash.c
+++ src/6model/reprs/MVMHash.c
@@ -252,7 +252,7 @@ const MVMREPROps * MVMHash_initialize(MVMThreadContext *tc) {
 
 static const MVMREPROps MVMHash_this_repr = {
     type_object_for,
-    MVM_gc_allocate_object,
+    MVM_gc_allocate_object, /* serialization.c relies on this and the next line */
     NULL, /* initialize */
     copy_to,
     MVM_REPR_DEFAULT_ATTR_FUNCS,
