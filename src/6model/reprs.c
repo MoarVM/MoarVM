8303a74ce3782ea354c96854604e34aeaad97156
Jonathan Worthington <jnthn@jnthn.net>
2020-05-06 14:51:01 +0200

    Stub in new MVMCapture REPR and BOOTCapture
    To be used as part of the new dispatch mechanism, and will eventually
    take over from the current MVMCallCapture (which will, when it's no
    longer needed, be removed).

--- src/6model/reprs.c
+++ src/6model/reprs.c
@@ -285,6 +285,7 @@ void MVM_repr_initialize_registry(MVMThreadContext *tc) {
     register_core_repr(SpeshLog);
     register_core_repr(StaticFrameSpesh);
     register_core_repr(SpeshPluginState);
+    register_core_repr(Capture);
 
     tc->instance->num_reprs = MVM_REPR_CORE_COUNT;
 }
