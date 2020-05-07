1e6fbdc78aad44abdf346cc9c00a7ebebd3e86fa
Jonathan Worthington <jnthn@jnthn.net>
2020-05-07 16:09:54 +0200

    Implement looking up dispatchers in registry
    Plus add GC marking of the dispatcher registry.

--- src/disp/boot.c
+++ src/disp/boot.c
@@ -1,7 +1,21 @@
 #include "moar.h"
 
+/* Wraps a C function into a BOOTCCode object. */
+static MVMObject * wrap(MVMThreadContext *tc, void (*func) (MVMThreadContext *, MVMArgs)) {
+    MVMObject *BOOTCCode = tc->instance->boot_types.BOOTCCode;
+    MVMObject *code_obj = REPR(BOOTCCode)->allocate(tc, STABLE(BOOTCCode));
+    ((MVMCFunction *)code_obj)->body.func = func;
+    return code_obj;
+}
+
+
+/* The boot-value dispatcher returns the first positional argument of the
+ * incoming argument capture. */
+static void boot_value(MVMThreadContext *tc, MVMArgs arg_info) {
+    MVM_panic(1, "boot-value dispatcher NYI");
+}
+
 /* Gets the MVMCFunction object wrapping the boot value dispatcher. */
 MVMObject * MVM_disp_boot_value_dispatch(MVMThreadContext *tc) {
-    /* TODO */
-    return NULL;
+    return wrap(tc, boot_value);
 }
