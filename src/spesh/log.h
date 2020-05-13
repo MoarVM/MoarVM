42ff7b197aceec189ebe1fd9fd4dc87198f34b44
Jonathan Worthington <jnthn@jnthn.net>
2020-05-13 15:52:23 +0200

    Use inline cache for spesh of getlexstatic
    Eliminating the logging approach that used to be used for this along the
    way. We now also introduce a specialized version of the op that can take
    advantage of the inline cache and is inlineable, meaning that unresolved
    getlexstatic_o instructions are no longer an inlining blocker, which may
    help code that would otherwise be possible to inline except for, for
    instance, an unused `die` path.

--- src/spesh/log.h
+++ src/spesh/log.h
@@ -42,7 +42,6 @@ void MVM_spesh_log_entry(MVMThreadContext *tc, MVMint32 cid, MVMStaticFrame *sf,
         MVMCallsite *cs, MVMRegister *args);
 void MVM_spesh_log_osr(MVMThreadContext *tc);
 void MVM_spesh_log_type(MVMThreadContext *tc, MVMObject *value);
-void MVM_spesh_log_static(MVMThreadContext *tc, MVMObject *value);
 void MVM_spesh_log_decont(MVMThreadContext *tc, MVMuint8 *prev_op, MVMObject *value);
 void MVM_spesh_log_invoke_target(MVMThreadContext *tc, MVMObject *invoke_target,
     MVMuint16 was_multi);
