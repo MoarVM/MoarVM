57918ffa544017dd1c51fe9bdd179e1798e03f45
Jonathan Worthington <jnthn@jnthn.net>
2020-05-14 15:14:39 +0200

    Make all frames have a call stack record
    Even those that get heap promoted now leave one behind. For now, this
    doesn't achieve anything particularly new, aside from cleaning up some
    aspects of call stack region management. However, it also paves the way
    for putting disptchers and flattening data on the call stack (and having
    them automatically get dropped when we return from the things that they
    dispatched to).
    
    This isn't a complete solution. NQP builds and passes tests only with
    spesh disabled, because deopt and OSR are not yet updated (although it
    is possible OSR will not need changes for now). Somehow, continuation
    tests also pass, however underneath things are surely somewhat busted.

--- src/core/threadcontext.c
+++ src/core/threadcontext.c
@@ -34,8 +34,8 @@ MVMThreadContext * MVM_tc_create(MVMThreadContext *parent, MVMInstance *instance
     /* The fixed size allocator also keeps pre-thread state. */
     MVM_fixed_size_create_thread(tc);
 
-    /* Allocate an initial call stack region for the thread. */
-    MVM_callstack_region_init(tc);
+    /* Allocate a call stack for the thread. */
+    MVM_callstack_init(tc);
 
     /* Initialize random number generator state. */
     MVM_proc_seed(tc, (MVM_platform_now() / 10000) * MVM_proc_getpid(tc));
@@ -114,8 +114,8 @@ void MVM_tc_destroy(MVMThreadContext *tc) {
     /* Destory the per-thread fixed size allocator state. */
     MVM_fixed_size_destroy_thread(tc);
 
-    /* Destroy all callstack regions. */
-    MVM_callstack_region_destroy_all(tc);
+    /* Destroy the callstack, releasing all allocated memory. */
+    MVM_callstack_destroy(tc);
 
     /* Free the thread-specific storage */
     MVM_free(tc->gc_work);
