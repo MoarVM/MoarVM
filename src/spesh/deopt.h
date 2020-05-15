9cf957efa22dfb9eea61f33b2fd10e88c3cc0c3f
Jonathan Worthington <jnthn@jnthn.net>
2020-05-15 16:17:51 +0200

    Perform global deopt lazily as we unwind callstack
    This is a bit more CPU cache friendly as it means we don't need to go
    looking up a bunch of stuff relating to a frame down the stack, only the
    record itself. The primary reason for this, however, is that it means we
    shall only have to perform uninlining at the very top of the call stack,
    which greatly eases the process (since it's trivial to create records at
    the top, but would be very fraught, and quite costly, to rewrite the
    whole stack at the point of a global deopt). The update of uninlining is
    still to come.

--- src/spesh/deopt.h
+++ src/spesh/deopt.h
@@ -1,3 +1,4 @@
 void MVM_spesh_deopt_all(MVMThreadContext *tc);
 void MVM_spesh_deopt_one(MVMThreadContext *tc, MVMuint32 deopt_idx);
 MVMint32 MVM_spesh_deopt_find_inactive_frame_deopt_idx(MVMThreadContext *tc, MVMFrame *f);
+void MVM_spesh_deopt_during_unwind(MVMThreadContext *tc);
