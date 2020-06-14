b4b045de40aed2d94efe4aa2cd6cb5b3650614b3
Nicholas Clark <nick@ccl4.org>
2020-06-14 13:52:34 +0000

    Add MVMIndexHashTable, for hash lookups into an array of MVMString*.
    Convert lexical_names to MVMIndexHashTable, lexical_names_list to MVMString *,
    and eliminate struct MMVMLexicalRegistry. MVM_get_lexical_by_name now returns
    type MVMuint32, with the sentinel value MVM_INDEX_HASH_NOT_FOUND to indicate
    "not found". Previously it returned *MMVMLexicalRegistry, or NULL for not
    found.

--- src/6model/serialization.c
+++ src/6model/serialization.c
@@ -1247,7 +1247,7 @@ static void serialize_context(MVMThreadContext *tc, MVMSerializationWriter *writ
 
     /* Grab lexpad, which we'll serialize later on. */
     MVMStaticFrame *sf   = frame->static_info;
-    MVMLexicalRegistry **lexnames = sf->body.lexical_names_list;
+    MVMString **lexnames = sf->body.lexical_names_list;
 
     /* Locate the static code ref this context points to. */
     MVMObject *static_code_ref = closure_to_static_code_ref(tc, frame->code_ref, 1);
@@ -1288,7 +1288,7 @@ static void serialize_context(MVMThreadContext *tc, MVMSerializationWriter *writ
     /* Serialize lexicals. */
     MVM_serialization_write_int(tc, writer, sf->body.num_lexicals);
     for (i = 0; i < sf->body.num_lexicals; i++) {
-        MVM_serialization_write_str(tc, writer, lexnames[i]->key);
+        MVM_serialization_write_str(tc, writer, lexnames[i]);
         switch (sf->body.lexical_types[i]) {
             case MVM_reg_int8:
             case MVM_reg_int16:
