0888383a089516f745cadce53ea3528885891a4e
Jonathan Worthington <jnthn@jnthn.net>
2020-06-18 15:42:07 +0200

    Add missing inline cache GC marking

--- src/disp/inline_cache.c
+++ src/disp/inline_cache.c
@@ -623,9 +623,21 @@ void MVM_disp_inline_cache_mark(MVMThreadContext *tc, MVMDispInlineCache *cache,
                     (MVMDispInlineCacheEntryMonomorphicDispatch *)entry;
                 MVM_disp_program_mark(tc, mono->dp, worklist);
             }
+            else if (entry->run_dispatch == dispatch_monomorphic_flattening) {
+                MVMDispInlineCacheEntryMonomorphicDispatchFlattening *mono =
+                    (MVMDispInlineCacheEntryMonomorphicDispatchFlattening *)entry;
+                MVM_disp_program_mark(tc, mono->dp, worklist);
+            }
             else if (entry->run_dispatch == dispatch_polymorphic) {
                 MVMDispInlineCacheEntryPolymorphicDispatch *poly =
                     (MVMDispInlineCacheEntryPolymorphicDispatch *)entry;
+                MVMuint32 i;
+                for (i = 0; i < poly->num_dps; i++)
+                    MVM_disp_program_mark(tc, poly->dps[i], worklist);
+            }
+            else if (entry->run_dispatch == dispatch_polymorphic_flattening) {
+                MVMDispInlineCacheEntryPolymorphicDispatchFlattening *poly =
+                    (MVMDispInlineCacheEntryPolymorphicDispatchFlattening *)entry;
                 MVMuint32 i;
                 for (i = 0; i < poly->num_dps; i++)
                     MVM_disp_program_mark(tc, poly->dps[i], worklist);
