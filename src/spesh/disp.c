37efc1fc62cab1437cd6a131234c9e7796207b11
Jonathan Worthington <jnthn@jnthn.net>
2020-06-18 18:00:44 +0200

    Analyze dispatch instructions, prepare for opts
    This look at the statistics for a given type tuple and decides if the
    dispatch is never hit, monomorphic, or polymorphic. Note that this is
    not the same thing as looking at the callsite state, since that is not
    correlated with particular incoming type tuples. With this, we also know
    we can't properly handle dispatch, meaning the SEGVs are replaced with
    panics.

--- src/spesh/disp.c
+++ src/spesh/disp.c
@@ -38,3 +38,116 @@ MVMOpInfo * MVM_spesh_disp_create_dispatch_op_info(MVMThreadContext *tc, MVMSpes
 
     return dispatch_info;
 }
+
+/* Hit count of an outcome, used for analizing how to optimize the dispatch. */
+typedef struct {
+    MVMuint32 outcome;
+    MVMuint32 hits;
+} OutcomeHitCount;
+
+/* Rewrite a dispatch instruction into an sp_dispatch one, resolving only the
+ * static frame and inline cache offset. */
+static void rewrite_to_sp_dispatch(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshIns *ins,
+        MVMuint32 bytecode_offset) {
+    MVM_panic(1, "rewrite to sp_dispatch_* is NYI");
+}
+
+/* Rewrite an unhit dispatch instruction. */
+static void rewrite_unhit(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshIns *ins,
+        MVMuint32 bytecode_offset) {
+    MVM_spesh_graph_add_comment(tc, g, ins, "Never dispatched");
+    rewrite_to_sp_dispatch(tc, g, ins, bytecode_offset);
+}
+
+/* Rewrite a dispatch instruction that is considered monomorphic. */
+static void rewrite_monomorphic(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshIns *ins,
+        MVMuint32 bytecode_offset, MVMuint32 outcome) {
+    // TODO
+    MVM_spesh_graph_add_comment(tc, g, ins, "Deemed monomorphic (outcome %d)", outcome);
+    rewrite_to_sp_dispatch(tc, g, ins, bytecode_offset);
+}
+
+/* Rewrite a dispatch instruction that is polymorphic or megamorphic. */
+static void rewrite_polymorphic(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshIns *ins,
+        MVMuint32 bytecode_offset, OutcomeHitCount *outcomes, MVMuint32 num_outcomes) {
+    // TODO
+    MVM_spesh_graph_add_comment(tc, g, ins, "Deemed polymorphic");
+    rewrite_to_sp_dispatch(tc, g, ins, bytecode_offset);
+}
+
+/* Drives the overall process of optimizing a dispatch instruction. The instruction
+ * will always recieve some transformation, even if it's simply to sp_dispatch_*,
+ * which pre-resolves the inline cache (and so allows inlining of code that still
+ * wants to reference the inline cache of the place it came from). */
+static int compare_hits(const void *a, const void *b) {
+    return ((OutcomeHitCount *)b)->hits - ((OutcomeHitCount *)a)->hits;
+}
+void MVM_spesh_disp_optimize(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshPlanned *p,
+        MVMSpeshIns *ins) {
+    /* Locate the inline cache bytecode offset. There must always be one. */
+    MVMSpeshAnn *ann = ins->annotations;
+    while (ann) {
+        if (ann->type == MVM_SPESH_ANN_CACHED)
+            break;
+        ann = ann->next;
+    }
+    if (!ann)
+        MVM_oops(tc, "Dispatch specialization could not find bytecode offset for dispatch instruction");
+    MVMuint32 bytecode_offset = ann->data.bytecode_offset;
+
+    /* Find any statistics we have on what outcomes were selected by the
+     * dispatcher and aggregate them. */
+    MVM_VECTOR_DECL(OutcomeHitCount, outcome_hits);
+    MVM_VECTOR_INIT(outcome_hits, 0);
+    MVMuint32 total_hits = 0;
+    MVMuint32 i;
+    for (i = 0; i < p->num_type_stats; i++) {
+        MVMSpeshStatsByType *ts = p->type_stats[i];
+        MVMuint32 j;
+        for (j = 0; j < ts->num_by_offset; j++) {
+            if (ts->by_offset[j].bytecode_offset == bytecode_offset) {
+                /* We found some stats at the offset of the dispatch. Count the
+                 * hits. */
+                MVMuint32 k;
+                for (k = 0; k < ts->by_offset[j].num_plugin_guards; k++) {
+                    MVMSpeshStatsPluginGuardCount *outcome_count =
+                            &(ts->by_offset[j].plugin_guards[k]);
+                    MVMuint32 l;
+                    MVMuint32 found = 0;
+                    for (l = 0; l < MVM_VECTOR_ELEMS(outcome_hits); l++) {
+                        if (outcome_hits[l].outcome == outcome_count->guard_index) {
+                            outcome_hits[l].hits += outcome_count->count;
+                            found = 1;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        OutcomeHitCount ohc = {
+                            .outcome = outcome_count->guard_index,
+                            .hits = outcome_count->count
+                        };
+                        MVM_VECTOR_PUSH(outcome_hits, ohc);
+                    }
+                    total_hits += outcome_count->count;
+                }
+                break;
+            }
+        }
+    }
+    qsort(outcome_hits, MVM_VECTOR_ELEMS(outcome_hits), sizeof(OutcomeHitCount), compare_hits);
+
+    /* If there are no hits, we can only rewrite it to sp_dispatch. */
+    if (MVM_VECTOR_ELEMS(outcome_hits) == 0)
+        rewrite_unhit(tc, g, ins, bytecode_offset);
+
+    /* If there's one hit, *or* the top hit has > 99% of the total hits, then we
+     * rewrite it to monomorphic. */
+    else if ((100 * outcome_hits[1].hits) / total_hits >= 99)
+        rewrite_monomorphic(tc, g, ins, bytecode_offset, outcome_hits[0].outcome);
+
+    /* Otherwise, it's polymoprhic or megamorphic. */
+    else
+        rewrite_polymorphic(tc, g, ins, bytecode_offset, outcome_hits, MVM_VECTOR_ELEMS(outcome_hits));
+
+    MVM_VECTOR_DESTROY(outcome_hits);
+}
--- src/spesh/disp.h
+++ src/spesh/disp.h
@@ -1,2 +1,3 @@
 MVMOpInfo * MVM_spesh_disp_create_dispatch_op_info(MVMThreadContext *tc, MVMSpeshGraph *g,
         MVMOpInfo *base_info, MVMCallsite *cs);
+void MVM_spesh_disp_optimize(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshPlanned *p, MVMSpeshIns *ins);
