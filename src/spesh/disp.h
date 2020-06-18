1515faf5580eb3baaee935daf8f885b08b20180a
Jonathan Worthington <jnthn@jnthn.net>
2020-06-18 18:29:44 +0200

    Rewrite dispatch_* into sp_dispatch_*
    The latter aren't yet implemented, but glancing the spesh log suggests
    the transformation is either correct or at least somewhat close to
    correct.

--- src/spesh/disp.h
+++ src/spesh/disp.h
@@ -1,3 +1,3 @@
 MVMOpInfo * MVM_spesh_disp_create_dispatch_op_info(MVMThreadContext *tc, MVMSpeshGraph *g,
-        MVMOpInfo *base_info, MVMCallsite *cs);
+        const MVMOpInfo *base_info, MVMCallsite *cs);
 void MVM_spesh_disp_optimize(MVMThreadContext *tc, MVMSpeshGraph *g, MVMSpeshPlanned *p, MVMSpeshIns *ins);
