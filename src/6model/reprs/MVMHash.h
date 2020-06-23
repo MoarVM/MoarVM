906c3be2fdf289bacbe0365c1d8c3851af7035c4
Nicholas Clark <nick@ccl4.org>
2020-06-23 13:42:15 +0000

    Remove all of the UT hash functional macros (and most of uthash.h)
    Also remove MVMUnicodeNameRegistry and MVMUnicodeGraphemeNameRegistry,
    which are no longer needed.
    
    What remains in uthash.h relates to types, siphash, instance->hashSecrets
    and logic we will need to re-instate hash randomisation at the end of the
    migration.
    
    For now, the UT Hash code remains in the 4 new hash implementations.

--- src/6model/reprs/MVMHash.h
+++ src/6model/reprs/MVMHash.h
@@ -18,16 +18,5 @@ struct MVMHash {
 /* Function for REPR setup. */
 const MVMREPROps * MVMHash_initialize(MVMThreadContext *tc);
 
-#define MVM_HASH_DESTROY(tc, hash_handle, hashentry_type, head_node) do { \
-    hashentry_type *current, *tmp; \
-    HASH_ITER_FAST(tc, hash_handle, head_node, current, { \
-        if (current != head_node) \
-            MVM_free(current); \
-    }); \
-    tmp = head_node; \
-    HASH_CLEAR(tc, hash_handle, head_node); \
-    MVM_free(tmp); \
-} while (0)
-
 void MVMHash_at_key(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMObject *key_obj, MVMRegister *result, MVMuint16 kind);
 void MVMHash_bind_key(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMObject *key_obj, MVMRegister value, MVMuint16 kind);
