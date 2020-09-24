56d29b0bfad518cfdcd6017643ce4fbea03596a2
Jonathan Worthington <jnthn@jnthn.net>
2020-09-24 15:14:53 +0200

    Stub in boot-resume[-caller]

--- src/disp/boot.h
+++ src/disp/boot.h
@@ -2,3 +2,5 @@ MVMObject * MVM_disp_boot_constant_dispatch(MVMThreadContext *tc);
 MVMObject * MVM_disp_boot_value_dispatch(MVMThreadContext *tc);
 MVMObject * MVM_disp_boot_code_constant_dispatch(MVMThreadContext *tc);
 MVMObject * MVM_disp_boot_syscall_dispatch(MVMThreadContext *tc);
+MVMObject * MVM_disp_boot_resume_dispatch(MVMThreadContext *tc);
+MVMObject * MVM_disp_boot_resume_caller_dispatch(MVMThreadContext *tc);
