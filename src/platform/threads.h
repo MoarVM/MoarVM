03d334f111e2fa6a7628119e9eaf84cc74d0b1dc
Timo Paulssen <timonator@perpetuum-immobile.de>
2020-11-05 19:36:35 +0100

    we need to import pthread on windows to get a type

--- src/platform/threads.h
+++ src/platform/threads.h
@@ -11,6 +11,9 @@
 #if defined _WIN32
 #define MVM_platform_thread_exit(status) ExitThread(0)
 #define MVM_platform_thread_id() (MVMint64)GetCurrentThreadId()
+#if defined MVM_HAS_PTHREAD_SETNAME_NP
+#include <pthread.h>
+#endif
 #else
 #define MVM_platform_thread_exit(status) pthread_exit(status)
 #define MVM_platform_thread_id() (MVMint64)uv_thread_self()
