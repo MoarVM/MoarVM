4aa1bc88b9295ee51e41ef7146d67e00b08f088e
Jonathan Worthington <jnthn@jnthn.net>
2021-01-28 16:18:26 +0100

    Refactor resumption resolution
    This approach finds all the needed pieces at once, will let us keep the
    decision about precisely which resumption we're doing in one place (once
    things come that far), and is suitable for both the record and playback
    phase.

--- src/disp/resume.h
+++ src/disp/resume.h
@@ -1,4 +1,14 @@
-MVMuint32 MVM_disp_resume_find_topmost(MVMThreadContext *tc, MVMDispProgram **found_disp_program,
-        MVMCallStackRecord **found_record);
-MVMObject * MVM_disp_resume_init_capture(MVMThreadContext *tc, MVMCallStackRecord *record,
-        MVMuint32 resumption_idx);
+/* Information about a dispatch resumption. */
+struct MVMDispResumptionData {
+    /* The dispatch program that is being resumed. */
+    MVMDispProgram *dp;
+
+    /* The initial arguments to the dispatch (that is, the root one, not
+     * the resumption). */
+    MVMArgs *initial_arg_info;
+
+    /* The particular resumption that we are resuming here. */
+    MVMDispProgramResumption *resumption;
+};
+
+MVMuint32 MVM_disp_resume_find_topmost(MVMThreadContext *tc, MVMDispResumptionData *data);
