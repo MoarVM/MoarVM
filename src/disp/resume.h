dd9bf7807406d2286f92f6aafe9de8a2d26cf135
Jonathan Worthington <jnthn@jnthn.net>
2021-01-29 18:54:32 +0100

    Add a place to store resumption state
    Along with the memory mangement of it. This is never yet set up. The
    design is such that we should never need to do extra allocations in the
    case there's only a single level of resumption, which should be the most
    common case anyway.

--- src/disp/resume.h
+++ src/disp/resume.h
@@ -1,4 +1,5 @@
-/* Information about a dispatch resumption. */
+/* Information about a dispatch resumption. This is held in the record of the
+ * resumption itself. */
 struct MVMDispResumptionData {
     /* The dispatch program that is being resumed. */
     MVMDispProgram *dp;
@@ -11,6 +12,25 @@ struct MVMDispResumptionData {
     MVMDispProgramResumption *resumption;
 };
 
+/* Dispatch resumption state. This is held in the record of the dispatch that
+ * was resumed. The design is such as to be zero allocation when there is only
+ * a single level of resumable dispatch; if there are more, we point to a
+ * linked list of them. */
+struct MVMDispResumptionState {
+    /* The dispatcher that this is the state for. */
+    MVMDispDefinition *disp;
+
+    /* The state. */
+    MVMObject *state;
+
+    /* State of another dispatcher. */
+    MVMDispResumptionState *next;
+};
+
 MVMuint32 MVM_disp_resume_find_topmost(MVMThreadContext *tc, MVMDispResumptionData *data);
 MVMRegister MVM_disp_resume_get_init_arg(MVMThreadContext *tc, MVMDispResumptionData *data,
         MVMuint32 arg_idx);
+void MVM_disp_resume_mark_resumption_state(MVMThreadContext *tc, MVMDispResumptionState *res_state,
+        MVMGCWorklist *worklist, MVMHeapSnapshotState *snapshot);
+void MVM_disp_resume_destroy_resumption_state(MVMThreadContext *tc,
+        MVMDispResumptionState *res_state);
