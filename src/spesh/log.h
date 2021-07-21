b11ca37015654c35bbf551b9ffbd6a7ae6474522
Jonathan Worthington <jnthn@jnthn.net>
2021-07-21 17:51:00 +0200

    Update spesh frame entry logging for new convs
    But don't switch it on yet, otherwise we'll quickly crash and burn due
    to lots of other spesh updates that are needed.

--- src/spesh/log.h
+++ src/spesh/log.h
@@ -39,7 +39,7 @@ void MVM_spesh_log_initialize_thread(MVMThreadContext *tc, MVMint32 main_thread)
 MVMSpeshLog * MVM_spesh_log_create(MVMThreadContext *tc, MVMThread *target_thread);
 void MVM_spesh_log_new_compunit(MVMThreadContext *tc);
 void MVM_spesh_log_entry(MVMThreadContext *tc, MVMint32 cid, MVMStaticFrame *sf,
-        MVMCallsite *cs, MVMRegister *args);
+        MVMArgs args);
 void MVM_spesh_log_osr(MVMThreadContext *tc);
 void MVM_spesh_log_type(MVMThreadContext *tc, MVMObject *value);
 void MVM_spesh_log_decont(MVMThreadContext *tc, MVMuint8 *prev_op, MVMObject *value);
