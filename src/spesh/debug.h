a6e7b00047e407dd638762d9262954e31f322a59
Timo Paulssen <timonator@perpetuum-immobile.de>
2021-09-16 19:36:44 +0200

    put "skip:1234" lines in spesh log to allow fast segmentation
    every skip line leads to the previous skip line, and the lines
    occur between individual specializations, and between stats
    updates and spesh plans

--- src/spesh/debug.h
+++ src/spesh/debug.h
@@ -1,5 +1,6 @@
 void MVM_spesh_debug_printf(MVMThreadContext *tc, const char *format, ...);
 void MVM_spesh_debug_flush(MVMThreadContext *tc);
+size_t MVM_spesh_debug_tell(MVMThreadContext *tc);
 
 MVM_STATIC_INLINE MVMint32 MVM_spesh_debug_enabled(MVMThreadContext *tc) {
     return tc->instance->spesh_log_fh != NULL &&
