8e197406f5fed7d69262322b60322c11ae114cea
Jonathan Worthington <jnthn@jnthn.net>
2021-10-27 13:26:31 +0200

    Eliminate redundant checks/logic during return
    Thanks to having a callstack record type to indicate the boundary of a
    nested runloop (used in NativeCall callbacks), we no longer need to
    explicitly check for the threaded interpreter boundary anywhere. We can
    return NULL from the callstack unwind function, upon encountering the
    native call boundary, to indicate interpreter exit instead. This gets
    rid of a bunch of logic in remove_one_frame, and a little logic in
    exit_frame.

--- src/core/frame.c
+++ src/core/frame.c
@@ -935,24 +935,7 @@ static MVMuint64 remove_one_frame(MVMThreadContext *tc, MVMuint8 unwind) {
     }
     if (thunked)
         return 1;
-
-    /* Switch back to the caller frame if there is one. */
-    if (caller && (returner != tc->thread_entry_frame || tc->nested_interpreter)) {
-        if (returner == tc->thread_entry_frame && tc->cur_frame == caller) {
-            tc->cur_frame = NULL;
-            return 0;
-        }
-        else {
-            /* We're either somewhere in a nested call or already up in the top
-               most frame but still need to run some finalizer, so keep the
-               runloop running */
-            return 1;
-        }
-    }
-    else {
-        tc->cur_frame = NULL;
-        return 0;
-    }
+    return caller != NULL;
 }
 
 /* Attempt to return from the current frame. Returns non-zero if we can,
