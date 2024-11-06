91feafbe088fb3abc3d0f768313843992a47206f
Patrick Böker <patrick.boeker@clarisights.com>
2024-11-06 22:16:07 +0100

    Implement a pty-resize syscall

--- src/io/procops.h
+++ src/io/procops.h
@@ -27,6 +27,7 @@ MVMObject * MVM_proc_clargs(MVMThreadContext *tc);
 MVMString * MVM_executable_name(MVMThreadContext *tc);
 void MVM_proc_getrusage(MVMThreadContext *tc, MVMObject *result);
 MVMint64 MVM_proc_fork(MVMThreadContext *tc);
+char *MVM_proc_resize_pty(MVMThreadContext *tc, MVMOSHandle *h, int cols, int rows);
 void MVM_proc_pty_spawn(char *prog, char *argv[]);
 
 #ifdef _WIN32
