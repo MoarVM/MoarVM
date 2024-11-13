2891ad5bd48638b67886f9e82a1193c44cccfd33
Patrick Böker <patrick.boeker@posteo.de>
2024-11-13 19:08:21 +0100

    Fix longjmp on Windows
    See https://blog.lazym.io/2020/09/21/Unicorn-Devblog-setjmp-longjmp-on-Windows/
    for a good explanation on what we're doing here.

--- src/platform/setjmp.h
+++ src/platform/setjmp.h
@@ -3,16 +3,25 @@
  * cygx++ for fix */
 #ifdef __MINGW32__
 #  ifndef USE_NO_MINGW_SETJMP_TWO_ARGS
-#    undef setjmp
 #    ifndef _INC_SETJMPEX
 #      ifdef _WIN64
-#        define setjmp(BUF) _setjmp((BUF), NULL)
+#        define MVM_setjmp(BUF) _setjmp((BUF), NULL)
 #      else
-#        define setjmp(BUF) _setjmp3((BUF), NULL)
+#        define MVM_setjmp(BUF) _setjmp3((BUF), NULL)
 #      endif
 #    else
-#      define setjmp(BUF) _setjmpex((BUF), NULL)
-#      define setjmpex(BUF) _setjmpex((BUF), NULL)
+#      define MVM_setjmp(BUF) _setjmpex((BUF), NULL)
+#      define MVM_setjmpex(BUF) _setjmpex((BUF), NULL)
+#    endif
+#  endif
+#else
+#  ifdef _MSC_VER
+#    if defined _M_X64
+#      define MVM_setjmp(BUF) do { setjmp((BUF)); tc->interp_jump[0].Part[0] = 0; } while (0)
+#    elif defined _M_ARM
+#      define MVM_setjmp(BUF) do { setjmp((BUF)); tc->interp_jump[0] = 0; } while (0)
+#    elif defined _M_ARM64
+#      define MVM_setjmp(BUF) do { setjmp((BUF)); tc->interp_jump[0] = 0; } while (0)
 #    endif
 #  endif
 #endif
