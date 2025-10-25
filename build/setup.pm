d23246b6aafa026b013454bba01079326543f04b
Daniel Green <ddgreen@gmail.com>
2025-10-24 20:58:27 -0400

    What about manually adding -fPIC?

--- build/setup.pm
+++ build/setup.pm
@@ -51,7 +51,7 @@ my %TP_UV = (
 my %TP_LF = (
     name  => 'ffi/install/lib/libffi',
     path  => '3rdparty',
-    rule  => 'cd 3rdparty/libffi && ./autogen.sh && ./configure --disable-docs --prefix=' . catdir(rel2abs(), '3rdparty', 'libffi', 'install') . ' && CC=\'$(CC)\' CFLAGS=\'$(CFLAGS)\' $(MAKE) install',
+    rule  => 'cd 3rdparty/libffi && ./autogen.sh && ./configure --disable-docs --prefix=' . catdir(rel2abs(), '3rdparty', 'libffi', 'install') . ' && CC=\'$(CC)\' CFLAGS=\'$(CFLAGS) -fPIC\' $(MAKE) install',
     clean => 'cd 3rdparty/libffi && $(MAKE) clean',
 );
 
@@ -495,7 +495,6 @@ my %OS_LINUX = (
 
     -thirdparty => {
         uv => { %TP_UVDUMMY, objects => '$(UV_LINUX)' },
-        lf => { %TP_LF, objects => '$(LF_LINUX)' },
     },
 );
 
